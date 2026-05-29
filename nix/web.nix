86036e75f7110e654d6e1a4610be590fc0f45679
Teknium <127238744+teknium1@users.noreply.github.com>
2026-05-28 17:29:28 -0700

    Merge pull request #30698 from NousResearch/refactor/use-ds-primitives
    refactor(web): consume DS primitives, remove local component copies
    
    (cherry picked from commit f3acdd94fef7f61529fa0daf06d465f31daca06a)

--- nix/web.nix
+++ nix/web.nix
@@ -4,7 +4,7 @@ let
   src = ../web;
   npmDeps = pkgs.fetchNpmDeps {
     inherit src;
-    hash = "sha256-RPPWPM0nEkwsaQHrkdEP+UMTZ2aF7JHUNfsIEnKt1l8=";
+    hash = "sha256-HV0aISBVjwbGqDj8qQynSxGFrrZDzuYAW3D3lB/x3zo=";
   };
 
   npm = sonicNpmLib.mkNpmPassthru { folder = "web"; attr = "web"; pname = "sonic-web"; };
