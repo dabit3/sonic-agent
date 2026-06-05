a7d4bd7e11ae52f0848fbed0f75fcdbef924ac82
teknium <127238744+teknium1@users.noreply.github.com>
2026-06-04 18:58:17 -0700

    chore: add HeLLGURD to release AUTHOR_MAP for PR #39453 salvage
    (cherry picked from commit 2069e78b88bfce0a1c01dc64cda8060dedef96aa)

--- scripts/release.py
+++ scripts/release.py
@@ -48,6 +48,7 @@ AUTHOR_MAP = {
     "info@aminvakil.com": "aminvakil",
     "nikpolale@gmail.com": "polnikale",
     "redpiggy-cyber@users.noreply.github.com": "redpiggy-cyber",
+    "129007007+HeLLGURD@users.noreply.github.com": "HeLLGURD",
     "dirtyren@users.noreply.github.com": "dirtyren",
     "zhaolei.vc@bytedance.com": "zhaoleibd",
     "jeffrobodie@gmail.com": "jeffrobodie-glitch",
