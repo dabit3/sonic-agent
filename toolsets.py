mohamedorigami-jpg/hermes-agent

refactor(ntfy): convert built-in adapter to platform plugin
ntfy now ships as a self-contained plugin under plugins/platforms/ntfy/
instead of editing 8 core files (gateway/config.py Platform enum,
gateway/run.py factory + auth maps, cron/scheduler.py, toolsets.py,
hermes_cli/status.py, agent/prompt_builder.py, gateway/channel_directory.py,
tools/send_message_tool.py).

All routing goes through gateway/platform_registry via register_platform():
- adapter_factory, check_fn, validate_config, is_connected
- env_enablement_fn seeds PlatformConfig.extra from NTFY_* env vars so
  gateway status reflects env-only setups without instantiating httpx
- standalone_sender_fn handles deliver=ntfy cron jobs when cron runs
  out-of-process from the gateway
- allowed_users_env / allow_all_env hook into _is_user_authorized
- cron_deliver_env_var=NTFY_HOME_CHANNEL for cron home routing
- platform_hint surfaces in the system prompt
- pii_safe=True (topic names are the only identifier; no PII to redact)

Tests moved to tests/gateway/test_ntfy_plugin.py using _plugin_adapter_loader
so the module lives under plugin_adapter_ntfy in sys.modules and cannot
collide with sibling plugin-adapter tests on the same xdist worker. The
core-file grep tests (Platform.NTFY in source, hermes-ntfy in toolsets,
etc.) are replaced with plugin-shape tests covering register() metadata,
env_enablement_fn output, and standalone_sender_fn behavior.

68 tests pass under scripts/run_tests.sh.

toolsets.py
@@ -270,11 +270,6 @@ TOOLSETS = {
         "includes": [],
     },
 
-    "ntfy": {
-        "description": "ntfy push notification toolset",
-        "tools": [],
-        "includes": ["hermes-ntfy"],
-    },
     "yuanbao": {
         "description": "Yuanbao platform tools - group info, member queries, DM, stickers",
         "tools": [
@@ -520,11 +515,6 @@ TOOLSETS = {
         "includes": []
     },
 
-    "hermes-ntfy": {
-        "description": "ntfy push notification bot toolset",
-        "tools": _HERMES_CORE_TOOLS,
-        "includes": []
-    },
     "hermes-sms": {
         "description": "SMS bot toolset - interact with Hermes via SMS (Twilio)",
         "tools": _HERMES_CORE_TOOLS,
@@ -540,7 +530,7 @@ TOOLSETS = {
     "hermes-gateway": {
         "description": "Gateway toolset - union of all messaging platform tools",
         "tools": [],
-        "includes": ["hermes-telegram", "hermes-discord", "hermes-whatsapp", "hermes-slack", "hermes-signal", "hermes-bluebubbles", "hermes-homeassistant", "hermes-email", "hermes-sms", "hermes-mattermost", "hermes-matrix", "hermes-dingtalk", "hermes-feishu", "hermes-wecom", "hermes-wecom-callback", "hermes-weixin", "hermes-qqbot", "hermes-webhook", "hermes-yuanbao", "hermes-ntfy"]
+        "includes": ["hermes-telegram", "hermes-discord", "hermes-whatsapp", "hermes-slack", "hermes-signal", "hermes-bluebubbles", "hermes-homeassistant", "hermes-email", "hermes-sms", "hermes-mattermost", "hermes-matrix", "hermes-dingtalk", "hermes-feishu", "hermes-wecom", "hermes-wecom-callback", "hermes-weixin", "hermes-qqbot", "hermes-webhook", "hermes-yuanbao"]
     }
 }
 

dabit3/sonic-agent

(cherry picked from commit 6a8e131a0ab42e23fb8ad5e1e27ce2382f1d6a3b)

toolsets.py
@@ -270,11 +270,6 @@ TOOLSETS = {
         "includes": [],
     },
 
-    "ntfy": {
-        "description": "ntfy push notification toolset",
-        "tools": [],
-        "includes": ["sonic-ntfy"],
-    },
     "yuanbao": {
         "description": "Yuanbao platform tools - group info, member queries, DM, stickers",
         "tools": [
@@ -520,11 +515,6 @@ TOOLSETS = {
         "includes": []
     },
 
-    "sonic-ntfy": {
-        "description": "ntfy push notification bot toolset",
-        "tools": _SONIC_CORE_TOOLS,
-        "includes": []
-    },
     "sonic-sms": {
         "description": "SMS bot toolset - interact with Sonic via SMS (Twilio)",
         "tools": _SONIC_CORE_TOOLS,
@@ -540,7 +530,7 @@ TOOLSETS = {
     "sonic-gateway": {
         "description": "Gateway toolset - union of all messaging platform tools",
         "tools": [],
-        "includes": ["sonic-telegram", "sonic-discord", "sonic-whatsapp", "sonic-slack", "sonic-signal", "sonic-bluebubbles", "sonic-homeassistant", "sonic-email", "sonic-sms", "sonic-mattermost", "sonic-matrix", "sonic-dingtalk", "sonic-feishu", "sonic-wecom", "sonic-wecom-callback", "sonic-weixin", "sonic-qqbot", "sonic-webhook", "sonic-yuanbao", "sonic-ntfy"]
+        "includes": ["sonic-telegram", "sonic-discord", "sonic-whatsapp", "sonic-slack", "sonic-signal", "sonic-bluebubbles", "sonic-homeassistant", "sonic-email", "sonic-sms", "sonic-mattermost", "sonic-matrix", "sonic-dingtalk", "sonic-feishu", "sonic-wecom", "sonic-wecom-callback", "sonic-weixin", "sonic-qqbot", "sonic-webhook", "sonic-yuanbao"]
     }
 }
 