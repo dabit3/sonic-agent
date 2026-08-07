AnandaAnugrahHandyanto/savarez_agent

feat(ntfy): add ntfy platform adapter with atomic reconnect, identity fix, and 81 tests

toolsets.py
@@ -270,6 +270,11 @@ TOOLSETS = {
         "includes": [],
     },
 
+    "ntfy": {
+        "description": "ntfy push notification toolset",
+        "tools": [],
+        "includes": ["hermes-ntfy"],
+    },
     "yuanbao": {
         "description": "Yuanbao platform tools - group info, member queries, DM, stickers",
         "tools": [
@@ -515,6 +520,11 @@ TOOLSETS = {
         "includes": []
     },
 
+    "hermes-ntfy": {
+        "description": "ntfy push notification bot toolset",
+        "tools": _HERMES_CORE_TOOLS,
+        "includes": []
+    },
     "hermes-sms": {
         "description": "SMS bot toolset - interact with Hermes via SMS (Twilio)",
         "tools": _HERMES_CORE_TOOLS,
@@ -530,7 +540,7 @@ TOOLSETS = {
     "hermes-gateway": {
         "description": "Gateway toolset - union of all messaging platform tools",
         "tools": [],
-        "includes": ["hermes-telegram", "hermes-discord", "hermes-whatsapp", "hermes-slack", "hermes-signal", "hermes-bluebubbles", "hermes-homeassistant", "hermes-email", "hermes-sms", "hermes-mattermost", "hermes-matrix", "hermes-dingtalk", "hermes-feishu", "hermes-wecom", "hermes-wecom-callback", "hermes-weixin", "hermes-qqbot", "hermes-webhook", "hermes-yuanbao"]
+        "includes": ["hermes-telegram", "hermes-discord", "hermes-whatsapp", "hermes-slack", "hermes-signal", "hermes-bluebubbles", "hermes-homeassistant", "hermes-email", "hermes-sms", "hermes-mattermost", "hermes-matrix", "hermes-dingtalk", "hermes-feishu", "hermes-wecom", "hermes-wecom-callback", "hermes-weixin", "hermes-qqbot", "hermes-webhook", "hermes-yuanbao", "hermes-ntfy"]
     }
 }
 

dabit3/sonic-agent

(cherry picked from commit b10f17bf1e9321c586cb3afb0d80ef5bd4ae34d6)

toolsets.py
@@ -270,6 +270,11 @@ TOOLSETS = {
         "includes": [],
     },
 
+    "ntfy": {
+        "description": "ntfy push notification toolset",
+        "tools": [],
+        "includes": ["sonic-ntfy"],
+    },
     "yuanbao": {
         "description": "Yuanbao platform tools - group info, member queries, DM, stickers",
         "tools": [
@@ -515,6 +520,11 @@ TOOLSETS = {
         "includes": []
     },
 
+    "sonic-ntfy": {
+        "description": "ntfy push notification bot toolset",
+        "tools": _SONIC_CORE_TOOLS,
+        "includes": []
+    },
     "sonic-sms": {
         "description": "SMS bot toolset - interact with Sonic via SMS (Twilio)",
         "tools": _SONIC_CORE_TOOLS,
@@ -530,7 +540,7 @@ TOOLSETS = {
     "sonic-gateway": {
         "description": "Gateway toolset - union of all messaging platform tools",
         "tools": [],
-        "includes": ["sonic-telegram", "sonic-discord", "sonic-whatsapp", "sonic-slack", "sonic-signal", "sonic-bluebubbles", "sonic-homeassistant", "sonic-email", "sonic-sms", "sonic-mattermost", "sonic-matrix", "sonic-dingtalk", "sonic-feishu", "sonic-wecom", "sonic-wecom-callback", "sonic-weixin", "sonic-qqbot", "sonic-webhook", "sonic-yuanbao"]
+        "includes": ["sonic-telegram", "sonic-discord", "sonic-whatsapp", "sonic-slack", "sonic-signal", "sonic-bluebubbles", "sonic-homeassistant", "sonic-email", "sonic-sms", "sonic-mattermost", "sonic-matrix", "sonic-dingtalk", "sonic-feishu", "sonic-wecom", "sonic-wecom-callback", "sonic-weixin", "sonic-qqbot", "sonic-webhook", "sonic-yuanbao", "sonic-ntfy"]
     }
 }
 