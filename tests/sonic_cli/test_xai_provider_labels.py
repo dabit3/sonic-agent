AnandaAnugrahHandyanto/savarez_agent

fix(model): disambiguate xAI OAuth picker label

tests/hermes_cli/test_xai_provider_labels.py
@@ -1,0 +1,16 @@
+"""Regression tests for xAI provider label disambiguation."""
+
+from hermes_cli.models import provider_label
+from hermes_cli.providers import get_label
+
+
+def test_xai_oauth_provider_label_is_not_collapsed_to_api_key_label():
+    """The model picker must distinguish xAI API-key and OAuth providers."""
+    assert get_label("xai") == "xAI"
+    assert get_label("xai-oauth") == "xAI Grok OAuth (SuperGrok Subscription)"
+    assert get_label("grok-oauth") == "xAI Grok OAuth (SuperGrok Subscription)"
+
+
+def test_xai_oauth_provider_labels_match_canonical_model_labels():
+    """Provider helpers should agree on the OAuth display label."""
+    assert get_label("xai-oauth") == provider_label("xai-oauth")

dabit3/sonic-agent

(cherry picked from commit 4987fd2a596c2d1c0bafaa876ec55c4e32455f22)

tests/sonic_cli/test_xai_provider_labels.py
@@ -1,0 +1,16 @@
+"""Regression tests for xAI provider label disambiguation."""
+
+from sonic_cli.models import provider_label
+from sonic_cli.providers import get_label
+
+
+def test_xai_oauth_provider_label_is_not_collapsed_to_api_key_label():
+    """The model picker must distinguish xAI API-key and OAuth providers."""
+    assert get_label("xai") == "xAI"
+    assert get_label("xai-oauth") == "xAI Grok OAuth (SuperGrok Subscription)"
+    assert get_label("grok-oauth") == "xAI Grok OAuth (SuperGrok Subscription)"
+
+
+def test_xai_oauth_provider_labels_match_canonical_model_labels():
+    """Provider helpers should agree on the OAuth display label."""
+    assert get_label("xai-oauth") == provider_label("xai-oauth")