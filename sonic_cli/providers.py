NousResearch/hermes-agent

feat(providers): add OpenAI API provider option

hermes_cli/providers.py
@@ -60,6 +60,11 @@ HERMES_OVERLAYS: Dict[str, HermesOverlay] = {
         auth_type="oauth_external",
         base_url_override="https://chatgpt.com/backend-api/codex",
     ),
+    "openai-api": HermesOverlay(
+        transport="codex_responses",
+        base_url_override="https://api.openai.com/v1",
+        base_url_env_var="OPENAI_BASE_URL",
+    ),
     "xai-oauth": HermesOverlay(
         transport="codex_responses",
         auth_type="oauth_external",

dabit3/sonic-agent

(cherry picked from commit aeb87508c6adc83347855b017d602c52730ca071)

sonic_cli/providers.py
@@ -60,6 +60,11 @@ SONIC_OVERLAYS: Dict[str, SonicOverlay] = {
         auth_type="oauth_external",
         base_url_override="https://chatgpt.com/backend-api/codex",
     ),
+    "openai-api": SonicOverlay(
+        transport="codex_responses",
+        base_url_override="https://api.openai.com/v1",
+        base_url_env_var="OPENAI_BASE_URL",
+    ),
     "xai-oauth": SonicOverlay(
         transport="codex_responses",
         auth_type="oauth_external",