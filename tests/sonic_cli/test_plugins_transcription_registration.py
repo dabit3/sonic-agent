NousResearch/hermes-agent

feat(stt): add register_transcription_provider() plugin hook
Add an opt-in Python plugin surface for speech-to-text backends,
mirroring the TTS hook pattern. New backends (OpenRouter, SenseAudio,
Gemini-STT, custom proprietary engines) can be implemented as plugins
without modifying tools/transcription_tools.py.

Built-ins always win
--------------------
The 6 built-in STT providers (local/faster-whisper, local_command,
groq, openai, mistral, xai) keep their native handlers. Plugins
attempting to register under a built-in name are rejected at
registration time with a warning and re-checked defensively at
dispatch.

Resolution order
----------------
1. stt.provider matches a built-in → built-in dispatch (unchanged)
2. stt.provider matches a registered plugin →
   a. if plugin.is_available() returns False → unavailability envelope
      identifying the plugin (not the generic "No STT provider"
      message — the user explicitly opted into this plugin)
   b. otherwise plugin.transcribe() with model + language forwarded
      from stt.<provider>.{model,language} config
3. No match → legacy "No STT provider available" error (unchanged)

Per-provider config namespace
-----------------------------
Plugins read their config from stt.<provider> in config.yaml, mirroring
how built-ins read stt.openai.model / stt.mistral.model. The dispatcher
forwards `model` and `language` from this section. Caller's explicit
`model=` argument overrides the config-set model.

Files
-----
- agent/transcription_provider.py: TranscriptionProvider ABC
- agent/transcription_registry.py: register/get/list providers,
  built-in shadow guard, _reset_for_tests
- hermes_cli/plugins.py: register_transcription_provider() on
  PluginContext
- tools/transcription_tools.py: BUILTIN_STT_PROVIDERS frozenset,
  _dispatch_to_plugin_provider() with availability gate, wire-in
  after xai branch and before "No STT provider" error
- tests/agent/test_transcription_registry.py: 27 tests
- tests/hermes_cli/test_plugi

tests/hermes_cli/test_plugins_transcription_registration.py
@@ -1,0 +1,148 @@
+"""Tests for PluginContext.register_transcription_provider().
+
+Exercises the plugin context hook end-to-end: drops a fake plugin into
+``$HERMES_HOME/plugins/``, runs ``PluginManager().discover_and_load()``,
+and asserts the registration result.
+
+Mirrors the shape of ``test_plugins_tts_registration.py`` (companion
+TTS hook from issue #30398).
+"""
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from typing import Any, Dict
+
+import yaml
+
+
+def _write_plugin(
+    root: Path,
+    name: str,
+    *,
+    manifest_extra: Dict[str, Any] | None = None,
+    register_body: str = "pass",
+) -> Path:
+    plugin_dir = root / name
+    plugin_dir.mkdir(parents=True, exist_ok=True)
+    manifest = {
+        "name": name,
+        "version": "0.1.0",
+        "description": f"Test plugin {name}",
+    }
+    if manifest_extra:
+        manifest.update(manifest_extra)
+    (plugin_dir / "plugin.yaml").write_text(yaml.dump(manifest))
+    (plugin_dir / "__init__.py").write_text(
+        f"def register(ctx):\n    {register_body}\n"
+    )
+    return plugin_dir
+
+
+def _enable(hermes_home: Path, name: str) -> None:
+    cfg_path = hermes_home / "config.yaml"
+    cfg: dict = {}
+    if cfg_path.exists():
+        try:
+            cfg = yaml.safe_load(cfg_path.read_text()) or {}
+        except Exception:
+            cfg = {}
+    plugins_cfg = cfg.setdefault("plugins", {})
+    enabled = plugins_cfg.setdefault("enabled", [])
+    if isinstance(enabled, list) and name not in enabled:
+        enabled.append(name)
+    cfg_path.write_text(yaml.safe_dump(cfg))
+
+
+class TestRegisterTranscriptionProvider:
+    def test_accepts_valid_provider(self):
+        from hermes_cli.plugins import PluginManager
+
+        from agent import transcription_registry
+        transcription_registry._reset_for_tests()
+
+        hermes_home = Path(os.environ["HERMES_HOME"])
+        _write_plugin(
+            hermes_home / "plugins",
+            "my-stt-plugin",
+            register_body=(
+                "from agent.transcription_provider import TranscriptionProvider\n"
+                "    class P(TranscriptionProvider):\n"
+                "        @property\n"
+                "        def name(self): return 'fake-stt'\n"
+                "        def transcribe(self, file_path, **kw):\n"
+                "            return {'success': True, 'transcript': 'hi', 'provider': 'fake-stt'}\n"
+                "    ctx.register_transcription_provider(P())"
+            ),
+        )
+        _enable(hermes_home, "my-stt-plugin")
+
+        mgr = PluginManager()
+        mgr.discover_and_load()
+
+        assert mgr._plugins["my-stt-plugin"].enabled is True, (
+            f"Plugin failed to load: {mgr._plugins['my-stt-plugin'].error}"
+        )
+        assert transcription_registry.get_provider("fake-stt") is not None
+
+        transcription_registry._reset_for_tests()
+
+    def test_rejects_non_provider(self, caplog):
+        from hermes_cli.plugins import PluginManager
+
+        from agent import transcription_registry
+        transcription_registry._reset_for_tests()
+
+        hermes_home = Path(os.environ["HERMES_HOME"])
+        _write_plugin(
+            hermes_home / "plugins",
+            "bad-stt-plugin",
+            register_body="ctx.register_transcription_provider('not a provider')",
+        )
+        _enable(hermes_home, "bad-stt-plugin")
+
+        with caplog.at_level("WARNING"):
+            mgr = PluginManager()
+            mgr.discover_and_load()
+
+        assert mgr._plugins["bad-stt-plugin"].enabled is True
+        assert transcription_registry.get_provider("not a provider") is None
+        assert transcription_registry.list_providers() == []
+        assert "does not inherit from TranscriptionProvider" in caplog.text
+
+        transcription_registry._reset_for_tests()
+
+    def test_rejects_builtin_shadow(self, caplog):
+        from hermes_cli.plugins import PluginManager
+
+        from agent import transcription_registry
+        transcription_registry._reset_for_tests()
+
+        hermes_home = Path(os.environ["HERMES_HOME"])
+        _write_plugin(
+            hermes_home / "plugins",
+            "shadow-stt-plugin",
+            register_body=(
+                "from agent.transcription_provider import TranscriptionProvider\n"
+                "    class P(TranscriptionProvider):\n"
+                "        @property\n"
+                "        def name(self): return 'openai'\n"
+                "        def transcribe(self, file_path, **kw):\n"
+                "            return {'success': True, 'transcript': 'hi'}\n"
+                "    ctx.register_transcription_provider(P())"
+            ),
+        )
+        _enable(hermes_home, "shadow-stt-plugin")
+
+        with caplog.at_level("WARNING"):
+            mgr = PluginManager()
+            mgr.discover_and_load()
+
+        # Plugin still loaded normally — built-in shadowing is a warning,
+        # not an exception. The registry rejects the entry though.
+        assert mgr._plugins["shadow-stt-plugin"].enabled is True
+        assert transcription_registry.get_provider("openai") is None
+        assert "shadows a built-in name" in caplog.text
+
+        transcription_registry._reset_for_tests()

dabit3/sonic-agent

(cherry picked from commit 2cd952e1102638c8c0f7b03d2ce7021ae3706886)

tests/sonic_cli/test_plugins_transcription_registration.py
@@ -1,0 +1,148 @@
+"""Tests for PluginContext.register_transcription_provider().
+
+Exercises the plugin context hook end-to-end: drops a fake plugin into
+``$SONIC_HOME/plugins/``, runs ``PluginManager().discover_and_load()``,
+and asserts the registration result.
+
+Mirrors the shape of ``test_plugins_tts_registration.py`` (companion
+TTS hook from issue #30398).
+"""
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from typing import Any, Dict
+
+import yaml
+
+
+def _write_plugin(
+    root: Path,
+    name: str,
+    *,
+    manifest_extra: Dict[str, Any] | None = None,
+    register_body: str = "pass",
+) -> Path:
+    plugin_dir = root / name
+    plugin_dir.mkdir(parents=True, exist_ok=True)
+    manifest = {
+        "name": name,
+        "version": "0.1.0",
+        "description": f"Test plugin {name}",
+    }
+    if manifest_extra:
+        manifest.update(manifest_extra)
+    (plugin_dir / "plugin.yaml").write_text(yaml.dump(manifest))
+    (plugin_dir / "__init__.py").write_text(
+        f"def register(ctx):\n    {register_body}\n"
+    )
+    return plugin_dir
+
+
+def _enable(sonic_home: Path, name: str) -> None:
+    cfg_path = sonic_home / "config.yaml"
+    cfg: dict = {}
+    if cfg_path.exists():
+        try:
+            cfg = yaml.safe_load(cfg_path.read_text()) or {}
+        except Exception:
+            cfg = {}
+    plugins_cfg = cfg.setdefault("plugins", {})
+    enabled = plugins_cfg.setdefault("enabled", [])
+    if isinstance(enabled, list) and name not in enabled:
+        enabled.append(name)
+    cfg_path.write_text(yaml.safe_dump(cfg))
+
+
+class TestRegisterTranscriptionProvider:
+    def test_accepts_valid_provider(self):
+        from sonic_cli.plugins import PluginManager
+
+        from agent import transcription_registry
+        transcription_registry._reset_for_tests()
+
+        sonic_home = Path(os.environ["SONIC_HOME"])
+        _write_plugin(
+            sonic_home / "plugins",
+            "my-stt-plugin",
+            register_body=(
+                "from agent.transcription_provider import TranscriptionProvider\n"
+                "    class P(TranscriptionProvider):\n"
+                "        @property\n"
+                "        def name(self): return 'fake-stt'\n"
+                "        def transcribe(self, file_path, **kw):\n"
+                "            return {'success': True, 'transcript': 'hi', 'provider': 'fake-stt'}\n"
+                "    ctx.register_transcription_provider(P())"
+            ),
+        )
+        _enable(sonic_home, "my-stt-plugin")
+
+        mgr = PluginManager()
+        mgr.discover_and_load()
+
+        assert mgr._plugins["my-stt-plugin"].enabled is True, (
+            f"Plugin failed to load: {mgr._plugins['my-stt-plugin'].error}"
+        )
+        assert transcription_registry.get_provider("fake-stt") is not None
+
+        transcription_registry._reset_for_tests()
+
+    def test_rejects_non_provider(self, caplog):
+        from sonic_cli.plugins import PluginManager
+
+        from agent import transcription_registry
+        transcription_registry._reset_for_tests()
+
+        sonic_home = Path(os.environ["SONIC_HOME"])
+        _write_plugin(
+            sonic_home / "plugins",
+            "bad-stt-plugin",
+            register_body="ctx.register_transcription_provider('not a provider')",
+        )
+        _enable(sonic_home, "bad-stt-plugin")
+
+        with caplog.at_level("WARNING"):
+            mgr = PluginManager()
+            mgr.discover_and_load()
+
+        assert mgr._plugins["bad-stt-plugin"].enabled is True
+        assert transcription_registry.get_provider("not a provider") is None
+        assert transcription_registry.list_providers() == []
+        assert "does not inherit from TranscriptionProvider" in caplog.text
+
+        transcription_registry._reset_for_tests()
+
+    def test_rejects_builtin_shadow(self, caplog):
+        from sonic_cli.plugins import PluginManager
+
+        from agent import transcription_registry
+        transcription_registry._reset_for_tests()
+
+        sonic_home = Path(os.environ["SONIC_HOME"])
+        _write_plugin(
+            sonic_home / "plugins",
+            "shadow-stt-plugin",
+            register_body=(
+                "from agent.transcription_provider import TranscriptionProvider\n"
+                "    class P(TranscriptionProvider):\n"
+                "        @property\n"
+                "        def name(self): return 'openai'\n"
+                "        def transcribe(self, file_path, **kw):\n"
+                "            return {'success': True, 'transcript': 'hi'}\n"
+                "    ctx.register_transcription_provider(P())"
+            ),
+        )
+        _enable(sonic_home, "shadow-stt-plugin")
+
+        with caplog.at_level("WARNING"):
+            mgr = PluginManager()
+            mgr.discover_and_load()
+
+        # Plugin still loaded normally — built-in shadowing is a warning,
+        # not an exception. The registry rejects the entry though.
+        assert mgr._plugins["shadow-stt-plugin"].enabled is True
+        assert transcription_registry.get_provider("openai") is None
+        assert "shadows a built-in name" in caplog.text
+
+        transcription_registry._reset_for_tests()