AnandaAnugrahHandyanto/savarez_agent

chore: prune unused imports and duplicate import redefinitions
Remove unused imports (F401) and duplicate/shadowed import
redefinitions (F811) across the codebase using ruff's safe
autofixes. No behavioral changes -- imports only.

- ~1400 safe autofixes applied across 644 files (net -1072 lines)
- __init__.py re-exports preserved (excluded from F401 removal so
  public re-export surfaces stay intact)
- Re-exports that are imported or monkeypatched by tests but look
  unused in their defining module are kept with explicit # noqa:
  F401 (gateway/run.py load_dotenv; run_agent re-exports from
  agent.message_sanitization, agent.context_compressor,
  agent.retry_utils, agent.prompt_builder, agent.process_bootstrap,
  agent.codex_responses_adapter)
- Unsafe F841 (unused-variable) fixes deliberately skipped -- those
  can change behavior when the RHS has side effects
- ruff lints remain disabled in pyproject.toml (only PLW1514 is
  selected); this is a one-time cleanup, not a config change

Verification:
- python -m compileall: clean
- pytest --collect-only: all 27161 tests collect (zero import errors)
- core entry points import clean (run_agent, model_tools, cli,
  toolsets, hermes_state, batch_runner, gateway)
- static scan: every name any test imports directly from an edited
  module still resolves

hermes_cli/mcp_catalog.py
@@ -23,7 +23,6 @@ See references/mcp-catalog.md (this repo's skill) for the manifest schema.
 
 from __future__ import annotations
 
-import os
 import re
 import shutil
 import subprocess
@@ -41,7 +40,7 @@ from hermes_cli.config import (
     get_env_value,
     save_env_value,
 )
-from hermes_cli.cli_output import prompt as _prompt_input, prompt_yes_no
+from hermes_cli.cli_output import prompt as _prompt_input
 
 _MANIFEST_VERSION = 1
 

tests/run_agent/test_steer.py
@@ -281,7 +281,7 @@ class TestSteerCommandRegistry:
         """The /steer slash command must be registered so it reaches all
         platforms (CLI, gateway, TUI autocomplete, Telegram/Slack menus).
         """
-        from hermes_cli.commands import resolve_command, ACTIVE_SESSION_BYPASS_COMMANDS
+        from hermes_cli.commands import resolve_command
 
         cmd = resolve_command("steer")
         assert cmd is not None

tests/hermes_cli/test_doctor.py
@@ -792,7 +792,7 @@ class TestGitHubTokenCheck:
         monkeypatch.setenv("HERMES_HOME", str(home))
         monkeypatch.setenv("PATH", "/nonexistent")  # gh not found
 
-        from hermes_cli.doctor import run_doctor, _DHH
+        from hermes_cli.doctor import run_doctor
         import io, contextlib
 
         buf = io.StringIO()

dabit3/sonic-agent

(cherry picked from commit 66827f8947f08686b31b3952e768772423cefbcf)

sonic_cli/mcp_catalog.py
@@ -23,7 +23,6 @@ See references/mcp-catalog.md (this repo's skill) for the manifest schema.
 
 from __future__ import annotations
 
-import os
 import re
 import shutil
 import subprocess
@@ -41,7 +40,7 @@ from sonic_cli.config import (
     get_env_value,
     save_env_value,
 )
-from sonic_cli.cli_output import prompt as _prompt_input, prompt_yes_no
+from sonic_cli.cli_output import prompt as _prompt_input
 
 _MANIFEST_VERSION = 1
 

tests/run_agent/test_steer.py
@@ -281,7 +281,7 @@ class TestSteerCommandRegistry:
         """The /steer slash command must be registered so it reaches all
         platforms (CLI, gateway, TUI autocomplete, Telegram/Slack menus).
         """
-        from sonic_cli.commands import resolve_command, ACTIVE_SESSION_BYPASS_COMMANDS
+        from sonic_cli.commands import resolve_command
 
         cmd = resolve_command("steer")
         assert cmd is not None

tests/sonic_cli/test_doctor.py
@@ -792,7 +792,7 @@ class TestGitHubTokenCheck:
         monkeypatch.setenv("SONIC_HOME", str(home))
         monkeypatch.setenv("PATH", "/nonexistent")  # gh not found
 
-        from sonic_cli.doctor import run_doctor, _DHH
+        from sonic_cli.doctor import run_doctor
         import io, contextlib
 
         buf = io.StringIO()