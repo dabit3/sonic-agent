Lexedgeai26/legal-hermes

feat(nix): add #messaging and #full package variants (#33108)
* fix(plugins/discord): correct install_hint extra to [messaging]

The Discord platform registered install_hint pointing at
'hermes-agent[discord]', but pyproject.toml has no [discord] extra —
the deps live in [messaging] alongside Telegram and Slack. Users hitting
"Platform 'Discord' requirements not met" were directed at a pip command
that installs nothing.

* feat(nix): add #messaging and #full package variants

Make Discord/Telegram/Slack work out of the box for `nix profile install`
users. Messaging deps were dropped from [all] on 2026-05-12 in favor of
lazy-install, but lazy-install can't write to the read-only /nix/store —
users hit "No adapter available for discord" with no actionable guidance.

  - #messaging: pre-built with discord.py/telegram/slack (+33 MB venv)
  - #full:      all 18 platform-portable extras + matrix on Linux only
                (python-olm lacks Darwin PyPI wheels) (+738 MB venv)

Also adds a `messaging-variant` flake check that verifies `import discord`
succeeds in the sealed venv — regression guard for the lazy-install
migration.

Docs updated: Quick Start callout, extraDependencyGroups rewrite with
messaging as primary example + full extras table, troubleshooting row,
cheatsheet row.

Closure size deltas (measured x86_64-linux):
  default   1792 MB pkg / 512 MB venv
  messaging 1826 MB pkg / 546 MB venv   (+33 MB)
  full      2530 MB pkg / 1250 MB venv  (+738 MB)

* chore(nix): trim variant comments + alphabetize full extras

Drop the date-stamped changelog from messaging-variant's comment and the
"+33 MB / +704 MB" numbers from the variant defs — those drift and belong
in the PR description, not source. Alphabetize the 18-extra list in #full
so future additions produce clean one-line diffs.

No semantic change. messaging-variant check still passes.

nix/packages.nix
@@ -2,7 +2,7 @@
 { inputs, ... }:
 {
   perSystem =
-    { pkgs, inputs', ... }:
+    { pkgs, lib, inputs', ... }:
     let
       hermesAgent = pkgs.callPackage ./hermes-agent.nix {
         inherit (inputs) uv2nix pyproject-nix pyproject-build-systems;
@@ -15,6 +15,39 @@
     {
       packages = {
         default = hermesAgent;
+
+        # Ships discord.py + python-telegram-bot + slack-sdk so a plain
+        # `nix profile install .#messaging` connects to Discord/Telegram/Slack
+        # on first run — lazy-install can't write to the read-only /nix/store.
+        messaging = hermesAgent.override {
+          extraDependencyGroups = [ "messaging" ];
+        };
+
+        # All platform-portable optional integrations pre-built.
+        # matrix is Linux-only (oqs/liboqs lacks aarch64-darwin wheels).
+        full = hermesAgent.override {
+          extraDependencyGroups = [
+            "anthropic"
+            "azure-identity"
+            "bedrock"
+            "daytona"
+            "dingtalk"
+            "edge-tts"
+            "exa"
+            "fal"
+            "feishu"
+            "firecrawl"
+            "hindsight"
+            "honcho"
+            "messaging"
+            "modal"
+            "parallel-web"
+            "tts-premium"
+            "vercel"
+            "voice"
+          ] ++ lib.optionals pkgs.stdenv.isLinux [ "matrix" ];
+        };
+
         tui = hermesAgent.hermesTui;
         web = hermesAgent.hermesWeb;
 

plugins/platforms/discord/adapter.py
@@ -6163,7 +6163,7 @@ def register(ctx) -> None:
         check_fn=check_discord_requirements,
         is_connected=_is_connected,
         required_env=["DISCORD_BOT_TOKEN"],
-        install_hint="pip install 'hermes-agent[discord]'",
+        install_hint="pip install 'hermes-agent[messaging]'",
         # Interactive setup wizard — replaces the central
         # hermes_cli/setup.py::_setup_discord function.  Same shape as Teams.
         setup_fn=interactive_setup,

dabit3/sonic-agent

(cherry picked from commit 976979489a682fecce6d6eb977f83e31254f3c18)

nix/packages.nix
@@ -2,7 +2,7 @@
 { inputs, ... }:
 {
   perSystem =
-    { pkgs, inputs', ... }:
+    { pkgs, lib, inputs', ... }:
     let
       sonicAgent = pkgs.callPackage ./sonic-agent.nix {
         inherit (inputs) uv2nix pyproject-nix pyproject-build-systems;
@@ -15,6 +15,39 @@
     {
       packages = {
         default = sonicAgent;
+
+        # Ships discord.py + python-telegram-bot + slack-sdk so a plain
+        # `nix profile install .#messaging` connects to Discord/Telegram/Slack
+        # on first run — lazy-install can't write to the read-only /nix/store.
+        messaging = sonicAgent.override {
+          extraDependencyGroups = [ "messaging" ];
+        };
+
+        # All platform-portable optional integrations pre-built.
+        # matrix is Linux-only (oqs/liboqs lacks aarch64-darwin wheels).
+        full = sonicAgent.override {
+          extraDependencyGroups = [
+            "anthropic"
+            "azure-identity"
+            "bedrock"
+            "daytona"
+            "dingtalk"
+            "edge-tts"
+            "exa"
+            "fal"
+            "feishu"
+            "firecrawl"
+            "hindsight"
+            "honcho"
+            "messaging"
+            "modal"
+            "parallel-web"
+            "tts-premium"
+            "vercel"
+            "voice"
+          ] ++ lib.optionals pkgs.stdenv.isLinux [ "matrix" ];
+        };
+
         tui = sonicAgent.sonicTui;
         web = sonicAgent.sonicWeb;
 

plugins/platforms/discord/adapter.py
@@ -6163,7 +6163,7 @@ def register(ctx) -> None:
         check_fn=check_discord_requirements,
         is_connected=_is_connected,
         required_env=["DISCORD_BOT_TOKEN"],
-        install_hint="pip install 'sonic-agent[discord]'",
+        install_hint="pip install 'sonic-agent[messaging]'",
         # Interactive setup wizard — replaces the central
         # sonic_cli/setup.py::_setup_discord function.  Same shape as Teams.
         setup_fn=interactive_setup,