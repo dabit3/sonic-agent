Lexedgeai26/legal-hermes

feat(tools): surface the free tool pool in entitlement + setup (#36153)
Read the Portal's tool_access claim (JWT + /api/oauth/account) into NousToolAccessInfo and gate managed Tool Gateway access on it: tool_gateway_entitled (paid OR live pool) and per-category tool_gateway_entitled_for(). The pool funds web/image/tts/browser but not video, so per-backend availability, the charge picker (ensure_nous_portal_access coverage_category), and managed defaults all respect coverage.

Setup: rebuild prompt_enable_tool_gateway as a per-tool checklist that renders whenever the pool is enabled, lists only pool-covered tools (video excluded for free-pool users), and is framed as the free tool pool for $0 subscribers rather than a paid subscription. get_gateway_eligible_tools now gates and filters off the entitlement snapshot.

hermes_cli/tools_config.py
@@ -1926,6 +1926,18 @@ def _visible_providers(
     *activates* the gateway once paid access is confirmed.
     """
     features = get_nous_subscription_features(config, force_fresh=force_fresh)
+    acct = features.account_info
+    # Pool-only users (entitled to managed tools via the free tool pool but with
+    # no paid access) get image gen but NOT video gen — the pool doesn't fund
+    # `fal-video`. Rather than advertise a managed video row that would be denied
+    # on select, hide it for them. Logged-out users still see it (advertising)
+    # and paid users are entitled to it.
+    pool_only = bool(
+        acct
+        and acct.logged_in
+        and acct.paid_service_access is not True
+        and acct.tool_gateway_entitled
+    )
     visible = []
     for provider in cat.get("providers", []):
         # Nous-managed Tool Gateway rows stay visible regardless of auth —
@@ -1936,6 +1948,14 @@ def _visible_providers(
             provider.get("requires_nous_auth")
             and not provider.get("managed_nous_feature")
             and not features.nous_auth_present
+        ):
+            continue
+        # Hide the managed video-gen row from pool-only users — their free tool
+        # pool doesn't cover video, so showing it would only lead to a denial.
+        if (
+            pool_only
+            and provider.get("managed_nous_feature") == "video_gen"
+            and not (acct and acct.tool_gateway_entitled_for("fal-video"))
         ):
             continue
         visible.append(provider)
@@ -2711,13 +2731,17 @@ def _configure_provider(
     # auth + entitlement only, no inference-provider switch and no bulk
     # "enable all tools" prompt (that lives in `hermes model`).
     if managed_feature:
-        from hermes_cli.nous_subscription import ensure_nous_portal_access
+        from hermes_cli.nous_subscription import (
+            MANAGED_FEATURE_COVERAGE_CATEGORY,
+            ensure_nous_portal_access,
+        )
 
         if not ensure_nous_portal_access(
-            capability=f"{provider.get('name', 'the Nous Tool Gateway')}"
+            capability=f"{provider.get('name', 'the Nous Tool Gateway')}",
+            coverage_category=MANAGED_FEATURE_COVERAGE_CATEGORY.get(managed_feature),
         ):
             _print_warning(
-                "  Not enabled — Nous Portal paid access is required for this backend."
+                "  Not enabled — Nous Portal access is required for this backend."
             )
             return
 
@@ -3075,13 +3099,17 @@ def _reconfigure_provider(
     # Same inline Nous Portal login + entitlement gate as _configure_provider:
     # managed Tool Gateway backends only activate with paid Portal access.
     if managed_feature:
-        from hermes_cli.nous_subscription import ensure_nous_portal_access
+        from hermes_cli.nous_subscription import (
+            MANAGED_FEATURE_COVERAGE_CATEGORY,
+            ensure_nous_portal_access,
+        )
 
         if not ensure_nous_portal_access(
-            capability=f"{provider.get('name', 'the Nous Tool Gateway')}"
+            capability=f"{provider.get('name', 'the Nous Tool Gateway')}",
+            coverage_category=MANAGED_FEATURE_COVERAGE_CATEGORY.get(managed_feature),
         ):
             _print_warning(
-                "  Not enabled — Nous Portal paid access is required for this backend."
+                "  Not enabled — Nous Portal access is required for this backend."
             )
             return
 

tests/cli/test_cli_provider_resolution.py
@@ -309,10 +309,28 @@ def test_model_flow_nous_prints_subscription_guidance_without_mutating_explicit_
 
 
 def test_model_flow_nous_offers_tool_gateway_prompt_when_unconfigured(monkeypatch, capsys):
+    from hermes_cli.nous_account import NousPortalAccountInfo
+
+    # Entitled account (paid → all tools eligible) drives the offer; the prompt
+    # is a per-tool checklist now, so capture the call rather than scrape stdout.
     monkeypatch.setattr(
-        "hermes_cli.nous_subscription.managed_nous_tools_enabled",
-        lambda *args, **kwargs: True,
+        "hermes_cli.nous_subscription.get_nous_portal_account_info",
+        lambda **kwargs: NousPortalAccountInfo(
+            logged_in=True,
+            source="account_api",
+            fresh=True,
+            paid_service_access=True,
+        ),
     )
+    captured = {}
+
+    def _fake_checklist(title, items, pre_selected=None):
+        captured["title"] = title
+        captured["items"] = list(items)
+        return []  # decline; we only assert the prompt was offered
+
+    monkeypatch.setattr("hermes_cli.setup.prompt_checklist", _fake_checklist, raising=False)
+
     config = {
         "model": {"provider": "nous", "default": "claude-opus-4-6"},
         "tts": {"provider": "edge"},
@@ -338,10 +356,9 @@ def test_model_flow_nous_offers_tool_gateway_prompt_when_unconfigured(monkeypatc
     monkeypatch.setattr("hermes_cli.auth._update_config_for_provider", lambda provider, url: None)
     hermes_main._model_flow_nous(config, current_model="claude-opus-4-6")
 
-    out = capsys.readouterr().out
-    # Tool Gateway prompt should be shown (input() raises OSError in pytest
-    # which is caught, so the prompt text appears but nothing is applied)
-    assert "Tool Gateway" in out
+    # The per-tool Tool Gateway checklist was offered.
+    assert "title" in captured
+    assert "Tool Gateway" in captured["title"] or "tool pool" in captured["title"].lower()
 
 
 def test_codex_provider_uses_config_model(monkeypatch):

tests/hermes_cli/test_nous_subscription.py
@@ -1,7 +1,17 @@
 """Tests for Nous subscription feature detection."""
 
-from hermes_cli.nous_account import NousPortalAccountInfo
+from hermes_cli.nous_account import NousPortalAccountInfo, NousToolAccessInfo
 from hermes_cli import nous_subscription as ns
+
+
+_POOL_COVERAGE = {
+    "firecrawl": True,
+    "fal": True,
+    "fal-video": False,
+    "openai-audio": True,
+    "browser-use": True,
+    "modal": True,
+}
 
 
 def _account(*, logged_in: bool, paid: bool | None = None) -> NousPortalAccountInfo:
@@ -10,6 +20,17 @@ def _account(*, logged_in: bool, paid: bool | None = None) -> NousPortalAccountI
         source="jwt" if logged_in else "none",
         fresh=False,
         paid_service_access=paid,
+    )
+
+
+def _pool_account() -> NousPortalAccountInfo:
+    """A $0 subscriber with a live free tool pool (no paid access)."""
+    return NousPortalAccountInfo(
+        logged_in=True,
+        source="jwt",
+        fresh=False,
+        paid_service_access=False,
+        tool_access=NousToolAccessInfo(enabled=True, coverage=_POOL_COVERAGE),
     )
 
 
@@ -214,7 +235,10 @@ def test_get_nous_subscription_features_does_not_treat_quoted_false_as_gateway_o
 
 
 def test_get_gateway_eligible_tools_ignores_quoted_false_opt_in(monkeypatch):
-    monkeypatch.setattr(ns, "managed_nous_tools_enabled", lambda: True)
+    # Paid account: entitled to every category, including video.
+    monkeypatch.setattr(
+        ns, "get_nous_portal_account_info", lambda **kw: _account(logged_in=True, paid=True)
+    )
     monkeypatch.setattr(
         ns,
         "_get_gateway_direct_credentials",
@@ -231,6 +255,117 @@ def test_get_gateway_eligible_tools_ignores_quoted_false_opt_in(monkeypatch):
     assert "web" in has_direct
     assert "web" not in already_managed
     assert set(unconfigured) == {"image_gen", "video_gen", "tts", "browser"}
+
+
+def test_get_gateway_eligible_tools_pool_excludes_video(monkeypatch):
+    """A free-tool-pool user is offered the covered tools but NOT video gen."""
+    monkeypatch.setattr(ns, "get_nous_portal_account_info", lambda **kw: _pool_account())
+    monkeypatch.setattr(
+        ns,
+        "_get_gateway_direct_credentials",
+        lambda: {"web": False, "image_gen": False, "video_gen": False, "tts": False, "browser": False},
+    )
+
+    unconfigured, has_direct, already_managed = ns.get_gateway_eligible_tools(
+        {"model": {"provider": "nous"}}
+    )
+
+    assert set(unconfigured) == {"web", "image_gen", "tts", "browser"}
+    assert "video_gen" not in unconfigured
+    assert "video_gen" not in has_direct
+    assert "video_gen" not in already_managed
+
+
+def test_get_gateway_eligible_tools_empty_when_not_entitled(monkeypatch):
+    """A logged-in free user with no pool and no paid access gets nothing."""
+    monkeypatch.setattr(
+        ns, "get_nous_portal_account_info", lambda **kw: _account(logged_in=True, paid=False)
+    )
+
+    unconfigured, has_direct, already_managed = ns.get_gateway_eligible_tools(
+        {"model": {"provider": "nous"}}
+    )
+
+    assert (unconfigured, has_direct, already_managed) == ([], [], [])
+
+
+def _capture_checklist(monkeypatch, *, selected_idx):
+    """Patch prompt_checklist to capture its args and return chosen indices."""
+    captured = {}
+
+    def _fake_checklist(title, items, pre_selected=None):
+        captured["title"] = title
+        captured["items"] = list(items)
+        captured["pre_selected"] = list(pre_selected or [])
+        return list(selected_idx)
+
+    import hermes_cli.setup as setup_mod
+
+    monkeypatch.setattr(setup_mod, "prompt_checklist", _fake_checklist, raising=False)
+    monkeypatch.setattr(
+        "hermes_cli.config.save_config", lambda cfg: None, raising=False
+    )
+    return captured
+
+
+def test_prompt_enable_tool_gateway_pool_offers_covered_tools_only(monkeypatch):
+    """Pool user's checklist lists web/image/tts/browser and never video."""
+    monkeypatch.setattr(ns, "get_nous_portal_account_info", lambda **kw: _pool_account())
+    monkeypatch.setattr(
+        ns,
+        "_get_gateway_direct_credentials",
+        lambda: {"web": False, "image_gen": False, "video_gen": False, "tts": False, "browser": False},
+    )
+    captured = _capture_checklist(monkeypatch, selected_idx=[])
+
+    config = {"model": {"provider": "nous"}}
+    ns.prompt_enable_tool_gateway(config)
+
+    blob = " ".join(captured["items"]).lower()
+    assert "firecrawl" in blob  # web offered
+    assert "video" not in blob  # video NOT offered to a pool user
+    # Pool-aware framing, not "subscription".
+    assert "free" in captured["title"].lower() and "pool" in captured["title"].lower()
+
+
+def test_prompt_enable_tool_gateway_writes_only_selected(monkeypatch):
+    """Selecting a subset writes use_gateway only for those tools."""
+    monkeypatch.setattr(ns, "get_nous_portal_account_info", lambda **kw: _pool_account())
+    monkeypatch.setattr(
+        ns,
+        "_get_gateway_direct_credentials",
+        lambda: {"web": False, "image_gen": False, "video_gen": False, "tts": False, "browser": False},
+    )
+    # Offered order is _ALL_GATEWAY_KEYS filtered to covered: web, image_gen, tts, browser.
+    # Select index 0 (web) and 1 (image_gen) only.
+    _capture_checklist(monkeypatch, selected_idx=[0, 1])
+
+    config = {"model": {"provider": "nous"}}
+    changed = ns.prompt_enable_tool_gateway(config)
+
+    assert changed == {"web", "image_gen"}
+    assert config["web"]["use_gateway"] is True
+    assert config["image_gen"]["use_gateway"] is True
+    assert "tts" not in config or config.get("tts", {}).get("use_gateway") is not True
+    assert "video_gen" not in config
+
+
+def test_prompt_enable_tool_gateway_paid_user_offers_video(monkeypatch):
+    """Paid users still get video gen in the offer (regression guard)."""
+    monkeypatch.setattr(
+        ns, "get_nous_portal_account_info", lambda **kw: _account(logged_in=True, paid=True)
+    )
+    monkeypatch.setattr(
+        ns,
+        "_get_gateway_direct_credentials",
+        lambda: {"web": False, "image_gen": False, "video_gen": False, "tts": False, "browser": False},
+    )
+    captured = _capture_checklist(monkeypatch, selected_idx=[])
+
+    ns.prompt_enable_tool_gateway({"model": {"provider": "nous"}})
+
+    blob = " ".join(captured["items"]).lower()
+    assert "video" in blob
 
 
 def test_apply_nous_managed_defaults_writes_video_gen_config(monkeypatch):

dabit3/sonic-agent

(cherry picked from commit e1c7a9aa7b91b0c153022a7c8b55f1b469cf5c3b)

sonic_cli/tools_config.py
@@ -1926,6 +1926,18 @@ def _visible_providers(
     *activates* the gateway once paid access is confirmed.
     """
     features = get_nous_subscription_features(config, force_fresh=force_fresh)
+    acct = features.account_info
+    # Pool-only users (entitled to managed tools via the free tool pool but with
+    # no paid access) get image gen but NOT video gen — the pool doesn't fund
+    # `fal-video`. Rather than advertise a managed video row that would be denied
+    # on select, hide it for them. Logged-out users still see it (advertising)
+    # and paid users are entitled to it.
+    pool_only = bool(
+        acct
+        and acct.logged_in
+        and acct.paid_service_access is not True
+        and acct.tool_gateway_entitled
+    )
     visible = []
     for provider in cat.get("providers", []):
         # Nous-managed Tool Gateway rows stay visible regardless of auth —
@@ -1936,6 +1948,14 @@ def _visible_providers(
             provider.get("requires_nous_auth")
             and not provider.get("managed_nous_feature")
             and not features.nous_auth_present
+        ):
+            continue
+        # Hide the managed video-gen row from pool-only users — their free tool
+        # pool doesn't cover video, so showing it would only lead to a denial.
+        if (
+            pool_only
+            and provider.get("managed_nous_feature") == "video_gen"
+            and not (acct and acct.tool_gateway_entitled_for("fal-video"))
         ):
             continue
         visible.append(provider)
@@ -2711,13 +2731,17 @@ def _configure_provider(
     # auth + entitlement only, no inference-provider switch and no bulk
     # "enable all tools" prompt (that lives in `sonic model`).
     if managed_feature:
-        from sonic_cli.nous_subscription import ensure_nous_portal_access
+        from sonic_cli.nous_subscription import (
+            MANAGED_FEATURE_COVERAGE_CATEGORY,
+            ensure_nous_portal_access,
+        )
 
         if not ensure_nous_portal_access(
-            capability=f"{provider.get('name', 'the Nous Tool Gateway')}"
+            capability=f"{provider.get('name', 'the Nous Tool Gateway')}",
+            coverage_category=MANAGED_FEATURE_COVERAGE_CATEGORY.get(managed_feature),
         ):
             _print_warning(
-                "  Not enabled — Nous Portal paid access is required for this backend."
+                "  Not enabled — Nous Portal access is required for this backend."
             )
             return
 
@@ -3075,13 +3099,17 @@ def _reconfigure_provider(
     # Same inline Nous Portal login + entitlement gate as _configure_provider:
     # managed Tool Gateway backends only activate with paid Portal access.
     if managed_feature:
-        from sonic_cli.nous_subscription import ensure_nous_portal_access
+        from sonic_cli.nous_subscription import (
+            MANAGED_FEATURE_COVERAGE_CATEGORY,
+            ensure_nous_portal_access,
+        )
 
         if not ensure_nous_portal_access(
-            capability=f"{provider.get('name', 'the Nous Tool Gateway')}"
+            capability=f"{provider.get('name', 'the Nous Tool Gateway')}",
+            coverage_category=MANAGED_FEATURE_COVERAGE_CATEGORY.get(managed_feature),
         ):
             _print_warning(
-                "  Not enabled — Nous Portal paid access is required for this backend."
+                "  Not enabled — Nous Portal access is required for this backend."
             )
             return
 

tests/cli/test_cli_provider_resolution.py
@@ -309,10 +309,28 @@ def test_model_flow_nous_prints_subscription_guidance_without_mutating_explicit_
 
 
 def test_model_flow_nous_offers_tool_gateway_prompt_when_unconfigured(monkeypatch, capsys):
+    from hermes_cli.nous_account import NousPortalAccountInfo
+
+    # Entitled account (paid → all tools eligible) drives the offer; the prompt
+    # is a per-tool checklist now, so capture the call rather than scrape stdout.
     monkeypatch.setattr(
-        "sonic_cli.nous_subscription.managed_nous_tools_enabled",
-        lambda *args, **kwargs: True,
+        "sonic_cli.nous_subscription.get_nous_portal_account_info",
+        lambda **kwargs: NousPortalAccountInfo(
+            logged_in=True,
+            source="account_api",
+            fresh=True,
+            paid_service_access=True,
+        ),
     )
+    captured = {}
+
+    def _fake_checklist(title, items, pre_selected=None):
+        captured["title"] = title
+        captured["items"] = list(items)
+        return []  # decline; we only assert the prompt was offered
+
+    monkeypatch.setattr("hermes_cli.setup.prompt_checklist", _fake_checklist, raising=False)
+
     config = {
         "model": {"provider": "nous", "default": "claude-opus-4-6"},
         "tts": {"provider": "edge"},
@@ -338,10 +356,9 @@ def test_model_flow_nous_offers_tool_gateway_prompt_when_unconfigured(monkeypatc
     monkeypatch.setattr("sonic_cli.auth._update_config_for_provider", lambda provider, url: None)
     sonic_main._model_flow_nous(config, current_model="claude-opus-4-6")
 
-    out = capsys.readouterr().out
-    # Tool Gateway prompt should be shown (input() raises OSError in pytest
-    # which is caught, so the prompt text appears but nothing is applied)
-    assert "Tool Gateway" in out
+    # The per-tool Tool Gateway checklist was offered.
+    assert "title" in captured
+    assert "Tool Gateway" in captured["title"] or "tool pool" in captured["title"].lower()
 
 
 def test_codex_provider_uses_config_model(monkeypatch):

tests/sonic_cli/test_nous_subscription.py
@@ -1,7 +1,17 @@
 """Tests for Nous subscription feature detection."""
 
-from sonic_cli.nous_account import NousPortalAccountInfo
+from sonic_cli.nous_account import NousPortalAccountInfo, NousToolAccessInfo
 from sonic_cli import nous_subscription as ns
+
+
+_POOL_COVERAGE = {
+    "firecrawl": True,
+    "fal": True,
+    "fal-video": False,
+    "openai-audio": True,
+    "browser-use": True,
+    "modal": True,
+}
 
 
 def _account(*, logged_in: bool, paid: bool | None = None) -> NousPortalAccountInfo:
@@ -10,6 +20,17 @@ def _account(*, logged_in: bool, paid: bool | None = None) -> NousPortalAccountI
         source="jwt" if logged_in else "none",
         fresh=False,
         paid_service_access=paid,
+    )
+
+
+def _pool_account() -> NousPortalAccountInfo:
+    """A $0 subscriber with a live free tool pool (no paid access)."""
+    return NousPortalAccountInfo(
+        logged_in=True,
+        source="jwt",
+        fresh=False,
+        paid_service_access=False,
+        tool_access=NousToolAccessInfo(enabled=True, coverage=_POOL_COVERAGE),
     )
 
 
@@ -214,7 +235,10 @@ def test_get_nous_subscription_features_does_not_treat_quoted_false_as_gateway_o
 
 
 def test_get_gateway_eligible_tools_ignores_quoted_false_opt_in(monkeypatch):
-    monkeypatch.setattr(ns, "managed_nous_tools_enabled", lambda: True)
+    # Paid account: entitled to every category, including video.
+    monkeypatch.setattr(
+        ns, "get_nous_portal_account_info", lambda **kw: _account(logged_in=True, paid=True)
+    )
     monkeypatch.setattr(
         ns,
         "_get_gateway_direct_credentials",
@@ -231,6 +255,117 @@ def test_get_gateway_eligible_tools_ignores_quoted_false_opt_in(monkeypatch):
     assert "web" in has_direct
     assert "web" not in already_managed
     assert set(unconfigured) == {"image_gen", "video_gen", "tts", "browser"}
+
+
+def test_get_gateway_eligible_tools_pool_excludes_video(monkeypatch):
+    """A free-tool-pool user is offered the covered tools but NOT video gen."""
+    monkeypatch.setattr(ns, "get_nous_portal_account_info", lambda **kw: _pool_account())
+    monkeypatch.setattr(
+        ns,
+        "_get_gateway_direct_credentials",
+        lambda: {"web": False, "image_gen": False, "video_gen": False, "tts": False, "browser": False},
+    )
+
+    unconfigured, has_direct, already_managed = ns.get_gateway_eligible_tools(
+        {"model": {"provider": "nous"}}
+    )
+
+    assert set(unconfigured) == {"web", "image_gen", "tts", "browser"}
+    assert "video_gen" not in unconfigured
+    assert "video_gen" not in has_direct
+    assert "video_gen" not in already_managed
+
+
+def test_get_gateway_eligible_tools_empty_when_not_entitled(monkeypatch):
+    """A logged-in free user with no pool and no paid access gets nothing."""
+    monkeypatch.setattr(
+        ns, "get_nous_portal_account_info", lambda **kw: _account(logged_in=True, paid=False)
+    )
+
+    unconfigured, has_direct, already_managed = ns.get_gateway_eligible_tools(
+        {"model": {"provider": "nous"}}
+    )
+
+    assert (unconfigured, has_direct, already_managed) == ([], [], [])
+
+
+def _capture_checklist(monkeypatch, *, selected_idx):
+    """Patch prompt_checklist to capture its args and return chosen indices."""
+    captured = {}
+
+    def _fake_checklist(title, items, pre_selected=None):
+        captured["title"] = title
+        captured["items"] = list(items)
+        captured["pre_selected"] = list(pre_selected or [])
+        return list(selected_idx)
+
+    import hermes_cli.setup as setup_mod
+
+    monkeypatch.setattr(setup_mod, "prompt_checklist", _fake_checklist, raising=False)
+    monkeypatch.setattr(
+        "hermes_cli.config.save_config", lambda cfg: None, raising=False
+    )
+    return captured
+
+
+def test_prompt_enable_tool_gateway_pool_offers_covered_tools_only(monkeypatch):
+    """Pool user's checklist lists web/image/tts/browser and never video."""
+    monkeypatch.setattr(ns, "get_nous_portal_account_info", lambda **kw: _pool_account())
+    monkeypatch.setattr(
+        ns,
+        "_get_gateway_direct_credentials",
+        lambda: {"web": False, "image_gen": False, "video_gen": False, "tts": False, "browser": False},
+    )
+    captured = _capture_checklist(monkeypatch, selected_idx=[])
+
+    config = {"model": {"provider": "nous"}}
+    ns.prompt_enable_tool_gateway(config)
+
+    blob = " ".join(captured["items"]).lower()
+    assert "firecrawl" in blob  # web offered
+    assert "video" not in blob  # video NOT offered to a pool user
+    # Pool-aware framing, not "subscription".
+    assert "free" in captured["title"].lower() and "pool" in captured["title"].lower()
+
+
+def test_prompt_enable_tool_gateway_writes_only_selected(monkeypatch):
+    """Selecting a subset writes use_gateway only for those tools."""
+    monkeypatch.setattr(ns, "get_nous_portal_account_info", lambda **kw: _pool_account())
+    monkeypatch.setattr(
+        ns,
+        "_get_gateway_direct_credentials",
+        lambda: {"web": False, "image_gen": False, "video_gen": False, "tts": False, "browser": False},
+    )
+    # Offered order is _ALL_GATEWAY_KEYS filtered to covered: web, image_gen, tts, browser.
+    # Select index 0 (web) and 1 (image_gen) only.
+    _capture_checklist(monkeypatch, selected_idx=[0, 1])
+
+    config = {"model": {"provider": "nous"}}
+    changed = ns.prompt_enable_tool_gateway(config)
+
+    assert changed == {"web", "image_gen"}
+    assert config["web"]["use_gateway"] is True
+    assert config["image_gen"]["use_gateway"] is True
+    assert "tts" not in config or config.get("tts", {}).get("use_gateway") is not True
+    assert "video_gen" not in config
+
+
+def test_prompt_enable_tool_gateway_paid_user_offers_video(monkeypatch):
+    """Paid users still get video gen in the offer (regression guard)."""
+    monkeypatch.setattr(
+        ns, "get_nous_portal_account_info", lambda **kw: _account(logged_in=True, paid=True)
+    )
+    monkeypatch.setattr(
+        ns,
+        "_get_gateway_direct_credentials",
+        lambda: {"web": False, "image_gen": False, "video_gen": False, "tts": False, "browser": False},
+    )
+    captured = _capture_checklist(monkeypatch, selected_idx=[])
+
+    ns.prompt_enable_tool_gateway({"model": {"provider": "nous"}})
+
+    blob = " ".join(captured["items"]).lower()
+    assert "video" in blob
 
 
 def test_apply_nous_managed_defaults_writes_video_gen_config(monkeypatch):