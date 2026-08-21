Lexedgeai26/legal-hermes

fix(docker): skip unnecessary boot chown when volume ownership already matches remapped UID (#35027)

docker/stage2-hook.sh
@@ -128,9 +128,7 @@ done
 # mkdir -p block below seeds. Keep them in sync if the seed list changes.
 actual_hermes_uid=$(id -u hermes)
 needs_chown=false
-if [ -n "${HERMES_UID:-}" ] && [ "$HERMES_UID" != "10000" ]; then
-    needs_chown=true
-elif [ "$(stat -c %u "$HERMES_HOME" 2>/dev/null)" != "$actual_hermes_uid" ]; then
+if [ "$(stat -c %u "$HERMES_HOME" 2>/dev/null)" != "$actual_hermes_uid" ]; then
     needs_chown=true
 fi
 if [ "$needs_chown" = true ]; then

dabit3/sonic-agent

(cherry picked from commit 1031031dece9dbacce1d4168de8dffd2302a67c5)

docker/stage2-hook.sh
@@ -128,9 +128,7 @@ done
 # mkdir -p block below seeds. Keep them in sync if the seed list changes.
 actual_sonic_uid=$(id -u sonic)
 needs_chown=false
-if [ -n "${SONIC_UID:-}" ] && [ "$SONIC_UID" != "10000" ]; then
-    needs_chown=true
-elif [ "$(stat -c %u "$SONIC_HOME" 2>/dev/null)" != "$actual_sonic_uid" ]; then
+if [ "$(stat -c %u "$SONIC_HOME" 2>/dev/null)" != "$actual_sonic_uid" ]; then
     needs_chown=true
 fi
 if [ "$needs_chown" = true ]; then