Lexedgeai26/legal-hermes

fix(ci): regen model catalog + stop gui tests consuming macos-fixup subprocess calls (#36687)
Two pre-existing failures on main, unrelated to each other:

- test_model_catalog: website/static/api/model-catalog.json was stale vs
  _PROVIDER_MODELS — minimax/minimax-m2.7 was renamed to minimax/minimax-m3
  without regenerating the committed manifest. Ran scripts/build_model_catalog.py.

- test_gui_command: the macOS relaunchable-signing fixup
  (_desktop_macos_relaunchable_fixup) makes two subprocess.run calls (xattr +
  codesign) on darwin before launch. The two darwin GUI tests set
  sys.platform='darwin' and mock subprocess.run with a 2-element side_effect
  (pack + launch), so the fixup's calls drained the iterator -> StopIteration.
  Mock out the fixup in those two tests so the subprocess accounting stays
  focused on pack/launch.

tests/hermes_cli/test_gui_command.py
@@ -60,6 +60,7 @@ def test_gui_installs_packages_and_launches_desktop_app(tmp_path, monkeypatch):
 
     with patch("hermes_cli.main.shutil.which", return_value="/usr/bin/npm"), \
          patch("hermes_cli.main._run_npm_install_deterministic", return_value=install_ok) as mock_install, \
+         patch("hermes_cli.main._desktop_macos_relaunchable_fixup"), \
          patch("hermes_cli.main.subprocess.run", side_effect=[pack_ok, launch_ok]) as mock_run, \
          pytest.raises(SystemExit) as exc:
         cli_main.cmd_gui(_ns())
@@ -85,6 +86,7 @@ def test_gui_forwards_desktop_environment_overrides(tmp_path, monkeypatch):
 
     with patch("hermes_cli.main.shutil.which", return_value="/usr/bin/npm"), \
          patch("hermes_cli.main._run_npm_install_deterministic", return_value=ok), \
+         patch("hermes_cli.main._desktop_macos_relaunchable_fixup"), \
          patch("hermes_cli.main.subprocess.run", side_effect=[ok, ok]) as mock_run, \
          pytest.raises(SystemExit):
         cli_main.cmd_gui(_ns(

dabit3/sonic-agent

(cherry picked from commit 92a567db2d7a5031df8211efbfdad864c2f51faf)

tests/sonic_cli/test_gui_command.py
@@ -60,6 +60,7 @@ def test_gui_installs_packages_and_launches_desktop_app(tmp_path, monkeypatch):
 
     with patch("sonic_cli.main.shutil.which", return_value="/usr/bin/npm"), \
          patch("sonic_cli.main._run_npm_install_deterministic", return_value=install_ok) as mock_install, \
+         patch("sonic_cli.main._desktop_macos_relaunchable_fixup"), \
          patch("sonic_cli.main.subprocess.run", side_effect=[pack_ok, launch_ok]) as mock_run, \
          pytest.raises(SystemExit) as exc:
         cli_main.cmd_gui(_ns())
@@ -85,6 +86,7 @@ def test_gui_forwards_desktop_environment_overrides(tmp_path, monkeypatch):
 
     with patch("sonic_cli.main.shutil.which", return_value="/usr/bin/npm"), \
          patch("sonic_cli.main._run_npm_install_deterministic", return_value=ok), \
+         patch("sonic_cli.main._desktop_macos_relaunchable_fixup"), \
          patch("sonic_cli.main.subprocess.run", side_effect=[ok, ok]) as mock_run, \
          pytest.raises(SystemExit):
         cli_main.cmd_gui(_ns(