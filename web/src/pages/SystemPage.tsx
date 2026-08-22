Lexedgeai26/legal-hermes

feat(dashboard): nous-blue theme, bulk sessions, schedule picker (#37383)
* feat(dashboard): nous-blue theme, bulk sessions, schedule picker

Batch of related dashboard improvements gathered on
austin/fix/dashboard-changes:

* Nous Blue theme — faithful port of the LENS_5I overlay system onto
  the existing DashboardTheme. Lifts the foreground inversion layer to
  z-index 200 to fix the long-standing hover / loading visual artifact,
  adds an explicit swatchColors slot so the theme picker shows the
  post-inversion preview, and migrates the legacy "lens-5i" theme key
  from localStorage / API to "nous-blue" on first read.
* Theme-aware series colors: new --series-input-token /
  --series-output-token CSS vars consumed by Analytics + Models
  charts; ToolCall + ModelInfoCard switched to semantic
  --color-success for diff lines and the Tools capability badge.
* Analytics + Models headers: consolidate period selector + refresh
  next to the page title and drop the redundant period badge.
* Bulk session management — "Delete empty (N)" button + per-row
  checkboxes with shift-click range select and a bulk-delete action
  bar. Backed by SessionDB.delete_sessions() /
  delete_empty_sessions() plus POST /api/sessions/bulk-delete and
  DELETE /api/sessions/empty (registered before the templated
  /api/sessions/{session_id} family so they don't get shadowed).
  Hard cap of 500 IDs per bulk request. Full pytest coverage.
* Cron page — human-readable schedule picker (every-interval / daily
  / weekly / monthly / once / custom) replaces the raw cron
  expression input; the job list now renders "Weekly on Mon, Wed,
  Fri at 14:30" instead of "30 14 * * 1,3,5". English-only ordinals
  for monthly schedules so non-English locales don't get incorrect
  suffixes.
* example-dashboard plugin moved from plugins/ to tests/fixtures/ so
  stock installs no longer ship the demo. Tests install it
  dynamically via a pytest fixture that also reorders the FastAPI
  routes.
* i18n: 40+ new key

web/src/pages/SystemPage.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import {
   Activity,
   Brain,
@@ -21,12 +22,12 @@ import {
 } from "lucide-react";
 import { Badge } from "@nous-research/ui/ui/components/badge";
 import { Button } from "@nous-research/ui/ui/components/button";
-import { Select, SelectOption } from "@nous-research/ui/ui/components/select";
 import { Spinner } from "@nous-research/ui/ui/components/spinner";
 import { H2 } from "@nous-research/ui/ui/components/typography/h2";
 import { Card, CardContent } from "@nous-research/ui/ui/components/card";
 import { Input } from "@nous-research/ui/ui/components/input";
 import { Label } from "@nous-research/ui/ui/components/label";
+import { Select, SelectOption } from "@nous-research/ui/ui/components/select";
 import { Toast } from "@nous-research/ui/ui/components/toast";
 import { useToast } from "@nous-research/ui/hooks/use-toast";
 import { useConfirmDelete } from "@nous-research/ui/hooks/use-confirm-delete";
@@ -236,16 +237,9 @@ export default function SystemPage() {
   };
 
   // ── Memory ─────────────────────────────────────────────────────────
-  const setMemoryProvider = async (provider: string) => {
-    try {
-      await api.setMemoryProvider(provider);
-      showToast(`Memory provider: ${provider || "built-in only"}`, "success");
-      loadAll();
-    } catch (e) {
-      showToast(`Failed to set provider: ${e}`, "error");
-    }
-  };
-
+  // Memory provider selection lives on the /plugins page now (see the
+  // read-only display + link below); the dropdown was intentionally
+  // dropped from this card during the admin-panel refresh.
   const memoryReset = useConfirmDelete({
     onDelete: useCallback(
       async (target: string) => {
@@ -748,26 +742,22 @@ export default function SystemPage() {
         </H2>
         <Card>
           <CardContent className="flex flex-col gap-4 py-4">
-            <div className="grid gap-2 max-w-sm">
-              <Label htmlFor="mem-provider">External provider</Label>
-              <Select
-                id="mem-provider"
-                value={memory?.active || ""}
-                onValueChange={setMemoryProvider}
-              >
-                <SelectOption value="">Built-in only</SelectOption>
-                {(memory?.providers ?? []).map((p) => (
-                  <SelectOption key={p.name} value={p.name}>
-                    {p.name}
-                    {p.configured ? " (configured)" : ""}
-                  </SelectOption>
-                ))}
-              </Select>
-              <p className="text-xs text-muted-foreground">
-                Set up a new provider's credentials with{" "}
-                <span className="font-mono">hermes memory setup</span>.
-              </p>
+            <div className="flex flex-wrap items-center gap-x-3 gap-y-1 text-xs text-muted-foreground">
+              <span>
+                External provider:{" "}
+                <span className="font-mono text-foreground">
+                  {memory?.active || "built-in only"}
+                </span>
+              </span>
+              <Link to="/plugins" className="underline">
+                Change in Plugins →
+              </Link>
+              <span className="ml-auto">
+                New credentials:{" "}
+                <span className="font-mono">hermes memory setup</span>
+              </span>
             </div>
+
             <div className="flex flex-wrap items-center gap-3 border-t border-border pt-3">
               <span className="text-xs text-muted-foreground">
                 Built-in files — MEMORY.md:{" "}

dabit3/sonic-agent

(cherry picked from commit 6d14a24b798c6494eff0f0012ad093500ca59bd9)

web/src/pages/SystemPage.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import {
   Activity,
   Brain,
@@ -21,12 +22,12 @@ import {
 } from "lucide-react";
 import { Badge } from "@nous-research/ui/ui/components/badge";
 import { Button } from "@nous-research/ui/ui/components/button";
-import { Select, SelectOption } from "@nous-research/ui/ui/components/select";
 import { Spinner } from "@nous-research/ui/ui/components/spinner";
 import { H2 } from "@nous-research/ui/ui/components/typography/h2";
 import { Card, CardContent } from "@nous-research/ui/ui/components/card";
 import { Input } from "@nous-research/ui/ui/components/input";
 import { Label } from "@nous-research/ui/ui/components/label";
+import { Select, SelectOption } from "@nous-research/ui/ui/components/select";
 import { Toast } from "@nous-research/ui/ui/components/toast";
 import { useToast } from "@nous-research/ui/hooks/use-toast";
 import { useConfirmDelete } from "@nous-research/ui/hooks/use-confirm-delete";
@@ -236,16 +237,9 @@ export default function SystemPage() {
   };
 
   // ── Memory ─────────────────────────────────────────────────────────
-  const setMemoryProvider = async (provider: string) => {
-    try {
-      await api.setMemoryProvider(provider);
-      showToast(`Memory provider: ${provider || "built-in only"}`, "success");
-      loadAll();
-    } catch (e) {
-      showToast(`Failed to set provider: ${e}`, "error");
-    }
-  };
-
+  // Memory provider selection lives on the /plugins page now (see the
+  // read-only display + link below); the dropdown was intentionally
+  // dropped from this card during the admin-panel refresh.
   const memoryReset = useConfirmDelete({
     onDelete: useCallback(
       async (target: string) => {
@@ -748,26 +742,22 @@ export default function SystemPage() {
         </H2>
         <Card>
           <CardContent className="flex flex-col gap-4 py-4">
-            <div className="grid gap-2 max-w-sm">
-              <Label htmlFor="mem-provider">External provider</Label>
-              <Select
-                id="mem-provider"
-                value={memory?.active || ""}
-                onValueChange={setMemoryProvider}
-              >
-                <SelectOption value="">Built-in only</SelectOption>
-                {(memory?.providers ?? []).map((p) => (
-                  <SelectOption key={p.name} value={p.name}>
-                    {p.name}
-                    {p.configured ? " (configured)" : ""}
-                  </SelectOption>
-                ))}
-              </Select>
-              <p className="text-xs text-muted-foreground">
-                Set up a new provider's credentials with{" "}
-                <span className="font-mono">sonic memory setup</span>.
-              </p>
+            <div className="flex flex-wrap items-center gap-x-3 gap-y-1 text-xs text-muted-foreground">
+              <span>
+                External provider:{" "}
+                <span className="font-mono text-foreground">
+                  {memory?.active || "built-in only"}
+                </span>
+              </span>
+              <Link to="/plugins" className="underline">
+                Change in Plugins →
+              </Link>
+              <span className="ml-auto">
+                New credentials:{" "}
+                <span className="font-mono">sonic memory setup</span>
+              </span>
             </div>
+
             <div className="flex flex-wrap items-center gap-3 border-t border-border pt-3">
               <span className="text-xs text-muted-foreground">
                 Built-in files — MEMORY.md:{" "}