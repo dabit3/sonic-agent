heyhudson/hermes-agent

fix(node/nix): consolidate workspace lockfile + update all consumers
Consolidate per-package package-lock.json files into a single root-level
workspace lockfile.  Update all consumers:

- Nix: shared src/npmDeps/npmDepsHash in lib.nix; devshell hook stamps
  package.json paths then runs npm ci from root; individual .nix files
  use mkNpmPassthru attrs instead of per-package fetchNpmDeps.
- Python CLI: new _workspace_root() helper so _tui_need_npm_install,
  _make_tui_argv, _build_web_ui resolve lockfile/node_modules from the
  workspace root.
- Desktop: replace --force-build/mtime heuristic with content-hash build
  stamp (_compute_desktop_content_hash via pathspec).  Remove --force-build
  flag.
- Dockerfile: single root npm install; no per-directory lockfile copies.
- CI: nix-lockfile-fix and osv-scanner reference root package-lock.json;
  apps/dashboard → apps/desktop.
- Tests: new test_tui_npm_install.py; desktop stamp tests in
  test_gui_command.py; updated assertions in test_cmd_update.py,
  test_web_ui_build.py, test_dockerfile_pid1_reaping.py.
- Docs: remove --force-build from desktop flag table.

Deleted: apps/desktop/package-lock.json, ui-tui/package-lock.json,
ui-tui/packages/hermes-ink/package-lock.json, web/package-lock.json.

nix/devShell.nix
@@ -1,13 +1,28 @@
 # nix/devShell.nix — Dev shell that delegates setup to each package
 #
-# Each package in inputsFrom might expose passthru.devShellHook — a bash snippet
-# with stamp-checked setup logic. This file collects and runs them all.
+# Each npm workspace package exposes passthru.packageJsonPath (e.g.
+# "ui-tui/package.json").  This file collects them all and passes the
+# list to mkNpmDevShellHook, which stamps all package.jsons at once,
+# then runs a single `npm i --package-lock-only` if any changed and
+# `npm ci` if the lockfile changed.
 { ... }:
 {
   perSystem =
     { pkgs, self', ... }:
     let
       packages = builtins.attrValues self'.packages;
+      hermesNpmLib = self'.packages.default.passthru.hermesNpmLib;
+      fixLockfilesExe = pkgs.lib.getExe self'.packages.fix-lockfiles;
+
+      # Collect all packageJsonPath values from npm workspace packages.
+      npmPackageJsonPaths = builtins.filter (p: p != null) (
+        map (p: p.passthru.packageJsonPath or null) packages
+      );
+
+      # Non-npm packages may have their own devShellHook (e.g. hermes-agent
+      # stamps pyproject.toml + uv.lock for Python venv setup).
+      nonNpmHooks = map (p: p.passthru.devShellHook or "") packages;
+      combinedNonNpm = pkgs.lib.concatStringsSep "\n" (builtins.filter (h: h != "") nonNpmHooks);
     in
     {
       devShells.default = pkgs.mkShell {
@@ -15,16 +30,12 @@
         packages = with pkgs; [
           uv
         ];
-        shellHook =
-          let
-            hooks = map (p: p.passthru.devShellHook or "") packages;
-            combined = pkgs.lib.concatStringsSep "\n" (builtins.filter (h: h != "") hooks);
-          in
-          ''
-            echo "Hermes Agent dev shell"
-            ${combined}
-            echo "Ready. Run 'hermes' to start."
-          '';
+        shellHook = ''
+          echo "Hermes Agent dev shell"
+          ${combinedNonNpm}
+          ${hermesNpmLib.mkNpmDevShellHook npmPackageJsonPaths fixLockfilesExe}
+          echo "Ready. Run 'hermes' to start."
+        '';
       };
     };
 }

nix/packages.nix
@@ -51,9 +51,7 @@
         web = hermesAgent.hermesWeb;
         desktop = hermesAgent.hermesDesktop;
 
-        fix-lockfiles = hermesAgent.hermesNpmLib.mkFixLockfiles {
-          packages = [ hermesAgent.hermesTui hermesAgent.hermesWeb hermesAgent.hermesDesktop ];
-        };
+        fix-lockfiles = hermesAgent.hermesNpmLib.mkFixLockfiles { attr = "tui"; };
       };
     };
 }

nix/tui.nix
@@ -1,34 +1,32 @@
 # nix/tui.nix — Hermes TUI (Ink/React) compiled with tsc and bundled
 { pkgs, hermesNpmLib, ... }:
 let
-  src = ../ui-tui;
-  npmDeps = pkgs.fetchNpmDeps {
-    inherit src;
-    hash = "sha256-F6/MzZOWc0zhW9mIfnaY+PrllPvJcsA/OdFdEM+NpLY=";
-  };
-
   npm = hermesNpmLib.mkNpmPassthru { folder = "ui-tui"; attr = "tui"; pname = "hermes-tui"; };
 
-  packageJson = builtins.fromJSON (builtins.readFile (src + "/package.json"));
+  packageJson = builtins.fromJSON (builtins.readFile (npm.src + "/ui-tui/package.json"));
   version = packageJson.version;
 in
 pkgs.buildNpmPackage (npm // {
   pname = "hermes-tui";
-  inherit src npmDeps version;
+  inherit version;
 
   doCheck = false;
-  npmFlags = [ "--legacy-peer-deps" ];
+
+  buildPhase = ''
+    # esbuild bundles everything — no need for tsc or vite.
+    # Run from the workspace root where node_modules/ lives.
+    node ui-tui/scripts/build.mjs
+  '';
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out/lib/hermes-tui
-
-    # Single self-contained bundle built by scripts/build.mjs (esbuild).
-    cp -r dist $out/lib/hermes-tui/dist
+    # esbuild writes to ui-tui/dist/ from the source root (no cd).
+    cp -r ui-tui/dist $out/lib/hermes-tui/dist
 
     # package.json kept for "type": "module" resolution on `node dist/entry.js`.
-    cp package.json $out/lib/hermes-tui/
+    cp ui-tui/package.json $out/lib/hermes-tui/
 
     runHook postInstall
   '';

dabit3/sonic-agent

(cherry picked from commit a51a7b9b92b63b5f97afa0fc31c9cd349f04b5ec)

nix/devShell.nix
@@ -1,13 +1,28 @@
 # nix/devShell.nix — Dev shell that delegates setup to each package
 #
-# Each package in inputsFrom might expose passthru.devShellHook — a bash snippet
-# with stamp-checked setup logic. This file collects and runs them all.
+# Each npm workspace package exposes passthru.packageJsonPath (e.g.
+# "ui-tui/package.json").  This file collects them all and passes the
+# list to mkNpmDevShellHook, which stamps all package.jsons at once,
+# then runs a single `npm i --package-lock-only` if any changed and
+# `npm ci` if the lockfile changed.
 { ... }:
 {
   perSystem =
     { pkgs, self', ... }:
     let
       packages = builtins.attrValues self'.packages;
+      sonicNpmLib = self'.packages.default.passthru.sonicNpmLib;
+      fixLockfilesExe = pkgs.lib.getExe self'.packages.fix-lockfiles;
+
+      # Collect all packageJsonPath values from npm workspace packages.
+      npmPackageJsonPaths = builtins.filter (p: p != null) (
+        map (p: p.passthru.packageJsonPath or null) packages
+      );
+
+      # Non-npm packages may have their own devShellHook (e.g. hermes-agent
+      # stamps pyproject.toml + uv.lock for Python venv setup).
+      nonNpmHooks = map (p: p.passthru.devShellHook or "") packages;
+      combinedNonNpm = pkgs.lib.concatStringsSep "\n" (builtins.filter (h: h != "") nonNpmHooks);
     in
     {
       devShells.default = pkgs.mkShell {
@@ -15,16 +30,12 @@
         packages = with pkgs; [
           uv
         ];
-        shellHook =
-          let
-            hooks = map (p: p.passthru.devShellHook or "") packages;
-            combined = pkgs.lib.concatStringsSep "\n" (builtins.filter (h: h != "") hooks);
-          in
-          ''
-            echo "Sonic Agent dev shell"
-            ${combined}
-            echo "Ready. Run 'sonic' to start."
-          '';
+        shellHook = ''
+          echo "Sonic Agent dev shell"
+          ${combinedNonNpm}
+          ${sonicNpmLib.mkNpmDevShellHook npmPackageJsonPaths fixLockfilesExe}
+          echo "Ready. Run 'sonic' to start."
+        '';
       };
     };
 }

nix/packages.nix
@@ -51,9 +51,7 @@
         web = sonicAgent.sonicWeb;
         desktop = sonicAgent.sonicDesktop;
 
-        fix-lockfiles = sonicAgent.sonicNpmLib.mkFixLockfiles {
-          packages = [ sonicAgent.sonicTui sonicAgent.sonicWeb sonicAgent.sonicDesktop ];
-        };
+        fix-lockfiles = sonicAgent.sonicNpmLib.mkFixLockfiles { attr = "tui"; };
       };
     };
 }

nix/tui.nix
@@ -1,34 +1,32 @@
 # nix/tui.nix — Sonic TUI (Ink/React) compiled with tsc and bundled
 { pkgs, sonicNpmLib, ... }:
 let
-  src = ../ui-tui;
-  npmDeps = pkgs.fetchNpmDeps {
-    inherit src;
-    hash = "sha256-qts8wiSNxu9pH/I0pM9Cn/YExxhGcCkqQydFJ8eE934=";
-  };
-
   npm = sonicNpmLib.mkNpmPassthru { folder = "ui-tui"; attr = "tui"; pname = "sonic-tui"; };
 
-  packageJson = builtins.fromJSON (builtins.readFile (src + "/package.json"));
+  packageJson = builtins.fromJSON (builtins.readFile (npm.src + "/ui-tui/package.json"));
   version = packageJson.version;
 in
 pkgs.buildNpmPackage (npm // {
   pname = "sonic-tui";
-  inherit src npmDeps version;
+  inherit version;
 
   doCheck = false;
-  npmFlags = [ "--legacy-peer-deps" ];
+
+  buildPhase = ''
+    # esbuild bundles everything — no need for tsc or vite.
+    # Run from the workspace root where node_modules/ lives.
+    node ui-tui/scripts/build.mjs
+  '';
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out/lib/sonic-tui
-
-    # Single self-contained bundle built by scripts/build.mjs (esbuild).
-    cp -r dist $out/lib/sonic-tui/dist
+    # esbuild writes to ui-tui/dist/ from the source root (no cd).
+    cp -r ui-tui/dist $out/lib/sonic-tui/dist
 
     # package.json kept for "type": "module" resolution on `node dist/entry.js`.
-    cp package.json $out/lib/sonic-tui/
+    cp ui-tui/package.json $out/lib/sonic-tui/
 
     runHook postInstall
   '';