Lexedgeai26/legal-hermes

fix(skills): browse shows full catalog, not first 5000 (#41413)
hermes skills browse capped the hermes-index source at 5000, so it
surfaced ~5.4k of the ~90.7k skills the index actually carries. Raise
the per-source ceiling above catalog size; browse already paginates
client-side and the index is disk-cached, so no extra fetch cost.

hermes_cli/skills_hub.py
@@ -337,12 +337,15 @@ def do_browse(page: int = 1, page_size: int = 20, source: str = "all",
     _TRUST_RANK = {"builtin": 3, "trusted": 2, "community": 1}
     # NOTE: when the centralized index is available, parallel_search_sources
     # skips the external API sources and serves everything from "hermes-index".
-    # That source MUST therefore carry a high limit, or browse silently caps
-    # the entire hub at the default (50) — it shipped that way and surfaced
-    # ~136 of 88k skills. The external-source limits below only apply when the
-    # index is unavailable (offline / first run before the cache populates).
+    # That source MUST therefore carry a limit large enough to cover the whole
+    # catalog, or browse silently caps the hub — it shipped at 50 (surfaced
+    # ~136 of 88k skills), then 5000 (surfaced ~5.4k of 90k). The index is
+    # disk-cached and browse paginates client-side, so a ceiling above the
+    # current catalog size is the right call. The external-source limits below
+    # only apply when the index is unavailable (offline / first run before the
+    # cache populates).
     _PER_SOURCE_LIMIT = {
-        "hermes-index": 5000,
+        "hermes-index": 1000000,
         "official": 200, "skills-sh": 200, "well-known": 50,
         "github": 200, "clawhub": 500, "claude-marketplace": 100,
         "lobehub": 500, "browse-sh": 500,

dabit3/sonic-agent

(cherry picked from commit 1892e22acb8cece06ae68c792eace1f3c85834f2)

sonic_cli/skills_hub.py
@@ -337,12 +337,15 @@ def do_browse(page: int = 1, page_size: int = 20, source: str = "all",
     _TRUST_RANK = {"builtin": 3, "trusted": 2, "community": 1}
     # NOTE: when the centralized index is available, parallel_search_sources
     # skips the external API sources and serves everything from "sonic-index".
-    # That source MUST therefore carry a high limit, or browse silently caps
-    # the entire hub at the default (50) — it shipped that way and surfaced
-    # ~136 of 88k skills. The external-source limits below only apply when the
-    # index is unavailable (offline / first run before the cache populates).
+    # That source MUST therefore carry a limit large enough to cover the whole
+    # catalog, or browse silently caps the hub — it shipped at 50 (surfaced
+    # ~136 of 88k skills), then 5000 (surfaced ~5.4k of 90k). The index is
+    # disk-cached and browse paginates client-side, so a ceiling above the
+    # current catalog size is the right call. The external-source limits below
+    # only apply when the index is unavailable (offline / first run before the
+    # cache populates).
     _PER_SOURCE_LIMIT = {
-        "sonic-index": 5000,
+        "sonic-index": 1000000,
         "official": 200, "skills-sh": 200, "well-known": 50,
         "github": 200, "clawhub": 500, "claude-marketplace": 100,
         "lobehub": 500, "browse-sh": 500,