Lexedgeai26/legal-hermes

docs(windows): fix Get-Command PATH guidance to venv\Scripts\hermes.exe (#40613)
Closes #40464.

Salvaged from #40488; re-verified on main, tightened, tested.

Co-authored-by: gauravsaxena1997 <gauravsaxena1997@users.noreply.github.com>

tests/hermes_cli/test_windows_native_docs.py
@@ -1,0 +1,10 @@
+from pathlib import Path
+
+
+def test_windows_native_install_path_docs_match_installer() -> None:
+    doc = Path("website/docs/user-guide/windows-native.md").read_text()
+    install = Path("scripts/install.ps1").read_text()
+
+    assert "%LOCALAPPDATA%\\hermes\\hermes-agent\\venv\\Scripts" in doc
+    assert "Get-Command hermes        # should print C:\\Users\\<you>\\AppData\\Local\\hermes\\hermes-agent\\venv\\Scripts\\hermes.exe" in doc
+    assert '$hermesBin = "$InstallDir\\venv\\Scripts"' in install

dabit3/sonic-agent

(cherry picked from commit 2aa316ec9c0406d4e8a057f04297215353ba38d0)

tests/sonic_cli/test_windows_native_docs.py
@@ -1,0 +1,10 @@
+from pathlib import Path
+
+
+def test_windows_native_install_path_docs_match_installer() -> None:
+    doc = Path("website/docs/user-guide/windows-native.md").read_text()
+    install = Path("scripts/install.ps1").read_text()
+
+    assert "%LOCALAPPDATA%\\sonic\\sonic-agent\\venv\\Scripts" in doc
+    assert "Get-Command sonic        # should print C:\\Users\\<you>\\AppData\\Local\\sonic\\sonic-agent\\venv\\Scripts\\sonic.exe" in doc
+    assert '$sonicBin = "$InstallDir\\venv\\Scripts"' in install