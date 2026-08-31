Lexedgeai26/legal-hermes

feat(windows): enable dashboard /chat tab via ConPTY (win_pty_bridge) + tests (#42251)
* feat(windows): enable dashboard chat tab via ConPTY (win_pty_bridge)

Add hermes_cli/win_pty_bridge.py — a pywinpty-backed drop-in for
PtyBridge with the same spawn/read/write/resize/close surface — and
wire it into the web_server PTY import block so Windows picks it up
instead of falling back to None.

pywinpty is already a declared win32 dependency (pyproject.toml).
The ConPTY read path runs inside run_in_executor so the event loop
is never blocked. Spawn/read/write/terminate call shapes are taken
directly from tools/process_registry.py which already exercises the
same pywinpty version.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

* docs: remove WSL2-only caveat for dashboard chat tab

The chat pane now works on native Windows via the ConPTY bridge added
in the previous commit.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

* test(windows): cover ConPTY bridge + web_server platform-branched import

Companion to the bridge added in the previous commits.  Verified live on
native Windows 11 (pywinpty 2.0.15) against `hermes dashboard`'s
`/api/pty` WebSocket: the spawned `hermes --tui` (node entry.js) renders
through ConPTY, resize escapes reach `setwinsize`, and closing the WS
reaps both the node child and the pywinpty agent with zero orphans.

tests/hermes_cli/test_win_pty_bridge.py
  Mirrors the layout of the existing POSIX test_pty_bridge.py:
  spawn/io/resize/close/env coverage against cmd.exe and python -c,
  plus the cross-platform fallback surface (PtyUnavailableError, the
  off-Windows `spawn -> raises PtyUnavailableError` guard, and the
  load-bearing _clamp() helper that protects setwinsize from garbage
  winsize values out of xterm.js).

tests/hermes_cli/test_web_server_pty_import.py
  Asserts that web_server.PtyBridge resolves to WinPtyBridge on win32
  and to the POSIX PtyBridge on POSIX, that PtyUnavailableError is the
  matching class on

hermes_cli/web_server.py
@@ -8288,20 +8288,32 @@ async def get_models_analytics(days: int = 30):
 # though uvicorn binds to 127.0.0.1.
 # ---------------------------------------------------------------------------
 
-# PTY bridge is POSIX-only (depends on fcntl/termios/ptyprocess).  On native
-# Windows the import raises; catch and leave PtyBridge=None so the rest of
-# the dashboard (sessions, jobs, metrics, config editor) still loads and the
-# /api/pty endpoint cleanly refuses with a WSL-suggested message.
-try:
-    from hermes_cli.pty_bridge import PtyBridge, PtyUnavailableError
-    _PTY_BRIDGE_AVAILABLE = True
-except ImportError as _pty_import_err:  # pragma: no cover - Windows-only path
-    PtyBridge = None  # type: ignore[assignment]
-    _PTY_BRIDGE_AVAILABLE = False
+# PTY bridge: POSIX uses pty_bridge (fcntl/termios/ptyprocess); native Windows
+# uses win_pty_bridge (pywinpty/ConPTY, already a declared dependency).  Both
+# expose the same public surface — spawn/read/write/resize/close/is_available —
+# so the /api/pty WebSocket handler needs no platform guards.
+if sys.platform.startswith("win"):
+    try:
+        from hermes_cli.win_pty_bridge import WinPtyBridge as PtyBridge, PtyUnavailableError
+        _PTY_BRIDGE_AVAILABLE = True
+    except ImportError:  # pragma: no cover - pywinpty missing
+        PtyBridge = None  # type: ignore[assignment]
+        _PTY_BRIDGE_AVAILABLE = False
 
-    class PtyUnavailableError(RuntimeError):  # type: ignore[no-redef]
-        """Stub on platforms where pty_bridge can't be imported."""
-        pass
+        class PtyUnavailableError(RuntimeError):  # type: ignore[no-redef]
+            """Stub when win_pty_bridge cannot be imported."""
+            pass
+else:
+    try:
+        from hermes_cli.pty_bridge import PtyBridge, PtyUnavailableError
+        _PTY_BRIDGE_AVAILABLE = True
+    except ImportError:  # pragma: no cover - dev env without ptyprocess
+        PtyBridge = None  # type: ignore[assignment]
+        _PTY_BRIDGE_AVAILABLE = False
+
+        class PtyUnavailableError(RuntimeError):  # type: ignore[no-redef]
+            """Stub on platforms where pty_bridge can't be imported."""
+            pass
 
 _RESIZE_RE = re.compile(rb"\x1b\[RESIZE:(\d+);(\d+)\]")
 _PTY_READ_CHUNK_TIMEOUT = 0.2

hermes_cli/win_pty_bridge.py
@@ -1,0 +1,179 @@
+"""Windows ConPTY bridge for the `hermes dashboard` chat tab.
+
+Drop-in counterpart to ``hermes_cli.pty_bridge.PtyBridge`` for native
+Windows. Mirrors the exact public surface the ``/api/pty`` WebSocket
+handler in ``hermes_cli.web_server`` consumes: ``spawn``, ``read``,
+``write``, ``resize``, ``close``, ``is_available``, plus the
+``PtyUnavailableError`` type.
+
+Backed by ``pywinpty`` (already a declared win32 dependency in
+pyproject.toml) instead of ``ptyprocess``/``fcntl``/``termios``, none of
+which exist on native Windows. The read/write/terminate calls here match
+the working winpty usage already shipping in ``tools/process_registry.py``.
+"""
+
+from __future__ import annotations
+
+import os
+import sys
+import time
+from typing import Optional, Sequence
+
+try:
+    from winpty import PtyProcess  # type: ignore
+    _PTY_AVAILABLE = sys.platform.startswith("win")
+except ImportError:  # pragma: no cover - non-Windows or pywinpty missing
+    PtyProcess = None  # type: ignore
+    _PTY_AVAILABLE = False
+
+
+__all__ = ["WinPtyBridge", "PtyUnavailableError"]
+
+
+# Same clamp ceiling as the POSIX bridge: a broken winsize probe must never
+# reach the resize call. ConPTY tolerates large values better than ioctl,
+# but we keep parity to avoid layout surprises.
+_MIN_DIMENSION = 1
+_MAX_COLS = 2000
+_MAX_ROWS = 1000
+
+
+def _clamp(value: int, maximum: int) -> int:
+    try:
+        n = int(value)
+    except (TypeError, ValueError, OverflowError):
+        return _MIN_DIMENSION
+    if n < _MIN_DIMENSION:
+        return _MIN_DIMENSION
+    if n > maximum:
+        return maximum
+    return n
+
+
+class PtyUnavailableError(RuntimeError):
+    """Raised when a PTY cannot be created on this platform."""
+
+
+class WinPtyBridge:
+    """pywinpty-backed bridge with the same interface as ``PtyBridge``.
+
+    ``web_server`` calls :meth:`read` inside ``run_in_executor``, so a
+    blocking/polling read here never stalls the event loop. ConPTY exposes
+    no selectable fd, so we poll with a short sleep instead of ``select``.
+    """
+
+    def __init__(self, proc: "PtyProcess") -> None:  # type: ignore[name-defined]
+        self._proc = proc
+        self._closed = False
+
+    # -- lifecycle --------------------------------------------------------
+
+    @classmethod
+    def is_available(cls) -> bool:
+        return bool(_PTY_AVAILABLE)
+
+    @classmethod
+    def spawn(
+        cls,
+        argv: Sequence[str],
+        *,
+        cwd: Optional[str] = None,
+        env: Optional[dict] = None,
+        cols: int = 80,
+        rows: int = 24,
+    ) -> "WinPtyBridge":
+        if not _PTY_AVAILABLE:
+            if PtyProcess is None:
+                raise PtyUnavailableError(
+                    "pywinpty is not installed. Install with: pip install pywinpty"
+                )
+            raise PtyUnavailableError("ConPTY is unavailable on this platform.")
+        spawn_env = (os.environ.copy() if env is None else dict(env))
+        if not spawn_env.get("TERM"):
+            spawn_env["TERM"] = "xterm-256color"
+        # pywinpty mirrors ptyprocess: dimensions=(rows, cols).
+        # This call shape is the one already used in tools/process_registry.py.
+        proc = PtyProcess.spawn(  # type: ignore[union-attr]
+            list(argv),
+            cwd=cwd,
+            env=spawn_env,
+            dimensions=(rows, cols),
+        )
+        return cls(proc)
+
+    @property
+    def pid(self) -> int:
+        return int(self._proc.pid)
+
+    def is_alive(self) -> bool:
+        if self._closed:
+            return False
+        try:
+            return bool(self._proc.isalive())
+        except Exception:
+            return False
+
+    # -- I/O --------------------------------------------------------------
+
+    def read(self, timeout: float = 0.2) -> Optional[bytes]:
+        """Up to 64 KiB of child output.
+
+        Returns bytes, ``b""`` when nothing is available this tick, or
+        ``None`` once the child has exited (EOF).
+        """
+        if self._closed:
+            return None
+        try:
+            data = self._proc.read(65536)  # pywinpty returns str
+        except EOFError:
+            return None
+        except Exception:
+            return None
+        if not data:
+            # No fd to select on; poll politely so the executor thread
+            # doesn't pin a core while the TUI is idle.
+            time.sleep(min(timeout, 0.02))
+            return b""
+        if isinstance(data, bytes):
+            return data
+        # NOTE: pywinpty decodes internally, so a multibyte UTF-8 sequence
+        # can in theory split across reads. xterm.js tolerates the rare
+        # replacement char; this is the one fidelity tradeoff vs the POSIX
+        # raw-fd path.
+        return data.encode("utf-8", errors="replace")
+
+    def write(self, data: bytes) -> None:
+        if self._closed or not data:
+            return
+        try:
+            # The dashboard sends raw keystroke bytes; pywinpty.write wants text.
+            self._proc.write(data.decode("utf-8", errors="replace"))
+        except Exception:
+            return
+
+    def resize(self, cols: int, rows: int) -> None:
+        if self._closed:
+            return
+        cols = _clamp(cols, _MAX_COLS)
+        rows = _clamp(rows, _MAX_ROWS)
+        try:
+            self._proc.setwinsize(rows, cols)  # pywinpty: (rows, cols)
+        except Exception:
+            pass
+
+    # -- teardown ---------------------------------------------------------
+
+    def close(self) -> None:
+        if self._closed:
+            return
+        self._closed = True
+        try:
+            self._proc.terminate(force=True)
+        except Exception:
+            pass
+
+    def __enter__(self) -> "WinPtyBridge":
+        return self
+
+    def __exit__(self, *_exc) -> None:
+        self.close()

tests/hermes_cli/test_web_server_pty_import.py
@@ -1,0 +1,83 @@
+"""Test the platform-branched PTY bridge import in hermes_cli.web_server.
+
+The /api/pty WebSocket handler in web_server.py picks its bridge at import
+time via ``sys.platform.startswith("win")`` — Windows gets the ConPTY
+backend, POSIX gets the fcntl/termios one.  Both branches must:
+
+  1. Expose ``PtyBridge`` as the bridge class (or None) and
+     ``PtyUnavailableError`` as an exception class.
+  2. Set ``_PTY_BRIDGE_AVAILABLE`` correctly.
+  3. Never raise at import time when the platform-native dependency is
+     missing — the dashboard's non-chat tabs must keep loading.
+
+This test asserts the live state on whichever platform CI runs on, plus a
+source-text check confirming the branch shape is preserved so a future
+refactor can't accidentally collapse it back to a POSIX-only import.
+"""
+
+from __future__ import annotations
+
+import sys
+
+import pytest
+
+from hermes_cli import web_server
+
+
+def test_web_server_exposes_pty_bridge_symbols():
+    """The two symbols /api/pty consumes must always exist."""
+    assert hasattr(web_server, "PtyBridge")
+    assert hasattr(web_server, "PtyUnavailableError")
+    assert hasattr(web_server, "_PTY_BRIDGE_AVAILABLE")
+    # PtyUnavailableError is always an exception class — either the real
+    # one from the platform bridge, or the local fallback class.
+    assert isinstance(web_server.PtyUnavailableError, type)
+    assert issubclass(web_server.PtyUnavailableError, BaseException)
+
+
+@pytest.mark.skipif(not sys.platform.startswith("win"), reason="Windows-only")
+def test_web_server_uses_win_pty_bridge_on_windows():
+    """On native Windows, web_server.PtyBridge must be the ConPTY backend."""
+    from hermes_cli.win_pty_bridge import WinPtyBridge
+
+    assert web_server.PtyBridge is WinPtyBridge
+    assert web_server._PTY_BRIDGE_AVAILABLE is True
+    # And the error class must be the one from the same module so isinstance
+    # checks in /api/pty's spawn fallback path actually work.
+    from hermes_cli.win_pty_bridge import PtyUnavailableError as WinErr
+
+    assert web_server.PtyUnavailableError is WinErr
+
+
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="POSIX-only")
+def test_web_server_uses_posix_pty_bridge_on_posix():
+    """On POSIX, the bridge must be the fcntl/termios PtyBridge."""
+    from hermes_cli.pty_bridge import PtyBridge as PosixBridge
+    from hermes_cli.pty_bridge import PtyUnavailableError as PosixErr
+
+    assert web_server.PtyBridge is PosixBridge
+    assert web_server._PTY_BRIDGE_AVAILABLE is True
+    assert web_server.PtyUnavailableError is PosixErr
+
+
+def test_pty_bridge_import_block_is_platform_branched():
+    """Source-level guard: a future refactor must not collapse the branch
+    back to a single POSIX import.  Reads web_server.py directly so this
+    fails the same way on every OS — the runtime symbol checks above can
+    pass even when the branch shape is wrong on the current platform."""
+    src = pytest.importorskip("inspect").getsource(web_server)
+    # The shape we expect (from PR #39913):
+    #
+    #   if sys.platform.startswith("win"):
+    #       try:
+    #           from hermes_cli.win_pty_bridge import WinPtyBridge as PtyBridge, ...
+    #       except ImportError:
+    #           PtyBridge = None
+    #           ...
+    #   else:
+    #       try:
+    #           from hermes_cli.pty_bridge import PtyBridge, PtyUnavailableError
+    #       ...
+    assert 'sys.platform.startswith("win")' in src or "sys.platform.startswith('win')" in src
+    assert "from hermes_cli.win_pty_bridge import" in src
+    assert "from hermes_cli.pty_bridge import" in src

dabit3/sonic-agent

(cherry picked from commit abcf996b1f749a647a1b213653a80d1eee58f6d1)

sonic_cli/web_server.py
@@ -8288,20 +8288,32 @@ async def get_models_analytics(days: int = 30):
 # though uvicorn binds to 127.0.0.1.
 # ---------------------------------------------------------------------------
 
-# PTY bridge is POSIX-only (depends on fcntl/termios/ptyprocess).  On native
-# Windows the import raises; catch and leave PtyBridge=None so the rest of
-# the dashboard (sessions, jobs, metrics, config editor) still loads and the
-# /api/pty endpoint cleanly refuses with a WSL-suggested message.
-try:
-    from sonic_cli.pty_bridge import PtyBridge, PtyUnavailableError
-    _PTY_BRIDGE_AVAILABLE = True
-except ImportError as _pty_import_err:  # pragma: no cover - Windows-only path
-    PtyBridge = None  # type: ignore[assignment]
-    _PTY_BRIDGE_AVAILABLE = False
+# PTY bridge: POSIX uses pty_bridge (fcntl/termios/ptyprocess); native Windows
+# uses win_pty_bridge (pywinpty/ConPTY, already a declared dependency).  Both
+# expose the same public surface — spawn/read/write/resize/close/is_available —
+# so the /api/pty WebSocket handler needs no platform guards.
+if sys.platform.startswith("win"):
+    try:
+        from sonic_cli.win_pty_bridge import WinPtyBridge as PtyBridge, PtyUnavailableError
+        _PTY_BRIDGE_AVAILABLE = True
+    except ImportError:  # pragma: no cover - pywinpty missing
+        PtyBridge = None  # type: ignore[assignment]
+        _PTY_BRIDGE_AVAILABLE = False
 
-    class PtyUnavailableError(RuntimeError):  # type: ignore[no-redef]
-        """Stub on platforms where pty_bridge can't be imported."""
-        pass
+        class PtyUnavailableError(RuntimeError):  # type: ignore[no-redef]
+            """Stub when win_pty_bridge cannot be imported."""
+            pass
+else:
+    try:
+        from hermes_cli.pty_bridge import PtyBridge, PtyUnavailableError
+        _PTY_BRIDGE_AVAILABLE = True
+    except ImportError:  # pragma: no cover - dev env without ptyprocess
+        PtyBridge = None  # type: ignore[assignment]
+        _PTY_BRIDGE_AVAILABLE = False
+
+        class PtyUnavailableError(RuntimeError):  # type: ignore[no-redef]
+            """Stub on platforms where pty_bridge can't be imported."""
+            pass
 
 _RESIZE_RE = re.compile(rb"\x1b\[RESIZE:(\d+);(\d+)\]")
 _PTY_READ_CHUNK_TIMEOUT = 0.2

sonic_cli/win_pty_bridge.py
@@ -1,0 +1,179 @@
+"""Windows ConPTY bridge for the `sonic dashboard` chat tab.
+
+Drop-in counterpart to ``sonic_cli.pty_bridge.PtyBridge`` for native
+Windows. Mirrors the exact public surface the ``/api/pty`` WebSocket
+handler in ``sonic_cli.web_server`` consumes: ``spawn``, ``read``,
+``write``, ``resize``, ``close``, ``is_available``, plus the
+``PtyUnavailableError`` type.
+
+Backed by ``pywinpty`` (already a declared win32 dependency in
+pyproject.toml) instead of ``ptyprocess``/``fcntl``/``termios``, none of
+which exist on native Windows. The read/write/terminate calls here match
+the working winpty usage already shipping in ``tools/process_registry.py``.
+"""
+
+from __future__ import annotations
+
+import os
+import sys
+import time
+from typing import Optional, Sequence
+
+try:
+    from winpty import PtyProcess  # type: ignore
+    _PTY_AVAILABLE = sys.platform.startswith("win")
+except ImportError:  # pragma: no cover - non-Windows or pywinpty missing
+    PtyProcess = None  # type: ignore
+    _PTY_AVAILABLE = False
+
+
+__all__ = ["WinPtyBridge", "PtyUnavailableError"]
+
+
+# Same clamp ceiling as the POSIX bridge: a broken winsize probe must never
+# reach the resize call. ConPTY tolerates large values better than ioctl,
+# but we keep parity to avoid layout surprises.
+_MIN_DIMENSION = 1
+_MAX_COLS = 2000
+_MAX_ROWS = 1000
+
+
+def _clamp(value: int, maximum: int) -> int:
+    try:
+        n = int(value)
+    except (TypeError, ValueError, OverflowError):
+        return _MIN_DIMENSION
+    if n < _MIN_DIMENSION:
+        return _MIN_DIMENSION
+    if n > maximum:
+        return maximum
+    return n
+
+
+class PtyUnavailableError(RuntimeError):
+    """Raised when a PTY cannot be created on this platform."""
+
+
+class WinPtyBridge:
+    """pywinpty-backed bridge with the same interface as ``PtyBridge``.
+
+    ``web_server`` calls :meth:`read` inside ``run_in_executor``, so a
+    blocking/polling read here never stalls the event loop. ConPTY exposes
+    no selectable fd, so we poll with a short sleep instead of ``select``.
+    """
+
+    def __init__(self, proc: "PtyProcess") -> None:  # type: ignore[name-defined]
+        self._proc = proc
+        self._closed = False
+
+    # -- lifecycle --------------------------------------------------------
+
+    @classmethod
+    def is_available(cls) -> bool:
+        return bool(_PTY_AVAILABLE)
+
+    @classmethod
+    def spawn(
+        cls,
+        argv: Sequence[str],
+        *,
+        cwd: Optional[str] = None,
+        env: Optional[dict] = None,
+        cols: int = 80,
+        rows: int = 24,
+    ) -> "WinPtyBridge":
+        if not _PTY_AVAILABLE:
+            if PtyProcess is None:
+                raise PtyUnavailableError(
+                    "pywinpty is not installed. Install with: pip install pywinpty"
+                )
+            raise PtyUnavailableError("ConPTY is unavailable on this platform.")
+        spawn_env = (os.environ.copy() if env is None else dict(env))
+        if not spawn_env.get("TERM"):
+            spawn_env["TERM"] = "xterm-256color"
+        # pywinpty mirrors ptyprocess: dimensions=(rows, cols).
+        # This call shape is the one already used in tools/process_registry.py.
+        proc = PtyProcess.spawn(  # type: ignore[union-attr]
+            list(argv),
+            cwd=cwd,
+            env=spawn_env,
+            dimensions=(rows, cols),
+        )
+        return cls(proc)
+
+    @property
+    def pid(self) -> int:
+        return int(self._proc.pid)
+
+    def is_alive(self) -> bool:
+        if self._closed:
+            return False
+        try:
+            return bool(self._proc.isalive())
+        except Exception:
+            return False
+
+    # -- I/O --------------------------------------------------------------
+
+    def read(self, timeout: float = 0.2) -> Optional[bytes]:
+        """Up to 64 KiB of child output.
+
+        Returns bytes, ``b""`` when nothing is available this tick, or
+        ``None`` once the child has exited (EOF).
+        """
+        if self._closed:
+            return None
+        try:
+            data = self._proc.read(65536)  # pywinpty returns str
+        except EOFError:
+            return None
+        except Exception:
+            return None
+        if not data:
+            # No fd to select on; poll politely so the executor thread
+            # doesn't pin a core while the TUI is idle.
+            time.sleep(min(timeout, 0.02))
+            return b""
+        if isinstance(data, bytes):
+            return data
+        # NOTE: pywinpty decodes internally, so a multibyte UTF-8 sequence
+        # can in theory split across reads. xterm.js tolerates the rare
+        # replacement char; this is the one fidelity tradeoff vs the POSIX
+        # raw-fd path.
+        return data.encode("utf-8", errors="replace")
+
+    def write(self, data: bytes) -> None:
+        if self._closed or not data:
+            return
+        try:
+            # The dashboard sends raw keystroke bytes; pywinpty.write wants text.
+            self._proc.write(data.decode("utf-8", errors="replace"))
+        except Exception:
+            return
+
+    def resize(self, cols: int, rows: int) -> None:
+        if self._closed:
+            return
+        cols = _clamp(cols, _MAX_COLS)
+        rows = _clamp(rows, _MAX_ROWS)
+        try:
+            self._proc.setwinsize(rows, cols)  # pywinpty: (rows, cols)
+        except Exception:
+            pass
+
+    # -- teardown ---------------------------------------------------------
+
+    def close(self) -> None:
+        if self._closed:
+            return
+        self._closed = True
+        try:
+            self._proc.terminate(force=True)
+        except Exception:
+            pass
+
+    def __enter__(self) -> "WinPtyBridge":
+        return self
+
+    def __exit__(self, *_exc) -> None:
+        self.close()

tests/sonic_cli/test_web_server_pty_import.py
@@ -1,0 +1,83 @@
+"""Test the platform-branched PTY bridge import in sonic_cli.web_server.
+
+The /api/pty WebSocket handler in web_server.py picks its bridge at import
+time via ``sys.platform.startswith("win")`` — Windows gets the ConPTY
+backend, POSIX gets the fcntl/termios one.  Both branches must:
+
+  1. Expose ``PtyBridge`` as the bridge class (or None) and
+     ``PtyUnavailableError`` as an exception class.
+  2. Set ``_PTY_BRIDGE_AVAILABLE`` correctly.
+  3. Never raise at import time when the platform-native dependency is
+     missing — the dashboard's non-chat tabs must keep loading.
+
+This test asserts the live state on whichever platform CI runs on, plus a
+source-text check confirming the branch shape is preserved so a future
+refactor can't accidentally collapse it back to a POSIX-only import.
+"""
+
+from __future__ import annotations
+
+import sys
+
+import pytest
+
+from sonic_cli import web_server
+
+
+def test_web_server_exposes_pty_bridge_symbols():
+    """The two symbols /api/pty consumes must always exist."""
+    assert hasattr(web_server, "PtyBridge")
+    assert hasattr(web_server, "PtyUnavailableError")
+    assert hasattr(web_server, "_PTY_BRIDGE_AVAILABLE")
+    # PtyUnavailableError is always an exception class — either the real
+    # one from the platform bridge, or the local fallback class.
+    assert isinstance(web_server.PtyUnavailableError, type)
+    assert issubclass(web_server.PtyUnavailableError, BaseException)
+
+
+@pytest.mark.skipif(not sys.platform.startswith("win"), reason="Windows-only")
+def test_web_server_uses_win_pty_bridge_on_windows():
+    """On native Windows, web_server.PtyBridge must be the ConPTY backend."""
+    from sonic_cli.win_pty_bridge import WinPtyBridge
+
+    assert web_server.PtyBridge is WinPtyBridge
+    assert web_server._PTY_BRIDGE_AVAILABLE is True
+    # And the error class must be the one from the same module so isinstance
+    # checks in /api/pty's spawn fallback path actually work.
+    from sonic_cli.win_pty_bridge import PtyUnavailableError as WinErr
+
+    assert web_server.PtyUnavailableError is WinErr
+
+
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="POSIX-only")
+def test_web_server_uses_posix_pty_bridge_on_posix():
+    """On POSIX, the bridge must be the fcntl/termios PtyBridge."""
+    from sonic_cli.pty_bridge import PtyBridge as PosixBridge
+    from sonic_cli.pty_bridge import PtyUnavailableError as PosixErr
+
+    assert web_server.PtyBridge is PosixBridge
+    assert web_server._PTY_BRIDGE_AVAILABLE is True
+    assert web_server.PtyUnavailableError is PosixErr
+
+
+def test_pty_bridge_import_block_is_platform_branched():
+    """Source-level guard: a future refactor must not collapse the branch
+    back to a single POSIX import.  Reads web_server.py directly so this
+    fails the same way on every OS — the runtime symbol checks above can
+    pass even when the branch shape is wrong on the current platform."""
+    src = pytest.importorskip("inspect").getsource(web_server)
+    # The shape we expect (from PR #39913):
+    #
+    #   if sys.platform.startswith("win"):
+    #       try:
+    #           from sonic_cli.win_pty_bridge import WinPtyBridge as PtyBridge, ...
+    #       except ImportError:
+    #           PtyBridge = None
+    #           ...
+    #   else:
+    #       try:
+    #           from sonic_cli.pty_bridge import PtyBridge, PtyUnavailableError
+    #       ...
+    assert 'sys.platform.startswith("win")' in src or "sys.platform.startswith('win')" in src
+    assert "from sonic_cli.win_pty_bridge import" in src
+    assert "from sonic_cli.pty_bridge import" in src