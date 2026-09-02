AnandaAnugrahHandyanto/savarez_agent

fix(skills): widen HERMES_HOME-aware .env resolution to all sibling skills
Follow-up to the GitHub-skills fix: the same hardcoded ~/.hermes/.env
pattern existed across other bundled and optional skills. Under the
official Docker setup (HERMES_HOME=/opt/data, subprocess HOME=/opt/data/home)
those paths point at a nonexistent file.

- kanban-video-orchestrator setup.sh.tmpl + docs: resolve via
  ${HERMES_HOME:-$HOME/.hermes}/.env in check_key()
- telephony.py / canvas_api.py / hyperliquid_client.py: error and
  save messages now report the real resolved env path instead of a
  hardcoded literal (path resolution itself was already correct)
- godmode SKILL.md: load_dotenv snippet resolves via HERMES_HOME
- watch_github.py + ~20 SKILL.md prose mentions: document the env file
  as ${HERMES_HOME:-~/.hermes}/.env so Docker users edit the right file

optional-skills/blockchain/hyperliquid/scripts/hyperliquid_client.py
@@ -115,7 +115,7 @@ def _resolve_user(user: Optional[str]) -> str:
 
     sys.exit(
         "Missing Hyperliquid address. Pass <address> explicitly or set "
-        f"{DEFAULT_USER_ENV} in your environment or ~/.hermes/.env."
+        f"{DEFAULT_USER_ENV} in your environment or {_hermes_home() / '.env'}."
     )
 
 

optional-skills/devops/watchers/scripts/watch_github.py
@@ -8,7 +8,8 @@ Usage (via cron with --no-agent):
       --script "$HERMES_HOME/skills/devops/watchers/scripts/watch_github.py" \\
       --script-args "--name hermes-issues --repo NousResearch/hermes-agent --scope issues"
 
-Set GITHUB_TOKEN (or GH_TOKEN) in ~/.hermes/.env to avoid the 60 req/hr
+Set GITHUB_TOKEN (or GH_TOKEN) in the Hermes .env file
+(``${HERMES_HOME:-~/.hermes}/.env``) to avoid the 60 req/hr
 anonymous rate limit.
 
 Scopes: issues | pulls | releases | commits.  Or pass --search QUERY to

optional-skills/productivity/canvas/scripts/canvas_api.py
@@ -28,9 +28,12 @@ def _check_config():
     if not CANVAS_BASE_URL:
         missing.append("CANVAS_BASE_URL")
     if missing:
+        hermes_env = os.path.join(
+            os.environ.get("HERMES_HOME", os.path.expanduser("~/.hermes")), ".env"
+        )
         print(
             f"Missing required environment variables: {', '.join(missing)}\n"
-            "Set them in ~/.hermes/.env or export them in your shell.\n"
+            f"Set them in {hermes_env} or export them in your shell.\n"
             "See the canvas skill SKILL.md for setup instructions.",
             file=sys.stderr,
         )

optional-skills/productivity/telephony/scripts/telephony.py
@@ -2,7 +2,7 @@
 """Telephony helper for the Hermes optional telephony skill.
 
 Capabilities:
-- Persist telephony provider credentials to ~/.hermes/.env
+- Persist telephony provider credentials to the Hermes .env file ($HERMES_HOME/.env)
 - Search for, buy, and remember Twilio phone numbers
 - Make direct Twilio calls (TwiML <Say> or <Play>)
 - Send SMS / MMS via Twilio
@@ -286,7 +286,7 @@ def _twilio_creds() -> tuple[str, str]:
     if not sid or not token:
         raise TelephonyError(
             "Twilio credentials are not configured. Use 'save-twilio' or set "
-            "TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN in ~/.hermes/.env."
+            f"TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN in {_env_path()}."
         )
     return sid, token
 
@@ -420,7 +420,7 @@ def _resolve_twilio_number(identifier: str | None = None) -> OwnedTwilioNumber:
 
     raise TelephonyError(
         "No default Twilio phone number is set. Use 'twilio-buy --save-env', "
-        "'twilio-set-default', or set TWILIO_PHONE_NUMBER in ~/.hermes/.env."
+        f"'twilio-set-default', or set TWILIO_PHONE_NUMBER in {_env_path()}."
     )
 
 
@@ -756,7 +756,7 @@ def _vapi_import_twilio_number(
     api_key = _vapi_api_key()
     if not api_key:
         raise TelephonyError(
-            "Vapi is not configured. Use 'save-vapi' or set VAPI_API_KEY in ~/.hermes/.env first."
+            f"Vapi is not configured. Use 'save-vapi' or set VAPI_API_KEY in {_env_path()} first."
         )
     owned = _resolve_twilio_number(phone_identifier)
     sid, token = _twilio_creds()
@@ -803,7 +803,7 @@ def _bland_call(
     api_key = _bland_api_key()
     if not api_key:
         raise TelephonyError(
-            "Bland.ai is not configured. Use 'save-bland' or set BLAND_API_KEY in ~/.hermes/.env."
+            f"Bland.ai is not configured. Use 'save-bland' or set BLAND_API_KEY in {_env_path()}."
         )
     normalized = _normalize_phone(phone_number)
     if voice is None:
@@ -881,13 +881,13 @@ def _vapi_call(
     api_key = _vapi_api_key()
     if not api_key:
         raise TelephonyError(
-            "Vapi is not configured. Use 'save-vapi' or set VAPI_API_KEY in ~/.hermes/.env."
+            f"Vapi is not configured. Use 'save-vapi' or set VAPI_API_KEY in {_env_path()}."
         )
     phone_number_id = _vapi_phone_number_id()
     if not phone_number_id:
         raise TelephonyError(
             "No Vapi phone number id is configured. Import an owned Twilio number with "
-            "'vapi-import-twilio --save-env' or set VAPI_PHONE_NUMBER_ID in ~/.hermes/.env."
+            f"'vapi-import-twilio --save-env' or set VAPI_PHONE_NUMBER_ID in {_env_path()}."
         )
     normalized = _normalize_phone(phone_number)
     voice_provider = _env_or_config(
@@ -1091,7 +1091,7 @@ def save_twilio(account_sid: str, auth_token: str, phone_number: str = "", phone
         "provider": "twilio",
         "saved_env_keys": sorted(updates),
         "env_path": str(env_file),
-        "message": "Twilio credentials saved to ~/.hermes/.env.",
+        "message": f"Twilio credentials saved to {env_file}.",
     }
     if phone_number:
         result.update(_remember_twilio_number(phone_number=updates["TWILIO_PHONE_NUMBER"], phone_sid=phone_sid.strip(), save_env=False))
@@ -1111,7 +1111,7 @@ def save_bland(api_key: str, voice: str = BLAND_DEFAULT_VOICE) -> dict[str, Any]
         "provider": "bland",
         "saved_env_keys": ["BLAND_API_KEY", "BLAND_DEFAULT_VOICE", "PHONE_PROVIDER"],
         "env_path": str(env_file),
-        "message": "Bland.ai configuration saved to ~/.hermes/.env.",
+        "message": f"Bland.ai configuration saved to {env_file}.",
     }
 
 
@@ -1138,7 +1138,7 @@ def save_vapi(
         "provider": "vapi",
         "saved_env_keys": sorted(updates),
         "env_path": str(env_file),
-        "message": "Vapi configuration saved to ~/.hermes/.env.",
+        "message": f"Vapi configuration saved to {env_file}.",
     }
     if phone_number_id:
         result.update(_remember_vapi_number(phone_number_id=phone_number_id.strip(), save_env=False))
@@ -1151,17 +1151,17 @@ def _build_parser() -> argparse.ArgumentParser:
 
     sub.add_parser("diagnose", help="Show saved telephony state and provider readiness")
 
-    p = sub.add_parser("save-twilio", help="Save Twilio credentials to ~/.hermes/.env")
+    p = sub.add_parser("save-twilio", help="Save Twilio credentials to the Hermes .env file")
     p.add_argument("account_sid")
     p.add_argument("auth_token")
     p.add_argument("--phone-number", default="")
     p.add_argument("--phone-sid", default="")
 
-    p = sub.add_parser("save-bland", help="Save Bland.ai settings to ~/.hermes/.env")
+    p = sub.add_parser("save-bland", help="Save Bland.ai settings to the Hermes .env file")
     p.add_argument("api_key")
     p.add_argument("--voice", default=BLAND_DEFAULT_VOICE)
 
-    p = sub.add_parser("save-vapi", help="Save Vapi settings to ~/.hermes/.env")
+    p = sub.add_parser("save-vapi", help="Save Vapi settings to the Hermes .env file")
     p.add_argument("api_key")
     p.add_argument("--phone-number-id", default="")
     p.add_argument("--voice-provider", default=VAPI_DEFAULT_VOICE_PROVIDER)
@@ -1312,7 +1312,7 @@ def _dispatch(args: argparse.Namespace) -> dict[str, Any]:
             )
         raise TelephonyError(
             f"Unsupported AI call provider '{provider}'. Use --provider bland or --provider vapi, "
-            "or set PHONE_PROVIDER in ~/.hermes/.env."
+            f"or set PHONE_PROVIDER in {_env_path()}."
         )
     if cmd == "ai-status":
         provider = (args.provider or _ai_provider()).lower().strip()
@@ -1322,7 +1322,7 @@ def _dispatch(args: argparse.Namespace) -> dict[str, Any]:
             return _bland_status(args.call_id, analyze=args.analyze or None)
         raise TelephonyError(
             f"Unsupported AI call provider '{provider}'. Use --provider bland or --provider vapi, "
-            "or set PHONE_PROVIDER in ~/.hermes/.env."
+            f"or set PHONE_PROVIDER in {_env_path()}."
         )
     raise TelephonyError(f"Unknown command: {cmd}")
 

dabit3/sonic-agent

(cherry picked from commit d1383a6b1450c6c139720b1b01f8b99cc130453f)

optional-skills/blockchain/hyperliquid/scripts/hyperliquid_client.py
@@ -115,7 +115,7 @@ def _resolve_user(user: Optional[str]) -> str:
 
     sys.exit(
         "Missing Hyperliquid address. Pass <address> explicitly or set "
-        f"{DEFAULT_USER_ENV} in your environment or ~/.sonic/.env."
+        f"{DEFAULT_USER_ENV} in your environment or {_sonic_home() / '.env'}."
     )
 
 

optional-skills/devops/watchers/scripts/watch_github.py
@@ -8,7 +8,8 @@ Usage (via cron with --no-agent):
       --script "$SONIC_HOME/skills/devops/watchers/scripts/watch_github.py" \\
       --script-args "--name sonic-issues --repo dabit3/sonic-agent --scope issues"
 
-Set GITHUB_TOKEN (or GH_TOKEN) in ~/.sonic/.env to avoid the 60 req/hr
+Set GITHUB_TOKEN (or GH_TOKEN) in the Sonic .env file
+(``${SONIC_HOME:-~/.sonic}/.env``) to avoid the 60 req/hr
 anonymous rate limit.
 
 Scopes: issues | pulls | releases | commits.  Or pass --search QUERY to

optional-skills/productivity/canvas/scripts/canvas_api.py
@@ -28,9 +28,12 @@ def _check_config():
     if not CANVAS_BASE_URL:
         missing.append("CANVAS_BASE_URL")
     if missing:
+        hermes_env = os.path.join(
+            os.environ.get("HERMES_HOME", os.path.expanduser("~/.hermes")), ".env"
+        )
         print(
             f"Missing required environment variables: {', '.join(missing)}\n"
-            "Set them in ~/.sonic/.env or export them in your shell.\n"
+            f"Set them in {sonic_env} or export them in your shell.\n"
             "See the canvas skill SKILL.md for setup instructions.",
             file=sys.stderr,
         )

optional-skills/productivity/telephony/scripts/telephony.py
@@ -2,7 +2,7 @@
 """Telephony helper for the Sonic optional telephony skill.
 
 Capabilities:
-- Persist telephony provider credentials to ~/.sonic/.env
+- Persist telephony provider credentials to the Sonic .env file ($SONIC_HOME/.env)
 - Search for, buy, and remember Twilio phone numbers
 - Make direct Twilio calls (TwiML <Say> or <Play>)
 - Send SMS / MMS via Twilio
@@ -286,7 +286,7 @@ def _twilio_creds() -> tuple[str, str]:
     if not sid or not token:
         raise TelephonyError(
             "Twilio credentials are not configured. Use 'save-twilio' or set "
-            "TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN in ~/.sonic/.env."
+            f"TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN in {_env_path()}."
         )
     return sid, token
 
@@ -420,7 +420,7 @@ def _resolve_twilio_number(identifier: str | None = None) -> OwnedTwilioNumber:
 
     raise TelephonyError(
         "No default Twilio phone number is set. Use 'twilio-buy --save-env', "
-        "'twilio-set-default', or set TWILIO_PHONE_NUMBER in ~/.sonic/.env."
+        f"'twilio-set-default', or set TWILIO_PHONE_NUMBER in {_env_path()}."
     )
 
 
@@ -756,7 +756,7 @@ def _vapi_import_twilio_number(
     api_key = _vapi_api_key()
     if not api_key:
         raise TelephonyError(
-            "Vapi is not configured. Use 'save-vapi' or set VAPI_API_KEY in ~/.sonic/.env first."
+            f"Vapi is not configured. Use 'save-vapi' or set VAPI_API_KEY in {_env_path()} first."
         )
     owned = _resolve_twilio_number(phone_identifier)
     sid, token = _twilio_creds()
@@ -803,7 +803,7 @@ def _bland_call(
     api_key = _bland_api_key()
     if not api_key:
         raise TelephonyError(
-            "Bland.ai is not configured. Use 'save-bland' or set BLAND_API_KEY in ~/.sonic/.env."
+            f"Bland.ai is not configured. Use 'save-bland' or set BLAND_API_KEY in {_env_path()}."
         )
     normalized = _normalize_phone(phone_number)
     if voice is None:
@@ -881,13 +881,13 @@ def _vapi_call(
     api_key = _vapi_api_key()
     if not api_key:
         raise TelephonyError(
-            "Vapi is not configured. Use 'save-vapi' or set VAPI_API_KEY in ~/.sonic/.env."
+            f"Vapi is not configured. Use 'save-vapi' or set VAPI_API_KEY in {_env_path()}."
         )
     phone_number_id = _vapi_phone_number_id()
     if not phone_number_id:
         raise TelephonyError(
             "No Vapi phone number id is configured. Import an owned Twilio number with "
-            "'vapi-import-twilio --save-env' or set VAPI_PHONE_NUMBER_ID in ~/.sonic/.env."
+            f"'vapi-import-twilio --save-env' or set VAPI_PHONE_NUMBER_ID in {_env_path()}."
         )
     normalized = _normalize_phone(phone_number)
     voice_provider = _env_or_config(
@@ -1091,7 +1091,7 @@ def save_twilio(account_sid: str, auth_token: str, phone_number: str = "", phone
         "provider": "twilio",
         "saved_env_keys": sorted(updates),
         "env_path": str(env_file),
-        "message": "Twilio credentials saved to ~/.sonic/.env.",
+        "message": f"Twilio credentials saved to {env_file}.",
     }
     if phone_number:
         result.update(_remember_twilio_number(phone_number=updates["TWILIO_PHONE_NUMBER"], phone_sid=phone_sid.strip(), save_env=False))
@@ -1111,7 +1111,7 @@ def save_bland(api_key: str, voice: str = BLAND_DEFAULT_VOICE) -> dict[str, Any]
         "provider": "bland",
         "saved_env_keys": ["BLAND_API_KEY", "BLAND_DEFAULT_VOICE", "PHONE_PROVIDER"],
         "env_path": str(env_file),
-        "message": "Bland.ai configuration saved to ~/.sonic/.env.",
+        "message": f"Bland.ai configuration saved to {env_file}.",
     }
 
 
@@ -1138,7 +1138,7 @@ def save_vapi(
         "provider": "vapi",
         "saved_env_keys": sorted(updates),
         "env_path": str(env_file),
-        "message": "Vapi configuration saved to ~/.sonic/.env.",
+        "message": f"Vapi configuration saved to {env_file}.",
     }
     if phone_number_id:
         result.update(_remember_vapi_number(phone_number_id=phone_number_id.strip(), save_env=False))
@@ -1151,17 +1151,17 @@ def _build_parser() -> argparse.ArgumentParser:
 
     sub.add_parser("diagnose", help="Show saved telephony state and provider readiness")
 
-    p = sub.add_parser("save-twilio", help="Save Twilio credentials to ~/.sonic/.env")
+    p = sub.add_parser("save-twilio", help="Save Twilio credentials to the Sonic .env file")
     p.add_argument("account_sid")
     p.add_argument("auth_token")
     p.add_argument("--phone-number", default="")
     p.add_argument("--phone-sid", default="")
 
-    p = sub.add_parser("save-bland", help="Save Bland.ai settings to ~/.sonic/.env")
+    p = sub.add_parser("save-bland", help="Save Bland.ai settings to the Sonic .env file")
     p.add_argument("api_key")
     p.add_argument("--voice", default=BLAND_DEFAULT_VOICE)
 
-    p = sub.add_parser("save-vapi", help="Save Vapi settings to ~/.sonic/.env")
+    p = sub.add_parser("save-vapi", help="Save Vapi settings to the Sonic .env file")
     p.add_argument("api_key")
     p.add_argument("--phone-number-id", default="")
     p.add_argument("--voice-provider", default=VAPI_DEFAULT_VOICE_PROVIDER)
@@ -1312,7 +1312,7 @@ def _dispatch(args: argparse.Namespace) -> dict[str, Any]:
             )
         raise TelephonyError(
             f"Unsupported AI call provider '{provider}'. Use --provider bland or --provider vapi, "
-            "or set PHONE_PROVIDER in ~/.sonic/.env."
+            f"or set PHONE_PROVIDER in {_env_path()}."
         )
     if cmd == "ai-status":
         provider = (args.provider or _ai_provider()).lower().strip()
@@ -1322,7 +1322,7 @@ def _dispatch(args: argparse.Namespace) -> dict[str, Any]:
             return _bland_status(args.call_id, analyze=args.analyze or None)
         raise TelephonyError(
             f"Unsupported AI call provider '{provider}'. Use --provider bland or --provider vapi, "
-            "or set PHONE_PROVIDER in ~/.sonic/.env."
+            f"or set PHONE_PROVIDER in {_env_path()}."
         )
     raise TelephonyError(f"Unknown command: {cmd}")
 