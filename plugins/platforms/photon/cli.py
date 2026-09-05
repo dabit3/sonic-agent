Lexedgeai26/legal-hermes

fix(photon): correct gateway start command (#45566)

plugins/platforms/photon/cli.py
@@ -274,7 +274,7 @@ def _cmd_setup(args: argparse.Namespace) -> int:
 
     print()
     print("✓ Photon setup complete.")
-    print("  Start the gateway:  hermes gateway start --platform photon")
+    print("  Start the gateway:  hermes gateway start")
     return 0
 
 

dabit3/sonic-agent

(cherry picked from commit 2681c5a12d8dbd8e27aa89949228fd2da04d6244)

plugins/platforms/photon/cli.py
@@ -274,7 +274,7 @@ def _cmd_setup(args: argparse.Namespace) -> int:
 
     print()
     print("✓ Photon setup complete.")
-    print("  Start the gateway:  sonic gateway start --platform photon")
+    print("  Start the gateway:  sonic gateway start")
     return 0
 
 