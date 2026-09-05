Lexedgeai26/legal-hermes

fix(slack): make video attachments available to agents (#45512)

tests/tools/test_credential_files.py
@@ -378,12 +378,14 @@ class TestCacheDirectoryMounts:
         hermes_home.mkdir()
         (hermes_home / "cache" / "documents").mkdir(parents=True)
         (hermes_home / "cache" / "audio").mkdir(parents=True)
+        (hermes_home / "cache" / "videos").mkdir(parents=True)
         monkeypatch.setenv("HERMES_HOME", str(hermes_home))
 
         mounts = get_cache_directory_mounts()
         paths = {m["container_path"] for m in mounts}
         assert "/root/.hermes/cache/documents" in paths
         assert "/root/.hermes/cache/audio" in paths
+        assert "/root/.hermes/cache/videos" in paths
 
     def test_skips_nonexistent_dirs(self, tmp_path, monkeypatch):
         """Dirs that don't exist on disk are not returned."""

dabit3/sonic-agent

(cherry picked from commit 2a5dc0ef3df433a36abed9ee544ea067d807c438)

tests/tools/test_credential_files.py
@@ -378,12 +378,14 @@ class TestCacheDirectoryMounts:
         sonic_home.mkdir()
         (sonic_home / "cache" / "documents").mkdir(parents=True)
         (sonic_home / "cache" / "audio").mkdir(parents=True)
+        (sonic_home / "cache" / "videos").mkdir(parents=True)
         monkeypatch.setenv("SONIC_HOME", str(sonic_home))
 
         mounts = get_cache_directory_mounts()
         paths = {m["container_path"] for m in mounts}
         assert "/root/.sonic/cache/documents" in paths
         assert "/root/.sonic/cache/audio" in paths
+        assert "/root/.sonic/cache/videos" in paths
 
     def test_skips_nonexistent_dirs(self, tmp_path, monkeypatch):
         """Dirs that don't exist on disk are not returned."""