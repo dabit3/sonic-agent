NousResearch/hermes-agent

fix: make profile subprocess HOME policy explicit

hermes_cli/tips.py
@@ -298,7 +298,7 @@ TIPS = [
     "Any website can expose skills via /.well-known/skills/index.json — the skills hub discovers them automatically.",
     "The skills audit log at ~/.hermes/skills/.hub/audit.log tracks every install and removal operation.",
     "Stale git worktrees are auto-cleaned: 24-72h old with no unpushed commits get pruned on startup.",
-    "Each profile gets its own subprocess HOME at HERMES_HOME/home/ — isolated git, ssh, npm, gh configs.",
+    "Profiles scope Hermes state via HERMES_HOME; host tool subprocesses keep your real HOME unless terminal.home_mode is profile.",
     "HERMES_HOME_MODE env var (octal, e.g. 0701) sets custom directory permissions for web server traversal.",
     "Container mode: place .container-mode in HERMES_HOME and the host CLI auto-execs into the container.",
     "Ctrl+C has 5 priority tiers: cancel recording → cancel prompts → cancel picker → interrupt agent → exit.",

tests/agent/test_copilot_acp_client.py
@@ -174,12 +174,13 @@ def _fake_popen_capture(captured):
     return _fake
 
 
-def test_run_prompt_prefers_profile_home_when_available(monkeypatch, tmp_path):
+def test_run_prompt_preserves_real_home_when_profile_home_available(monkeypatch, tmp_path):
     hermes_home = tmp_path / "hermes"
-    profile_home = hermes_home / "home"
-    profile_home.mkdir(parents=True)
+    (hermes_home / "home").mkdir(parents=True)
+    real_home = tmp_path / "real-home"
+    real_home.mkdir()
 
-    monkeypatch.delenv("HOME", raising=False)
+    monkeypatch.setenv("HOME", str(real_home))
     monkeypatch.setenv("HERMES_HOME", str(hermes_home))
 
     captured = {}
@@ -189,7 +190,8 @@ def test_run_prompt_prefers_profile_home_when_available(monkeypatch, tmp_path):
         with pytest.raises(RuntimeError, match="Could not start Copilot ACP command"):
             client._run_prompt("hello", timeout_seconds=1)
 
-    assert captured["kwargs"]["env"]["HOME"] == str(profile_home)
+    assert captured["kwargs"]["env"]["HOME"] == str(real_home)
+    assert captured["kwargs"]["env"]["HERMES_REAL_HOME"] == str(real_home)
 
 
 def test_run_prompt_passes_home_when_parent_env_is_clean(monkeypatch, tmp_path):

tools/code_execution_tool.py
@@ -1270,14 +1270,8 @@ def execute_code(
             child_env["TZ"] = _tz_name
         child_env.pop("HERMES_TIMEZONE", None)
 
-        # Per-profile HOME isolation: redirect system tool configs into
-        # {HERMES_HOME}/home/ when that directory exists.
-        from hermes_constants import get_subprocess_home
-        _profile_home = get_subprocess_home()
-        if _profile_home:
-            child_env["HOME"] = _profile_home
-            from hermes_constants import get_real_home
-            child_env["HERMES_REAL_HOME"] = get_real_home()
+        from hermes_constants import apply_subprocess_home_env
+        apply_subprocess_home_env(child_env)
 
         # Resolve interpreter + CWD based on execute_code mode.
         #   - strict : today's behavior (sys.executable + tmpdir CWD).

tools/environments/local.py
@@ -227,13 +227,8 @@ def _sanitize_subprocess_env(base_env: dict | None, extra_env: dict | None = Non
 
     _inject_context_hermes_home(sanitized)
 
-    # Per-profile HOME isolation for background processes (same as _make_run_env).
-    from hermes_constants import get_subprocess_home
-    _profile_home = get_subprocess_home()
-    if _profile_home:
-        sanitized["HOME"] = _profile_home
-        from hermes_constants import get_real_home
-        sanitized["HERMES_REAL_HOME"] = get_real_home()
+    from hermes_constants import apply_subprocess_home_env
+    apply_subprocess_home_env(sanitized)
 
     return sanitized
 
@@ -389,15 +384,8 @@ def _make_run_env(env: dict) -> dict:
 
     _inject_context_hermes_home(run_env)
 
-    # Per-profile HOME isolation: redirect system tool configs (git, ssh, gh,
-    # npm …) into {HERMES_HOME}/home/ when that directory exists.  Only the
-    # subprocess sees the override — the Python process keeps the real HOME.
-    from hermes_constants import get_subprocess_home
-    _profile_home = get_subprocess_home()
-    if _profile_home:
-        run_env["HOME"] = _profile_home
-        from hermes_constants import get_real_home
-        run_env["HERMES_REAL_HOME"] = get_real_home()
+    from hermes_constants import apply_subprocess_home_env
+    apply_subprocess_home_env(run_env)
 
     # Inject ContextVar-based session vars into subprocess env.
     # ContextVars don't propagate to child processes, so we bridge them here.

dabit3/sonic-agent

(cherry picked from commit 723c2331bd236fdb9bbc0d6e6f85a1b7704e4aa1)

sonic_cli/tips.py
@@ -298,7 +298,7 @@ TIPS = [
     "Any website can expose skills via /.well-known/skills/index.json — the skills hub discovers them automatically.",
     "The skills audit log at ~/.sonic/skills/.hub/audit.log tracks every install and removal operation.",
     "Stale git worktrees are auto-cleaned: 24-72h old with no unpushed commits get pruned on startup.",
-    "Each profile gets its own subprocess HOME at SONIC_HOME/home/ — isolated git, ssh, npm, gh configs.",
+    "Profiles scope Sonic state via SONIC_HOME; host tool subprocesses keep your real HOME unless terminal.home_mode is profile.",
     "SONIC_HOME_MODE env var (octal, e.g. 0701) sets custom directory permissions for web server traversal.",
     "Container mode: place .container-mode in SONIC_HOME and the host CLI auto-execs into the container.",
     "Ctrl+C has 5 priority tiers: cancel recording → cancel prompts → cancel picker → interrupt agent → exit.",

tests/agent/test_copilot_acp_client.py
@@ -174,12 +174,13 @@ def _fake_popen_capture(captured):
     return _fake
 
 
-def test_run_prompt_prefers_profile_home_when_available(monkeypatch, tmp_path):
+def test_run_prompt_preserves_real_home_when_profile_home_available(monkeypatch, tmp_path):
     sonic_home = tmp_path / "sonic"
-    profile_home = sonic_home / "home"
-    profile_home.mkdir(parents=True)
+    (sonic_home / "home").mkdir(parents=True)
+    real_home = tmp_path / "real-home"
+    real_home.mkdir()
 
-    monkeypatch.delenv("HOME", raising=False)
+    monkeypatch.setenv("HOME", str(real_home))
     monkeypatch.setenv("SONIC_HOME", str(sonic_home))
 
     captured = {}
@@ -189,7 +190,8 @@ def test_run_prompt_prefers_profile_home_when_available(monkeypatch, tmp_path):
         with pytest.raises(RuntimeError, match="Could not start Copilot ACP command"):
             client._run_prompt("hello", timeout_seconds=1)
 
-    assert captured["kwargs"]["env"]["HOME"] == str(profile_home)
+    assert captured["kwargs"]["env"]["HOME"] == str(real_home)
+    assert captured["kwargs"]["env"]["HERMES_REAL_HOME"] == str(real_home)
 
 
 def test_run_prompt_passes_home_when_parent_env_is_clean(monkeypatch, tmp_path):

tools/code_execution_tool.py
@@ -1270,14 +1270,8 @@ def execute_code(
             child_env["TZ"] = _tz_name
         child_env.pop("SONIC_TIMEZONE", None)
 
-        # Per-profile HOME isolation: redirect system tool configs into
-        # {SONIC_HOME}/home/ when that directory exists.
-        from sonic_constants import get_subprocess_home
-        _profile_home = get_subprocess_home()
-        if _profile_home:
-            child_env["HOME"] = _profile_home
-            from hermes_constants import get_real_home
-            child_env["HERMES_REAL_HOME"] = get_real_home()
+        from sonic_constants import apply_subprocess_home_env
+        apply_subprocess_home_env(child_env)
 
         # Resolve interpreter + CWD based on execute_code mode.
         #   - strict : today's behavior (sys.executable + tmpdir CWD).

tools/environments/local.py
@@ -227,13 +227,8 @@ def _sanitize_subprocess_env(base_env: dict | None, extra_env: dict | None = Non
 
     _inject_context_sonic_home(sanitized)
 
-    # Per-profile HOME isolation for background processes (same as _make_run_env).
-    from sonic_constants import get_subprocess_home
-    _profile_home = get_subprocess_home()
-    if _profile_home:
-        sanitized["HOME"] = _profile_home
-        from hermes_constants import get_real_home
-        sanitized["HERMES_REAL_HOME"] = get_real_home()
+    from sonic_constants import apply_subprocess_home_env
+    apply_subprocess_home_env(sanitized)
 
     return sanitized
 
@@ -389,15 +384,8 @@ def _make_run_env(env: dict) -> dict:
 
     _inject_context_sonic_home(run_env)
 
-    # Per-profile HOME isolation: redirect system tool configs (git, ssh, gh,
-    # npm …) into {SONIC_HOME}/home/ when that directory exists.  Only the
-    # subprocess sees the override — the Python process keeps the real HOME.
-    from sonic_constants import get_subprocess_home
-    _profile_home = get_subprocess_home()
-    if _profile_home:
-        run_env["HOME"] = _profile_home
-        from hermes_constants import get_real_home
-        run_env["HERMES_REAL_HOME"] = get_real_home()
+    from sonic_constants import apply_subprocess_home_env
+    apply_subprocess_home_env(run_env)
 
     # Inject ContextVar-based session vars into subprocess env.
     # ContextVars don't propagate to child processes, so we bridge them here.