NousResearch/hermes-agent

feat(xai): add grok-composer-2.5-fast to xAI OAuth model picker
The model is callable via xAI OAuth but omitted from models.dev and
/v1/models listings. Merge it into the curated xAI catalog so it appears
in `hermes model` without requiring a custom model name.

tests/hermes_cli/test_xai_curated_models.py
@@ -1,0 +1,14 @@
+"""Regression tests for xAI curated model list (OAuth picker)."""
+
+from hermes_cli.models import _PROVIDER_MODELS, provider_model_ids
+
+
+def test_xai_oauth_includes_grok_composer_2_5_fast():
+    models = provider_model_ids("xai-oauth")
+    assert "grok-composer-2.5-fast" in models
+
+
+def test_grok_composer_slots_after_grok_build():
+    models = _PROVIDER_MODELS["xai-oauth"]
+    assert models[0] == "grok-build-0.1"
+    assert models[1] == "grok-composer-2.5-fast"

dabit3/sonic-agent

(cherry picked from commit eaddeaf2e6b91e5b6543f4db46b969d153c067d0)

tests/sonic_cli/test_xai_curated_models.py
@@ -1,0 +1,14 @@
+"""Regression tests for xAI curated model list (OAuth picker)."""
+
+from sonic_cli.models import _PROVIDER_MODELS, provider_model_ids
+
+
+def test_xai_oauth_includes_grok_composer_2_5_fast():
+    models = provider_model_ids("xai-oauth")
+    assert "grok-composer-2.5-fast" in models
+
+
+def test_grok_composer_slots_after_grok_build():
+    models = _PROVIDER_MODELS["xai-oauth"]
+    assert models[0] == "grok-build-0.1"
+    assert models[1] == "grok-composer-2.5-fast"