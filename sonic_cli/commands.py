Lexedgeai26/legal-hermes

feat(billing): /billing terminal billing — interactive TUI + CLI client (#45449)
* feat(billing): nous_billing http client + BillingState core (phase 2b)

Phase 2b terminal-billing client foundation:
- hermes_cli/nous_billing.py: typed client for the 4 /api/billing/* endpoints
  (state/charge/poll/auto-top-up). Raises typed errors (BillingScopeRequired,
  BillingRateLimited, BillingAuthError) mapped from the live-verified contract;
  fail-open is the caller's job. Idempotency-Key enforced client-side.
- agent/billing_view.py: surface-agnostic BillingState core + Decimal money
  parsing (server emits decimal strings, not 2dp), fail-open builder,
  idempotency-key gen, custom-amount validation.
- 51 unit tests (decimal parse/format, payload tiering, error->exception
  matrix, fail-open, amount validation).

Plan: docs/plans/2026-06-13-001-phase-2b-terminal-billing-tui-plan.md

* feat(billing): billing:manage scope + lazy step-up re-auth (phase 2b)

- NOUS_BILLING_MANAGE_SCOPE constant.
- nous_token_has_billing_scope(): split-based scope check (no false-positive
  substring match).
- step_up_nous_billing_scope(): re-runs the device flow requesting
  billing:manage, reusing the held credential's portal/inference URLs + client_id
  (so a preview stays a preview), persists like _login_nous but WITHOUT the model
  picker. Returns True iff the minted token carries the scope (False when NAS
  silently downscopes a non-admin / unticked grant).

Lazy step-up (plan D-A): normal login path unchanged; 403 insufficient_scope
from a billing call triggers this. 7 unit tests.

* feat(billing): billing JSON-RPC methods for the TUI (phase 2b)

billing.state / charge / charge_status / auto_reload / step_up in
tui_gateway/server.py. Return STRUCTURED success envelopes (result.ok +
result.error=<code>) rather than JSON-RPC-level errors, so the Ink rpc() promise
always resolves and the TUI branches on the typed billing error code
(insufficient_scope, rate_limited, no_payment_method, …) to 

hermes_cli/commands.py
@@ -215,6 +215,7 @@ COMMAND_REGISTRY: list[CommandDef] = [
                gateway_only=True),
     CommandDef("usage", "Show token usage and rate limits for the current session", "Info"),
     CommandDef("credits", "Show Nous credit balance and top up", "Info"),
+    CommandDef("billing", "Manage Nous terminal billing — buy credits, auto-reload, limits", "Info"),
     CommandDef("insights", "Show usage insights and analytics", "Info",
                args_hint="[days]"),
     CommandDef("platforms", "Show gateway/messaging platform status", "Info",
@@ -1053,8 +1054,9 @@ _SLACK_PRIORITY_ALIASES = ("btw", "bg")
 # the telegram-parity test reads it so an entry here is a deliberate
 # "Slack-via-/hermes" decision, not a silent clamp.
 #   - credits: the billing/top-up surface; reached via /hermes credits on Slack.
+#   - billing: the terminal-billing surface (buy/auto-reload/limit); /hermes billing.
 #   - debug: the log/report upload surface; reached via /hermes debug on Slack.
-_SLACK_VIA_HERMES_ONLY = frozenset({"credits", "debug"})
+_SLACK_VIA_HERMES_ONLY = frozenset({"credits", "billing", "debug"})
 
 
 def _sanitize_slack_name(raw: str) -> str:

tests/hermes_cli/test_billing_cli.py
@@ -1,0 +1,136 @@
+"""Tests for the /billing CLI handler (cli.py::_show_billing).
+
+Focus on the non-interactive (no live prompt_toolkit app) path — the same
+discipline as the /credits non-interactive test: it must render text, never
+invoke the modal (which would read the slash-worker's JSON-RPC stdin and hang).
+Plus role/kill-switch gating and logged-out handling.
+"""
+
+from __future__ import annotations
+
+from decimal import Decimal
+
+import pytest
+
+import agent.billing_view as bv
+from agent.billing_view import BillingState, CardInfo, MonthlyCap
+from cli import HermesCLI
+
+
+@pytest.fixture
+def cli():
+    obj = HermesCLI.__new__(HermesCLI)  # bypass __init__ (no full app needed)
+    obj._app = None  # non-interactive: forces the text path
+    return obj
+
+
+def _boom_modal(*a, **kw):
+    raise AssertionError("modal must NOT be called in non-interactive mode")
+
+
+def test_billing_logged_out(cli, monkeypatch, capsys):
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: BillingState(logged_in=False))
+    cli._show_billing("/billing")
+    out = capsys.readouterr().out
+    assert "Not logged into Nous Portal" in out
+    assert "hermes portal" in out
+
+
+def test_billing_overview_non_interactive_renders_text_not_modal(cli, monkeypatch, capsys):
+    monkeypatch.setattr(HermesCLI, "_prompt_text_input_modal", _boom_modal, raising=False)
+    state = BillingState(
+        logged_in=True,
+        org_name="Acme",
+        role="OWNER",
+        balance_usd=Decimal("142.5"),
+        cli_billing_enabled=True,
+        charge_presets=(Decimal("100"),),
+        monthly_cap=MonthlyCap(limit_usd=Decimal("1000"), spent_this_month_usd=Decimal("180"),
+                               is_default_ceiling=True),
+        portal_url="https://portal/billing?topup=open",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    cli._show_billing("/billing")
+    out = capsys.readouterr().out
+    assert "Usage credits" in out
+    assert "$142.50" in out
+    assert "$180 of $1000 used (default ceiling)" in out
+    # New design: a spend bar with a percentage on the overview.
+    assert "%" in out and ("█" in out or "░" in out)
+    # ZERO sub-commands: no /billing buy|auto-reload|limit advertising.
+    assert "/billing buy" not in out
+    assert "Actions:" not in out
+    # Non-interactive funnels to the portal (the URL is the affordance).
+    assert "Manage on portal:" in out
+
+
+def test_billing_member_cannot_charge(cli, monkeypatch, capsys):
+    state = BillingState(
+        logged_in=True, role="MEMBER", balance_usd=Decimal("10"),
+        cli_billing_enabled=True, portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    cli._show_billing("/billing")
+    out = capsys.readouterr().out
+    assert "require an org admin/owner" in out
+
+
+def test_billing_killswitch_off_blocks(cli, monkeypatch, capsys):
+    state = BillingState(
+        logged_in=True, role="OWNER", balance_usd=Decimal("10"),
+        cli_billing_enabled=False, portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    cli._show_billing("/billing")
+    out = capsys.readouterr().out
+    assert "turned off for this org" in out
+
+
+def test_billing_limit_screen_readonly(cli, monkeypatch, capsys):
+    state = BillingState(
+        logged_in=True, role="OWNER", cli_billing_enabled=True,
+        monthly_cap=MonthlyCap(limit_usd=Decimal("1000"), spent_this_month_usd=Decimal("250"),
+                               is_default_ceiling=True),
+        portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    # ZERO sub-commands: the limit screen is reached via the menu, never a
+    # sub-command — call it directly the way the overview menu would.
+    cli._billing_limit_screen(state)
+    out = capsys.readouterr().out
+    assert "Monthly spend limit" in out
+    assert "$250 of $1000 used" in out
+    assert "read-only" in out
+
+
+def test_billing_sub_arg_ignored_opens_overview(cli, monkeypatch, capsys):
+    # A stray sub-arg must NOT error and must NOT dispatch to a sub-screen —
+    # it just opens the overview (spec §0.4: zero sub-commands).
+    monkeypatch.setattr(HermesCLI, "_prompt_text_input_modal", _boom_modal, raising=False)
+    state = BillingState(
+        logged_in=True, role="OWNER", balance_usd=Decimal("142.5"),
+        cli_billing_enabled=True, charge_presets=(Decimal("25"),),
+        portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    cli._show_billing("/billing buy")  # arg is ignored
+    out = capsys.readouterr().out
+    assert "Usage credits" in out  # overview, NOT the buy screen
+    assert "Buy usage credits" not in out
+
+
+def test_billing_buy_non_interactive_defers_to_portal(cli, monkeypatch, capsys):
+    monkeypatch.setattr(HermesCLI, "_prompt_text_input_modal", _boom_modal, raising=False)
+    state = BillingState(
+        logged_in=True, role="OWNER", cli_billing_enabled=True,
+        charge_presets=(Decimal("25"), Decimal("50"), Decimal("100")),
+        card=CardInfo(brand="visa", last4="4242"),
+        portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    # Reached via the menu in real use; non-interactively it defers to the portal.
+    cli._billing_buy_flow(state)
+    out = capsys.readouterr().out
+    assert "Buy usage credits" in out
+    assert "$25" in out and "$50" in out and "$100" in out
+    assert "interactive CLI" in out  # defers; no charge attempted non-interactively

tests/hermes_cli/test_billing_portal_url.py
@@ -1,0 +1,53 @@
+"""Portal-URL resolution for Phase 2b billing errors (nous_billing).
+
+The server emits ``portalUrl`` relative by design (``/billing?topup=open``); the
+client must resolve it against the active portal base so deep-links are clickable
+on whatever deployment (preview / staging / prod) the user is pointed at.
+"""
+
+from __future__ import annotations
+
+import pytest
+
+from hermes_cli.nous_billing import (
+    BillingError,
+    _absolutize_portal_url,
+    _raise_for_error,
+)
+
+
+@pytest.fixture
+def _preview(monkeypatch):
+    monkeypatch.setenv("HERMES_PORTAL_BASE_URL", "https://nas-pr-412.nousresearch.wtf")
+
+
+def test_absolutize_resolves_relative(_preview):
+    assert (
+        _absolutize_portal_url("/billing?topup=open")
+        == "https://nas-pr-412.nousresearch.wtf/billing?topup=open"
+    )
+
+
+def test_absolutize_leaves_absolute_unchanged(_preview):
+    # Idempotent: an already-absolute URL must NOT be double-prefixed.
+    url = "https://other.example/billing?topup=open"
+    assert _absolutize_portal_url(url) == url
+
+
+def test_absolutize_passthrough_empty(_preview):
+    assert _absolutize_portal_url(None) is None
+    assert _absolutize_portal_url("") == ""
+
+
+def test_raise_for_error_attaches_absolute_portal_url(_preview):
+    # The 403 no_payment_method envelope carries a RELATIVE portalUrl; the raised
+    # BillingError must expose it as ABSOLUTE so CLI + TUI render a clickable link.
+    with pytest.raises(BillingError) as exc_info:
+        _raise_for_error(
+            403,
+            {"error": "no_payment_method", "portalUrl": "/billing?topup=open"},
+        )
+    assert (
+        exc_info.value.portal_url
+        == "https://nas-pr-412.nousresearch.wtf/billing?topup=open"
+    )

dabit3/sonic-agent

sonic_cli/commands.py
@@ -215,6 +215,7 @@ COMMAND_REGISTRY: list[CommandDef] = [
                gateway_only=True),
     CommandDef("usage", "Show token usage and rate limits for the current session", "Info"),
     CommandDef("credits", "Show Nous credit balance and top up", "Info"),
+    CommandDef("billing", "Manage Nous terminal billing — buy credits, auto-reload, limits", "Info"),
     CommandDef("insights", "Show usage insights and analytics", "Info",
                args_hint="[days]"),
     CommandDef("platforms", "Show gateway/messaging platform status", "Info",
@@ -1053,8 +1054,9 @@ _SLACK_PRIORITY_ALIASES = ("btw", "bg")
 # the telegram-parity test reads it so an entry here is a deliberate
 # "Slack-via-/sonic" decision, not a silent clamp.
 #   - credits: the billing/top-up surface; reached via /sonic credits on Slack.
+#   - billing: the terminal-billing surface (buy/auto-reload/limit); /sonic billing.
 #   - debug: the log/report upload surface; reached via /sonic debug on Slack.
-_SLACK_VIA_SONIC_ONLY = frozenset({"credits", "debug"})
+_SLACK_VIA_SONIC_ONLY = frozenset({"credits", "billing", "debug"})
 
 
 def _sanitize_slack_name(raw: str) -> str:

tests/sonic_cli/test_billing_cli.py
@@ -1,0 +1,136 @@
+"""Tests for the /billing CLI handler (cli.py::_show_billing).
+
+Focus on the non-interactive (no live prompt_toolkit app) path — the same
+discipline as the /credits non-interactive test: it must render text, never
+invoke the modal (which would read the slash-worker's JSON-RPC stdin and hang).
+Plus role/kill-switch gating and logged-out handling.
+"""
+
+from __future__ import annotations
+
+from decimal import Decimal
+
+import pytest
+
+import agent.billing_view as bv
+from agent.billing_view import BillingState, CardInfo, MonthlyCap
+from cli import SonicCLI
+
+
+@pytest.fixture
+def cli():
+    obj = SonicCLI.__new__(SonicCLI)  # bypass __init__ (no full app needed)
+    obj._app = None  # non-interactive: forces the text path
+    return obj
+
+
+def _boom_modal(*a, **kw):
+    raise AssertionError("modal must NOT be called in non-interactive mode")
+
+
+def test_billing_logged_out(cli, monkeypatch, capsys):
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: BillingState(logged_in=False))
+    cli._show_billing("/billing")
+    out = capsys.readouterr().out
+    assert "Not logged into Nous Portal" in out
+    assert "sonic portal" in out
+
+
+def test_billing_overview_non_interactive_renders_text_not_modal(cli, monkeypatch, capsys):
+    monkeypatch.setattr(SonicCLI, "_prompt_text_input_modal", _boom_modal, raising=False)
+    state = BillingState(
+        logged_in=True,
+        org_name="Acme",
+        role="OWNER",
+        balance_usd=Decimal("142.5"),
+        cli_billing_enabled=True,
+        charge_presets=(Decimal("100"),),
+        monthly_cap=MonthlyCap(limit_usd=Decimal("1000"), spent_this_month_usd=Decimal("180"),
+                               is_default_ceiling=True),
+        portal_url="https://portal/billing?topup=open",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    cli._show_billing("/billing")
+    out = capsys.readouterr().out
+    assert "Usage credits" in out
+    assert "$142.50" in out
+    assert "$180 of $1000 used (default ceiling)" in out
+    # New design: a spend bar with a percentage on the overview.
+    assert "%" in out and ("█" in out or "░" in out)
+    # ZERO sub-commands: no /billing buy|auto-reload|limit advertising.
+    assert "/billing buy" not in out
+    assert "Actions:" not in out
+    # Non-interactive funnels to the portal (the URL is the affordance).
+    assert "Manage on portal:" in out
+
+
+def test_billing_member_cannot_charge(cli, monkeypatch, capsys):
+    state = BillingState(
+        logged_in=True, role="MEMBER", balance_usd=Decimal("10"),
+        cli_billing_enabled=True, portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    cli._show_billing("/billing")
+    out = capsys.readouterr().out
+    assert "require an org admin/owner" in out
+
+
+def test_billing_killswitch_off_blocks(cli, monkeypatch, capsys):
+    state = BillingState(
+        logged_in=True, role="OWNER", balance_usd=Decimal("10"),
+        cli_billing_enabled=False, portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    cli._show_billing("/billing")
+    out = capsys.readouterr().out
+    assert "turned off for this org" in out
+
+
+def test_billing_limit_screen_readonly(cli, monkeypatch, capsys):
+    state = BillingState(
+        logged_in=True, role="OWNER", cli_billing_enabled=True,
+        monthly_cap=MonthlyCap(limit_usd=Decimal("1000"), spent_this_month_usd=Decimal("250"),
+                               is_default_ceiling=True),
+        portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    # ZERO sub-commands: the limit screen is reached via the menu, never a
+    # sub-command — call it directly the way the overview menu would.
+    cli._billing_limit_screen(state)
+    out = capsys.readouterr().out
+    assert "Monthly spend limit" in out
+    assert "$250 of $1000 used" in out
+    assert "read-only" in out
+
+
+def test_billing_sub_arg_ignored_opens_overview(cli, monkeypatch, capsys):
+    # A stray sub-arg must NOT error and must NOT dispatch to a sub-screen —
+    # it just opens the overview (spec §0.4: zero sub-commands).
+    monkeypatch.setattr(SonicCLI, "_prompt_text_input_modal", _boom_modal, raising=False)
+    state = BillingState(
+        logged_in=True, role="OWNER", balance_usd=Decimal("142.5"),
+        cli_billing_enabled=True, charge_presets=(Decimal("25"),),
+        portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    cli._show_billing("/billing buy")  # arg is ignored
+    out = capsys.readouterr().out
+    assert "Usage credits" in out  # overview, NOT the buy screen
+    assert "Buy usage credits" not in out
+
+
+def test_billing_buy_non_interactive_defers_to_portal(cli, monkeypatch, capsys):
+    monkeypatch.setattr(SonicCLI, "_prompt_text_input_modal", _boom_modal, raising=False)
+    state = BillingState(
+        logged_in=True, role="OWNER", cli_billing_enabled=True,
+        charge_presets=(Decimal("25"), Decimal("50"), Decimal("100")),
+        card=CardInfo(brand="visa", last4="4242"),
+        portal_url="https://portal/billing",
+    )
+    monkeypatch.setattr(bv, "build_billing_state", lambda *a, **kw: state)
+    # Reached via the menu in real use; non-interactively it defers to the portal.
+    cli._billing_buy_flow(state)
+    out = capsys.readouterr().out
+    assert "Buy usage credits" in out
+    assert "$25" in out and "$50" in out and "$100" in out
+    assert "interactive CLI" in out  # defers; no charge attempted non-interactively

tests/sonic_cli/test_billing_portal_url.py
@@ -1,0 +1,53 @@
+"""Portal-URL resolution for Phase 2b billing errors (nous_billing).
+
+The server emits ``portalUrl`` relative by design (``/billing?topup=open``); the
+client must resolve it against the active portal base so deep-links are clickable
+on whatever deployment (preview / staging / prod) the user is pointed at.
+"""
+
+from __future__ import annotations
+
+import pytest
+
+from sonic_cli.nous_billing import (
+    BillingError,
+    _absolutize_portal_url,
+    _raise_for_error,
+)
+
+
+@pytest.fixture
+def _preview(monkeypatch):
+    monkeypatch.setenv("SONIC_PORTAL_BASE_URL", "https://nas-pr-412.nousresearch.wtf")
+
+
+def test_absolutize_resolves_relative(_preview):
+    assert (
+        _absolutize_portal_url("/billing?topup=open")
+        == "https://nas-pr-412.nousresearch.wtf/billing?topup=open"
+    )
+
+
+def test_absolutize_leaves_absolute_unchanged(_preview):
+    # Idempotent: an already-absolute URL must NOT be double-prefixed.
+    url = "https://other.example/billing?topup=open"
+    assert _absolutize_portal_url(url) == url
+
+
+def test_absolutize_passthrough_empty(_preview):
+    assert _absolutize_portal_url(None) is None
+    assert _absolutize_portal_url("") == ""
+
+
+def test_raise_for_error_attaches_absolute_portal_url(_preview):
+    # The 403 no_payment_method envelope carries a RELATIVE portalUrl; the raised
+    # BillingError must expose it as ABSOLUTE so CLI + TUI render a clickable link.
+    with pytest.raises(BillingError) as exc_info:
+        _raise_for_error(
+            403,
+            {"error": "no_payment_method", "portalUrl": "/billing?topup=open"},
+        )
+    assert (
+        exc_info.value.portal_url
+        == "https://nas-pr-412.nousresearch.wtf/billing?topup=open"
+    )