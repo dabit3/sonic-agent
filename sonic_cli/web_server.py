AnandaAnugrahHandyanto/savarez_agent

fix(tui): restart dashboard chat on idle exit hotkeys

hermes_cli/web_server.py
@@ -10830,6 +10830,7 @@ def _resolve_chat_argv(
     # the dashboard PTY path.
     env.setdefault("HERMES_TUI_DISABLE_MOUSE", "1")
     env.setdefault("HERMES_TUI_INLINE", "1")
+    env["HERMES_TUI_DASHBOARD"] = "1"
 
     if profile_dir is not None:
         env["HERMES_HOME"] = str(profile_dir)

tests/hermes_cli/test_web_server.py
@@ -5062,6 +5062,7 @@ class TestPtyWebSocket:
 
         _argv, _cwd, env = self.ws_module._resolve_chat_argv()
 
+        assert env["HERMES_TUI_DASHBOARD"] == "1"
         assert env["HERMES_TUI_INLINE"] == "1"
         assert env["HERMES_TUI_DISABLE_MOUSE"] == "1"
 

ui-tui/src/config/env.ts
@@ -1,4 +1,5 @@
 import type { MouseTrackingMode } from '@hermes/ink'
+
 import { isTermuxTuiMode } from '../lib/termux.js'
 
 const truthy = (v?: string) => /^(?:1|true|yes|on)$/i.test((v ?? '').trim())
@@ -43,11 +44,18 @@ export const STARTUP_IMAGE = (process.env.HERMES_TUI_IMAGE ?? '').trim()
 //   behavior.
 const mouseTrackingOverride = parseToggle(process.env.HERMES_TUI_MOUSE_TRACKING)
 const mouseTrackingDisabledLegacy = truthy(process.env.HERMES_TUI_DISABLE_MOUSE)
+
 const resolvedBootMouseEnabled =
   mouseTrackingOverride ?? (TERMUX_TUI_MODE ? false : !mouseTrackingDisabledLegacy)
+
 export const MOUSE_TRACKING: MouseTrackingMode = resolvedBootMouseEnabled ? 'all' : 'off'
 
 export const NO_CONFIRM_DESTRUCTIVE = truthy(process.env.HERMES_TUI_NO_CONFIRM)
+
+// Set by the dashboard PTY launcher. This is intentionally narrower than
+// INLINE_MODE: users can opt into inline terminal rendering locally, but the
+// browser-embedded TUI has no healthy restart path after an idle exit.
+export const DASHBOARD_TUI_MODE = truthy(process.env.HERMES_TUI_DASHBOARD)
 
 // HERMES_DEV_CREDITS — dev-only live-spend readout (Δ status segment + "(dev credits)"
 // banner). Throwaway dev scaffolding; the whole readout gates on this one flag.

dabit3/sonic-agent

(cherry picked from commit 12dfcfdf73ed0543617ce0f4779aae8a9acb1e33)

sonic_cli/web_server.py
@@ -10830,6 +10830,7 @@ def _resolve_chat_argv(
     # the dashboard PTY path.
     env.setdefault("SONIC_TUI_DISABLE_MOUSE", "1")
     env.setdefault("SONIC_TUI_INLINE", "1")
+    env["SONIC_TUI_DASHBOARD"] = "1"
 
     if profile_dir is not None:
         env["SONIC_HOME"] = str(profile_dir)

tests/sonic_cli/test_web_server.py
@@ -5062,6 +5062,7 @@ class TestPtyWebSocket:
 
         _argv, _cwd, env = self.ws_module._resolve_chat_argv()
 
+        assert env["SONIC_TUI_DASHBOARD"] == "1"
         assert env["SONIC_TUI_INLINE"] == "1"
         assert env["SONIC_TUI_DISABLE_MOUSE"] == "1"
 

ui-tui/src/config/env.ts
@@ -1,4 +1,5 @@
 import type { MouseTrackingMode } from '@sonic/ink'
+
 import { isTermuxTuiMode } from '../lib/termux.js'
 
 const truthy = (v?: string) => /^(?:1|true|yes|on)$/i.test((v ?? '').trim())
@@ -43,11 +44,18 @@ export const STARTUP_IMAGE = (process.env.SONIC_TUI_IMAGE ?? '').trim()
 //   behavior.
 const mouseTrackingOverride = parseToggle(process.env.SONIC_TUI_MOUSE_TRACKING)
 const mouseTrackingDisabledLegacy = truthy(process.env.SONIC_TUI_DISABLE_MOUSE)
+
 const resolvedBootMouseEnabled =
   mouseTrackingOverride ?? (TERMUX_TUI_MODE ? false : !mouseTrackingDisabledLegacy)
+
 export const MOUSE_TRACKING: MouseTrackingMode = resolvedBootMouseEnabled ? 'all' : 'off'
 
 export const NO_CONFIRM_DESTRUCTIVE = truthy(process.env.SONIC_TUI_NO_CONFIRM)
+
+// Set by the dashboard PTY launcher. This is intentionally narrower than
+// INLINE_MODE: users can opt into inline terminal rendering locally, but the
+// browser-embedded TUI has no healthy restart path after an idle exit.
+export const DASHBOARD_TUI_MODE = truthy(process.env.SONIC_TUI_DASHBOARD)
 
 // SONIC_DEV_CREDITS — dev-only live-spend readout (Δ status segment + "(dev credits)"
 // banner). Throwaway dev scaffolding; the whole readout gates on this one flag.