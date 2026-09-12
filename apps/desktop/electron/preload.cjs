NousResearch/hermes-agent

feat(desktop): notify renderer when GPU acceleration is disabled due to remote display
Remote displays (RDP/SSH/X11) silently disable GPU hardware acceleration with
only a console.log, leaving the user unaware that software rendering is
active. Expose the detected reason over IPC and surface a dismissible banner
in the renderer.

apps/desktop/electron/preload.cjs
@@ -140,6 +140,7 @@ contextBridge.exposeInMainWorld('hermesDesktop', {
     return () => ipcRenderer.removeListener('hermes:bootstrap:event', listener)
   },
   getVersion: () => ipcRenderer.invoke('hermes:version'),
+  getRemoteDisplayReason: () => ipcRenderer.invoke('hermes:get-remote-display-reason'),
   uninstall: {
     summary: () => ipcRenderer.invoke('hermes:uninstall:summary'),
     run: mode => ipcRenderer.invoke('hermes:uninstall:run', { mode })

dabit3/sonic-agent

(cherry picked from commit 8ebe37f6ad2de723ea87b3568a1ba6698f85eca4)

apps/desktop/electron/preload.cjs
@@ -140,6 +140,7 @@ contextBridge.exposeInMainWorld('sonicDesktop', {
     return () => ipcRenderer.removeListener('sonic:bootstrap:event', listener)
   },
   getVersion: () => ipcRenderer.invoke('sonic:version'),
+  getRemoteDisplayReason: () => ipcRenderer.invoke('sonic:get-remote-display-reason'),
   uninstall: {
     summary: () => ipcRenderer.invoke('sonic:uninstall:summary'),
     run: mode => ipcRenderer.invoke('sonic:uninstall:run', { mode })