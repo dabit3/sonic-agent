Lexedgeai26/legal-hermes

fix(dashboard): hide sidecar sessions from history (#49269)
* fix(dashboard): hide sidecar sessions from history

* test(dashboard): allow sidecar source in session payload

gateway/session_context.py
@@ -49,6 +49,7 @@ _UNSET: Any = object()
 # ---------------------------------------------------------------------------
 
 _SESSION_PLATFORM: ContextVar = ContextVar("HERMES_SESSION_PLATFORM", default=_UNSET)
+_SESSION_SOURCE: ContextVar = ContextVar("HERMES_SESSION_SOURCE", default=_UNSET)
 _SESSION_CHAT_ID: ContextVar = ContextVar("HERMES_SESSION_CHAT_ID", default=_UNSET)
 _SESSION_CHAT_NAME: ContextVar = ContextVar("HERMES_SESSION_CHAT_NAME", default=_UNSET)
 _SESSION_THREAD_ID: ContextVar = ContextVar("HERMES_SESSION_THREAD_ID", default=_UNSET)
@@ -69,6 +70,7 @@ _CRON_AUTO_DELIVER_THREAD_ID: ContextVar = ContextVar("HERMES_CRON_AUTO_DELIVER_
 
 _VAR_MAP = {
     "HERMES_SESSION_PLATFORM": _SESSION_PLATFORM,
+    "HERMES_SESSION_SOURCE": _SESSION_SOURCE,
     "HERMES_SESSION_CHAT_ID": _SESSION_CHAT_ID,
     "HERMES_SESSION_CHAT_NAME": _SESSION_CHAT_NAME,
     "HERMES_SESSION_THREAD_ID": _SESSION_THREAD_ID,
@@ -100,6 +102,7 @@ def set_current_session_id(session_id: str) -> None:
 
 def set_session_vars(
     platform: str = "",
+    source: str = "",
     chat_id: str = "",
     chat_name: str = "",
     thread_id: str = "",
@@ -122,6 +125,7 @@ def set_session_vars(
     """
     tokens = [
         _SESSION_PLATFORM.set(platform),
+        _SESSION_SOURCE.set(source),
         _SESSION_CHAT_ID.set(chat_id),
         _SESSION_CHAT_NAME.set(chat_name),
         _SESSION_THREAD_ID.set(thread_id),
@@ -153,6 +157,7 @@ def clear_session_vars(tokens: list) -> None:
     """
     for var in (
         _SESSION_PLATFORM,
+        _SESSION_SOURCE,
         _SESSION_CHAT_ID,
         _SESSION_CHAT_NAME,
         _SESSION_THREAD_ID,

dabit3/sonic-agent

(cherry picked from commit a7983d5ad768551508667e8c708e13def7ee28ab)

gateway/session_context.py
@@ -49,6 +49,7 @@ _UNSET: Any = object()
 # ---------------------------------------------------------------------------
 
 _SESSION_PLATFORM: ContextVar = ContextVar("SONIC_SESSION_PLATFORM", default=_UNSET)
+_SESSION_SOURCE: ContextVar = ContextVar("SONIC_SESSION_SOURCE", default=_UNSET)
 _SESSION_CHAT_ID: ContextVar = ContextVar("SONIC_SESSION_CHAT_ID", default=_UNSET)
 _SESSION_CHAT_NAME: ContextVar = ContextVar("SONIC_SESSION_CHAT_NAME", default=_UNSET)
 _SESSION_THREAD_ID: ContextVar = ContextVar("SONIC_SESSION_THREAD_ID", default=_UNSET)
@@ -69,6 +70,7 @@ _CRON_AUTO_DELIVER_THREAD_ID: ContextVar = ContextVar("SONIC_CRON_AUTO_DELIVER_T
 
 _VAR_MAP = {
     "SONIC_SESSION_PLATFORM": _SESSION_PLATFORM,
+    "SONIC_SESSION_SOURCE": _SESSION_SOURCE,
     "SONIC_SESSION_CHAT_ID": _SESSION_CHAT_ID,
     "SONIC_SESSION_CHAT_NAME": _SESSION_CHAT_NAME,
     "SONIC_SESSION_THREAD_ID": _SESSION_THREAD_ID,
@@ -100,6 +102,7 @@ def set_current_session_id(session_id: str) -> None:
 
 def set_session_vars(
     platform: str = "",
+    source: str = "",
     chat_id: str = "",
     chat_name: str = "",
     thread_id: str = "",
@@ -122,6 +125,7 @@ def set_session_vars(
     """
     tokens = [
         _SESSION_PLATFORM.set(platform),
+        _SESSION_SOURCE.set(source),
         _SESSION_CHAT_ID.set(chat_id),
         _SESSION_CHAT_NAME.set(chat_name),
         _SESSION_THREAD_ID.set(thread_id),
@@ -153,6 +157,7 @@ def clear_session_vars(tokens: list) -> None:
     """
     for var in (
         _SESSION_PLATFORM,
+        _SESSION_SOURCE,
         _SESSION_CHAT_ID,
         _SESSION_CHAT_NAME,
         _SESSION_THREAD_ID,