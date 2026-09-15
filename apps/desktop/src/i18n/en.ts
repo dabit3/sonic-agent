NousResearch/hermes-agent

fix(desktop): relaunch on Linux after in-app update instead of hanging (#45205)
On a Linux source install the in-app updater ran the full backend update +
desktop rebuild successfully but never restarted the app — it hung forever on
the applying overlay with no close button. Two causes:

- applyUpdatesPosixInApp() only handled the macOS .app bundle swap;
  runningAppBundle() is null off macOS, so Linux fell through to
  { ok: true, backendUpdated: true } without ever relaunching.
- The renderer store had no terminal state for that result shape, so
  $updateApply stayed { applying: true } and the overlay's close button
  (hidden while applying) never appeared.

Fix (new electron/update-relaunch.cjs, pure + unit-tested):
- Decide the Linux outcome from whether the *running* binary is the one we
  just rebuilt (execPath under release/<plat>-unpacked, path-segment-aware so
  linux-unpacked-evil can't masquerade) and whether its chrome-sandbox helper
  is launchable (root:root + setuid, or an --no-sandbox / ELECTRON_DISABLE_SANDBOX
  opt-out):
    relaunch — detached watcher waits for this PID to exit (graceful, then
      SIGKILL), self-deletes, and re-execs the rebuilt binary with the original
      launch context (filtered args + HERMES_*/sandbox env + cwd) restored.
    guiSkew  — AppImage/.deb/.rpm/dev: backend updated but this GUI package was
      NOT changed; surface an honest closeable 'reinstall the desktop app'
      terminal state instead of lying that it loads next launch (#37541 skew).
    manual   — rebuilt binary but sandbox helper not launchable: keep the
      working window, don't quit into a dead app.
- store/updates.ts lands a terminal, closeable state for EVERY resolved apply
  outcome (handedOff / guiSkew / manualRestart / updated-not-relaunched / error)
  so the hang is impossible regardless of platform or result.
- New DesktopUpdateStage values (update/rebuild/done/guiSkew) + GuiSkewView so
  progress reads correctly and the skew state is closeab

apps/desktop/src/i18n/en.ts
@@ -1355,8 +1355,12 @@ export const en: Translations = {
       fetch: 'Downloading…',
       pull: 'Almost there…',
       pydeps: 'Finishing up…',
+      update: 'Updating Hermes…',
+      rebuild: 'Rebuilding the desktop app…',
       restart: 'Restarting Hermes…',
+      done: 'Update complete',
       manual: 'Update from your terminal',
+      guiSkew: 'Update the desktop app',
       error: 'Update paused'
     },
     checking: 'Looking for updates…',
@@ -1379,6 +1383,9 @@ export const en: Translations = {
     manualTitle: 'Update from your terminal',
     manualBody: 'You installed Hermes from the command line, so updates run there too. Paste this into your terminal:',
     manualPickedUp: 'Hermes will pick up the new version next time you launch it.',
+    guiSkewTitle: 'Update the desktop app',
+    guiSkewBody:
+      'The backend was updated, but this desktop app package wasn’t changed. Update or reinstall the Hermes desktop app (your AppImage / .deb / .rpm) to match.',
     copy: 'Copy',
     copied: 'Copied',
     done: 'Done',

apps/desktop/src/i18n/ja.ts
@@ -1483,8 +1483,12 @@ export const ja = defineLocale({
       fetch: 'ダウンロード中…',
       pull: 'もうすぐ完了…',
       pydeps: '仕上げ中…',
+      update: 'Hermes を更新中…',
+      rebuild: 'デスクトップアプリを再ビルド中…',
       restart: 'Hermes を再起動中…',
+      done: '更新が完了しました',
       manual: 'ターミナルから更新',
+      guiSkew: 'デスクトップアプリを更新してください',
       error: '更新が一時停止中'
     },
     checking: '更新を確認中…',
@@ -1509,6 +1513,9 @@ export const ja = defineLocale({
     manualBody:
       'Hermes をコマンドラインからインストールしたため、更新もそこで実行されます。これをターミナルに貼り付けてください:',
     manualPickedUp: 'Hermes は次回起動時に新しいバージョンを読み込みます。',
+    guiSkewTitle: 'デスクトップアプリを更新してください',
+    guiSkewBody:
+      'バックエンドは更新されましたが、このデスクトップアプリのパッケージは変更されていません。一致させるために Hermes デスクトップアプリ（AppImage / .deb / .rpm）を更新または再インストールしてください。',
     copy: 'コピー',
     copied: 'コピーしました',
     done: '完了',

apps/desktop/src/i18n/zh-hant.ts
@@ -1436,8 +1436,12 @@ export const zhHant = defineLocale({
       fetch: '下載中…',
       pull: '快完成了…',
       pydeps: '收尾中…',
+      update: '正在更新 Hermes…',
+      rebuild: '正在重新建置桌面應用程式…',
       restart: '正在重新啟動 Hermes…',
+      done: '更新完成',
       manual: '從終端機更新',
+      guiSkew: '請更新桌面應用程式',
       error: '更新已暫停'
     },
     checking: '正在檢查更新…',
@@ -1460,6 +1464,9 @@ export const zhHant = defineLocale({
     manualTitle: '從終端機更新',
     manualBody: '您是從命令列安裝的 Hermes，因此更新也需要在那裡執行。請將此指令貼到終端機：',
     manualPickedUp: '下次啟動 Hermes 時會使用新版本。',
+    guiSkewTitle: '請更新桌面應用程式',
+    guiSkewBody:
+      '後端已更新，但此桌面應用程式套件未變更。請更新或重新安裝 Hermes 桌面應用程式（你的 AppImage / .deb / .rpm）以保持一致。',
     copy: '複製',
     copied: '已複製',
     done: '完成',

apps/desktop/src/i18n/zh.ts
@@ -1541,8 +1541,12 @@ export const zh: Translations = {
       fetch: '下载中…',
       pull: '马上完成…',
       pydeps: '收尾中…',
+      update: '正在更新 Hermes…',
+      rebuild: '正在重新构建桌面应用…',
       restart: '正在重启 Hermes…',
+      done: '更新完成',
       manual: '从终端更新',
+      guiSkew: '请更新桌面应用',
       error: '更新已暂停'
     },
     checking: '正在检查更新…',
@@ -1565,6 +1569,8 @@ export const zh: Translations = {
     manualTitle: '从终端更新',
     manualBody: '你是从命令行安装的 Hermes，因此更新也需要在那里运行。请将此命令粘贴到终端：',
     manualPickedUp: '下次启动 Hermes 时会使用新版本。',
+    guiSkewTitle: '请更新桌面应用',
+    guiSkewBody: '后端已更新，但此桌面应用包未更改。请更新或重新安装 Hermes 桌面应用（你的 AppImage / .deb / .rpm）以保持一致。',
     copy: '复制',
     copied: '已复制',
     done: '完成',

dabit3/sonic-agent

(cherry picked from commit e5e25836350a7041e58bb4ecfd55cba893630df4)

apps/desktop/src/i18n/en.ts
@@ -1355,8 +1355,12 @@ export const en: Translations = {
       fetch: 'Downloading…',
       pull: 'Almost there…',
       pydeps: 'Finishing up…',
+      update: 'Updating Sonic…',
+      rebuild: 'Rebuilding the desktop app…',
       restart: 'Restarting Sonic…',
+      done: 'Update complete',
       manual: 'Update from your terminal',
+      guiSkew: 'Update the desktop app',
       error: 'Update paused'
     },
     checking: 'Looking for updates…',
@@ -1379,6 +1383,9 @@ export const en: Translations = {
     manualTitle: 'Update from your terminal',
     manualBody: 'You installed Sonic from the command line, so updates run there too. Paste this into your terminal:',
     manualPickedUp: 'Sonic will pick up the new version next time you launch it.',
+    guiSkewTitle: 'Update the desktop app',
+    guiSkewBody:
+      'The backend was updated, but this desktop app package wasn’t changed. Update or reinstall the Sonic desktop app (your AppImage / .deb / .rpm) to match.',
     copy: 'Copy',
     copied: 'Copied',
     done: 'Done',

apps/desktop/src/i18n/ja.ts
@@ -1483,8 +1483,12 @@ export const ja = defineLocale({
       fetch: 'ダウンロード中…',
       pull: 'もうすぐ完了…',
       pydeps: '仕上げ中…',
+      update: 'Sonic を更新中…',
+      rebuild: 'デスクトップアプリを再ビルド中…',
       restart: 'Sonic を再起動中…',
+      done: '更新が完了しました',
       manual: 'ターミナルから更新',
+      guiSkew: 'デスクトップアプリを更新してください',
       error: '更新が一時停止中'
     },
     checking: '更新を確認中…',
@@ -1509,6 +1513,9 @@ export const ja = defineLocale({
     manualBody:
       'Sonic をコマンドラインからインストールしたため、更新もそこで実行されます。これをターミナルに貼り付けてください:',
     manualPickedUp: 'Sonic は次回起動時に新しいバージョンを読み込みます。',
+    guiSkewTitle: 'デスクトップアプリを更新してください',
+    guiSkewBody:
+      'バックエンドは更新されましたが、このデスクトップアプリのパッケージは変更されていません。一致させるために Sonic デスクトップアプリ（AppImage / .deb / .rpm）を更新または再インストールしてください。',
     copy: 'コピー',
     copied: 'コピーしました',
     done: '完了',

apps/desktop/src/i18n/zh-hant.ts
@@ -1436,8 +1436,12 @@ export const zhHant = defineLocale({
       fetch: '下載中…',
       pull: '快完成了…',
       pydeps: '收尾中…',
+      update: '正在更新 Sonic…',
+      rebuild: '正在重新建置桌面應用程式…',
       restart: '正在重新啟動 Sonic…',
+      done: '更新完成',
       manual: '從終端機更新',
+      guiSkew: '請更新桌面應用程式',
       error: '更新已暫停'
     },
     checking: '正在檢查更新…',
@@ -1460,6 +1464,9 @@ export const zhHant = defineLocale({
     manualTitle: '從終端機更新',
     manualBody: '您是從命令列安裝的 Sonic，因此更新也需要在那裡執行。請將此指令貼到終端機：',
     manualPickedUp: '下次啟動 Sonic 時會使用新版本。',
+    guiSkewTitle: '請更新桌面應用程式',
+    guiSkewBody:
+      '後端已更新，但此桌面應用程式套件未變更。請更新或重新安裝 Sonic 桌面應用程式（你的 AppImage / .deb / .rpm）以保持一致。',
     copy: '複製',
     copied: '已複製',
     done: '完成',

apps/desktop/src/i18n/zh.ts
@@ -1541,8 +1541,12 @@ export const zh: Translations = {
       fetch: '下载中…',
       pull: '马上完成…',
       pydeps: '收尾中…',
+      update: '正在更新 Sonic…',
+      rebuild: '正在重新构建桌面应用…',
       restart: '正在重启 Sonic…',
+      done: '更新完成',
       manual: '从终端更新',
+      guiSkew: '请更新桌面应用',
       error: '更新已暂停'
     },
     checking: '正在检查更新…',
@@ -1565,6 +1569,8 @@ export const zh: Translations = {
     manualTitle: '从终端更新',
     manualBody: '你是从命令行安装的 Sonic，因此更新也需要在那里运行。请将此命令粘贴到终端：',
     manualPickedUp: '下次启动 Sonic 时会使用新版本。',
+    guiSkewTitle: '请更新桌面应用',
+    guiSkewBody: '后端已更新，但此桌面应用包未更改。请更新或重新安装 Sonic 桌面应用（你的 AppImage / .deb / .rpm）以保持一致。',
     copy: '复制',
     copied: '已复制',
     done: '完成',