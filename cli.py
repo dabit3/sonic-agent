Carrie111998/Hermes

feat(cli): /timestamps command + timestamps in /history (#50506)
display.timestamps already drove the [HH:MM] suffix on live submitted and
streamed message labels, but there was no runtime command to toggle it and
/history ignored the setting entirely. Add /timestamps [on|off|status]
(alias /ts) and render [HH:MM] in /history for turns that carry a stored
unix timestamp (resumed sessions). Live unsaved turns without a stored time
are never given a fabricated one. Uses the existing sanctioned non-wire
'timestamp' message key (stripped before the API call in chat_completions),
so message-alternation and prompt-cache invariants are untouched.

cli.py
@@ -6216,6 +6216,22 @@ class HermesCLI(CLIAgentSetupMixin, CLICommandsMixin):
         preview_limit = 400
         visible_index = 0
         hidden_tool_messages = 0
+        show_ts = bool(getattr(self, "show_timestamps", False))
+
+        def _ts_suffix(message: dict) -> str:
+            # Messages restored from SessionDB carry a unix `timestamp`; live
+            # unsaved turns may not. Only annotate when both the toggle is on
+            # and the turn actually has a stored time — never fabricate one.
+            if not show_ts:
+                return ""
+            ts = message.get("timestamp")
+            if not ts:
+                return ""
+            try:
+                from datetime import datetime
+                return f"  [{datetime.fromtimestamp(float(ts)).strftime('%H:%M')}]"
+            except (ValueError, OSError, TypeError):
+                return ""
 
         def flush_tool_summary():
             nonlocal hidden_tool_messages
@@ -6249,13 +6265,13 @@ class HermesCLI(CLIAgentSetupMixin, CLICommandsMixin):
             content_text = "" if content is None else str(content)
 
             if role == "user":
-                print(f"\n  [You #{visible_index}]")
+                print(f"\n  [You #{visible_index}]{_ts_suffix(msg)}")
                 print(
                     f"    {content_text[:preview_limit]}{'...' if len(content_text) > preview_limit else ''}"
                 )
                 continue
 
-            print(f"\n  [Hermes #{visible_index}]")
+            print(f"\n  [Hermes #{visible_index}]{_ts_suffix(msg)}")
             tool_calls = msg.get("tool_calls") or []
             if content_text:
                 preview = content_text[:preview_limit]
@@ -7978,6 +7994,8 @@ class HermesCLI(CLIAgentSetupMixin, CLICommandsMixin):
             self._status_bar_visible = not self._status_bar_visible
             state = "visible" if self._status_bar_visible else "hidden"
             self._console_print(f"  Status bar {state}")
+        elif canonical == "timestamps":
+            self._handle_timestamps_command(cmd_original)
         elif canonical == "verbose":
             self._toggle_verbose()
         elif canonical == "footer":

tests/hermes_cli/test_timestamps_command.py
@@ -1,0 +1,98 @@
+"""Tests for the CLI `/timestamps` toggle and timestamps in `/history`.
+
+`display.timestamps` already drove the live `[HH:MM]` label suffix on
+submitted/streamed messages but had no runtime toggle and `/history`
+ignored it. These assert the new `/timestamps` command flips and persists
+the flag and that `/history` renders `[HH:MM]` only for turns that carry a
+stored unix `timestamp` (never fabricating one for live unsaved turns).
+"""
+
+import io
+import sys
+import time
+from datetime import datetime
+
+import yaml
+
+from hermes_cli.cli_commands_mixin import CLICommandsMixin
+
+
+class _Stub(CLICommandsMixin):
+    def __init__(self):
+        self.show_timestamps = False
+
+
+def _seed(tmp_path, monkeypatch, value=False):
+    hh = tmp_path / ".hermes"
+    hh.mkdir()
+    (hh / "config.yaml").write_text(f"display:\n  timestamps: {str(value).lower()}\n")
+    monkeypatch.setenv("HERMES_HOME", str(hh))
+    import cli
+
+    monkeypatch.setattr(cli, "_hermes_home", hh, raising=False)
+    return hh
+
+
+def test_timestamps_on_sets_and_persists(tmp_path, monkeypatch):
+    hh = _seed(tmp_path, monkeypatch)
+    s = _Stub()
+    s._handle_timestamps_command("/timestamps on")
+    assert s.show_timestamps is True
+    assert yaml.safe_load((hh / "config.yaml").read_text())["display"]["timestamps"] is True
+
+
+def test_timestamps_bare_toggles(tmp_path, monkeypatch):
+    _seed(tmp_path, monkeypatch)
+    s = _Stub()
+    s.show_timestamps = True
+    s._handle_timestamps_command("/timestamps")
+    assert s.show_timestamps is False
+
+
+def test_timestamps_status_is_noop(tmp_path, monkeypatch):
+    _seed(tmp_path, monkeypatch)
+    s = _Stub()
+    s.show_timestamps = True
+    s._handle_timestamps_command("/timestamps status")
+    assert s.show_timestamps is True
+
+
+def _render_history(history, show_ts):
+    from cli import HermesCLI
+
+    h = HermesCLI.__new__(HermesCLI)
+    h.show_timestamps = show_ts
+    h.conversation_history = history
+    h._show_recent_sessions = lambda reason="history", limit=10: True
+    buf = io.StringIO()
+    old = sys.stdout
+    sys.stdout = buf
+    try:
+        h.show_history()
+    finally:
+        sys.stdout = old
+    return buf.getvalue()
+
+
+def test_history_shows_timestamp_for_stored_turns():
+    ts = time.time()
+    hist = [
+        {"role": "user", "content": "hello", "timestamp": ts},
+        {"role": "assistant", "content": "hi", "timestamp": ts + 60},
+        {"role": "user", "content": "live turn, no ts"},
+    ]
+    out = _render_history(hist, show_ts=True)
+    hhmm = datetime.fromtimestamp(ts).strftime("%H:%M")
+    assert f"[You #1]  [{hhmm}]" in out
+    assert "[Hermes #2]  [" in out
+    # a turn with no stored timestamp must NOT get a fabricated time
+    assert "[You #3]\n" in out
+
+
+def test_history_hides_timestamps_when_off():
+    ts = time.time()
+    hist = [{"role": "user", "content": "hello", "timestamp": ts}]
+    out = _render_history(hist, show_ts=False)
+    # label present, no [HH:MM] suffix
+    first_label_line = out.split("[You #1]")[1].split("\n")[0]
+    assert "[" not in first_label_line

dabit3/sonic-agent

(cherry picked from commit 5ff11a689b561fdb1404aede3fafa543bbbb86bf)

cli.py
@@ -6214,6 +6214,22 @@ class SonicCLI(CLIAgentSetupMixin, CLICommandsMixin):
         preview_limit = 400
         visible_index = 0
         hidden_tool_messages = 0
+        show_ts = bool(getattr(self, "show_timestamps", False))
+
+        def _ts_suffix(message: dict) -> str:
+            # Messages restored from SessionDB carry a unix `timestamp`; live
+            # unsaved turns may not. Only annotate when both the toggle is on
+            # and the turn actually has a stored time — never fabricate one.
+            if not show_ts:
+                return ""
+            ts = message.get("timestamp")
+            if not ts:
+                return ""
+            try:
+                from datetime import datetime
+                return f"  [{datetime.fromtimestamp(float(ts)).strftime('%H:%M')}]"
+            except (ValueError, OSError, TypeError):
+                return ""
 
         def flush_tool_summary():
             nonlocal hidden_tool_messages
@@ -6247,13 +6263,13 @@ class SonicCLI(CLIAgentSetupMixin, CLICommandsMixin):
             content_text = "" if content is None else str(content)
 
             if role == "user":
-                print(f"\n  [You #{visible_index}]")
+                print(f"\n  [You #{visible_index}]{_ts_suffix(msg)}")
                 print(
                     f"    {content_text[:preview_limit]}{'...' if len(content_text) > preview_limit else ''}"
                 )
                 continue
 
-            print(f"\n  [Sonic #{visible_index}]")
+            print(f"\n  [Sonic #{visible_index}]{_ts_suffix(msg)}")
             tool_calls = msg.get("tool_calls") or []
             if content_text:
                 preview = content_text[:preview_limit]
@@ -7976,6 +7992,8 @@ class SonicCLI(CLIAgentSetupMixin, CLICommandsMixin):
             self._status_bar_visible = not self._status_bar_visible
             state = "visible" if self._status_bar_visible else "hidden"
             self._console_print(f"  Status bar {state}")
+        elif canonical == "timestamps":
+            self._handle_timestamps_command(cmd_original)
         elif canonical == "verbose":
             self._toggle_verbose()
         elif canonical == "footer":

tests/sonic_cli/test_timestamps_command.py
@@ -1,0 +1,98 @@
+"""Tests for the CLI `/timestamps` toggle and timestamps in `/history`.
+
+`display.timestamps` already drove the live `[HH:MM]` label suffix on
+submitted/streamed messages but had no runtime toggle and `/history`
+ignored it. These assert the new `/timestamps` command flips and persists
+the flag and that `/history` renders `[HH:MM]` only for turns that carry a
+stored unix `timestamp` (never fabricating one for live unsaved turns).
+"""
+
+import io
+import sys
+import time
+from datetime import datetime
+
+import yaml
+
+from sonic_cli.cli_commands_mixin import CLICommandsMixin
+
+
+class _Stub(CLICommandsMixin):
+    def __init__(self):
+        self.show_timestamps = False
+
+
+def _seed(tmp_path, monkeypatch, value=False):
+    hh = tmp_path / ".sonic"
+    hh.mkdir()
+    (hh / "config.yaml").write_text(f"display:\n  timestamps: {str(value).lower()}\n")
+    monkeypatch.setenv("SONIC_HOME", str(hh))
+    import cli
+
+    monkeypatch.setattr(cli, "_sonic_home", hh, raising=False)
+    return hh
+
+
+def test_timestamps_on_sets_and_persists(tmp_path, monkeypatch):
+    hh = _seed(tmp_path, monkeypatch)
+    s = _Stub()
+    s._handle_timestamps_command("/timestamps on")
+    assert s.show_timestamps is True
+    assert yaml.safe_load((hh / "config.yaml").read_text())["display"]["timestamps"] is True
+
+
+def test_timestamps_bare_toggles(tmp_path, monkeypatch):
+    _seed(tmp_path, monkeypatch)
+    s = _Stub()
+    s.show_timestamps = True
+    s._handle_timestamps_command("/timestamps")
+    assert s.show_timestamps is False
+
+
+def test_timestamps_status_is_noop(tmp_path, monkeypatch):
+    _seed(tmp_path, monkeypatch)
+    s = _Stub()
+    s.show_timestamps = True
+    s._handle_timestamps_command("/timestamps status")
+    assert s.show_timestamps is True
+
+
+def _render_history(history, show_ts):
+    from cli import SonicCLI
+
+    h = SonicCLI.__new__(SonicCLI)
+    h.show_timestamps = show_ts
+    h.conversation_history = history
+    h._show_recent_sessions = lambda reason="history", limit=10: True
+    buf = io.StringIO()
+    old = sys.stdout
+    sys.stdout = buf
+    try:
+        h.show_history()
+    finally:
+        sys.stdout = old
+    return buf.getvalue()
+
+
+def test_history_shows_timestamp_for_stored_turns():
+    ts = time.time()
+    hist = [
+        {"role": "user", "content": "hello", "timestamp": ts},
+        {"role": "assistant", "content": "hi", "timestamp": ts + 60},
+        {"role": "user", "content": "live turn, no ts"},
+    ]
+    out = _render_history(hist, show_ts=True)
+    hhmm = datetime.fromtimestamp(ts).strftime("%H:%M")
+    assert f"[You #1]  [{hhmm}]" in out
+    assert "[Sonic #2]  [" in out
+    # a turn with no stored timestamp must NOT get a fabricated time
+    assert "[You #3]\n" in out
+
+
+def test_history_hides_timestamps_when_off():
+    ts = time.time()
+    hist = [{"role": "user", "content": "hello", "timestamp": ts}]
+    out = _render_history(hist, show_ts=False)
+    # label present, no [HH:MM] suffix
+    first_label_line = out.split("[You #1]")[1].split("\n")[0]
+    assert "[" not in first_label_line