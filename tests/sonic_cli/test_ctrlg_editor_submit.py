Carrie111998/Hermes

feat(cli): Ctrl+G submits the edited draft on save (TUI parity) (#50560)
Ctrl+G already opened $EDITOR with the current draft, but used
open_in_editor(validate_and_handle=False), which only loaded the saved text
back into the input area — the user still had to press Enter. The TUI's
Ctrl+G (openEditor) submits the draft on a clean exit. Since CLI submission
is driven by the custom Enter keybinding (not the buffer accept_handler),
validate_and_handle can't route through it; instead chain a done-callback on
the editor Task that calls the new _submit_editor_buffer(), which mirrors the
Enter handler's idle/queue/slash branches and drops an empty save.

tests/hermes_cli/test_ctrlg_editor_submit.py
@@ -1,0 +1,86 @@
+"""Tests for Ctrl+G external-editor submit in the classic CLI.
+
+Ctrl+G opens the current draft in ``$EDITOR``; on a clean save the draft is
+submitted (TUI parity) rather than left in the input area. Submission in the
+CLI is driven by the custom Enter keybinding, not the buffer accept_handler,
+so ``_open_external_editor`` chains a done-callback that calls
+``_submit_editor_buffer``. These exercise that submit helper directly.
+"""
+
+import queue
+
+from cli import HermesCLI
+
+
+class _FakeBuf:
+    def __init__(self, text: str):
+        self.text = text
+        self.reset_called = False
+
+    def reset(self, append_to_history: bool = False):
+        self.reset_called = True
+        self.text = ""
+
+
+def _make(agent_running: bool = False, busy: str = "queue") -> HermesCLI:
+    c = HermesCLI.__new__(HermesCLI)
+    c._pending_input = queue.Queue()
+    c._interrupt_queue = queue.Queue()
+    c._agent_running = agent_running
+    c.busy_input_mode = busy
+    c._app = None
+    c._should_exit = False
+    return c
+
+
+def test_idle_prompt_routed_to_pending_input():
+    c = _make()
+    buf = _FakeBuf("Explain vector databases.\nKeep it short.")
+
+    c._submit_editor_buffer(buf)
+
+    assert c._pending_input.get_nowait() == "Explain vector databases.\nKeep it short."
+    assert buf.reset_called
+
+
+def test_empty_save_does_not_submit():
+    c = _make()
+    buf = _FakeBuf("   \n  \n")
+
+    c._submit_editor_buffer(buf)
+
+    assert c._pending_input.empty()
+    # An empty save must not clear-and-submit a blank turn.
+    assert not buf.reset_called
+
+
+def test_running_queue_mode_queues_for_next_turn():
+    c = _make(agent_running=True, busy="queue")
+    buf = _FakeBuf("next turn please")
+
+    c._submit_editor_buffer(buf)
+
+    assert c._pending_input.get_nowait() == "next turn please"
+    assert c._interrupt_queue.empty()
+
+
+def test_running_interrupt_mode_uses_interrupt_queue():
+    c = _make(agent_running=True, busy="interrupt")
+    buf = _FakeBuf("interrupt this")
+
+    c._submit_editor_buffer(buf)
+
+    assert c._interrupt_queue.get_nowait() == "interrupt this"
+    assert c._pending_input.empty()
+
+
+def test_slash_command_dispatched_not_queued():
+    c = _make()
+    seen = {}
+    c.process_command = lambda command: seen.setdefault("cmd", command) or True
+    buf = _FakeBuf("/status")
+
+    c._submit_editor_buffer(buf)
+
+    assert seen.get("cmd") == "/status"
+    assert c._pending_input.empty()

dabit3/sonic-agent

(cherry picked from commit 74f0dd62e87536e2d53ece79a71f9a1fa75f038c)

tests/sonic_cli/test_ctrlg_editor_submit.py
@@ -1,0 +1,86 @@
+"""Tests for Ctrl+G external-editor submit in the classic CLI.
+
+Ctrl+G opens the current draft in ``$EDITOR``; on a clean save the draft is
+submitted (TUI parity) rather than left in the input area. Submission in the
+CLI is driven by the custom Enter keybinding, not the buffer accept_handler,
+so ``_open_external_editor`` chains a done-callback that calls
+``_submit_editor_buffer``. These exercise that submit helper directly.
+"""
+
+import queue
+
+from cli import SonicCLI
+
+
+class _FakeBuf:
+    def __init__(self, text: str):
+        self.text = text
+        self.reset_called = False
+
+    def reset(self, append_to_history: bool = False):
+        self.reset_called = True
+        self.text = ""
+
+
+def _make(agent_running: bool = False, busy: str = "queue") -> SonicCLI:
+    c = SonicCLI.__new__(SonicCLI)
+    c._pending_input = queue.Queue()
+    c._interrupt_queue = queue.Queue()
+    c._agent_running = agent_running
+    c.busy_input_mode = busy
+    c._app = None
+    c._should_exit = False
+    return c
+
+
+def test_idle_prompt_routed_to_pending_input():
+    c = _make()
+    buf = _FakeBuf("Explain vector databases.\nKeep it short.")
+
+    c._submit_editor_buffer(buf)
+
+    assert c._pending_input.get_nowait() == "Explain vector databases.\nKeep it short."
+    assert buf.reset_called
+
+
+def test_empty_save_does_not_submit():
+    c = _make()
+    buf = _FakeBuf("   \n  \n")
+
+    c._submit_editor_buffer(buf)
+
+    assert c._pending_input.empty()
+    # An empty save must not clear-and-submit a blank turn.
+    assert not buf.reset_called
+
+
+def test_running_queue_mode_queues_for_next_turn():
+    c = _make(agent_running=True, busy="queue")
+    buf = _FakeBuf("next turn please")
+
+    c._submit_editor_buffer(buf)
+
+    assert c._pending_input.get_nowait() == "next turn please"
+    assert c._interrupt_queue.empty()
+
+
+def test_running_interrupt_mode_uses_interrupt_queue():
+    c = _make(agent_running=True, busy="interrupt")
+    buf = _FakeBuf("interrupt this")
+
+    c._submit_editor_buffer(buf)
+
+    assert c._interrupt_queue.get_nowait() == "interrupt this"
+    assert c._pending_input.empty()
+
+
+def test_slash_command_dispatched_not_queued():
+    c = _make()
+    seen = {}
+    c.process_command = lambda command: seen.setdefault("cmd", command) or True
+    buf = _FakeBuf("/status")
+
+    c._submit_editor_buffer(buf)
+
+    assert seen.get("cmd") == "/status"
+    assert c._pending_input.empty()