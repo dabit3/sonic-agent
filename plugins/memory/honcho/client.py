Carrie111998/Hermes

feat(memory): Honcho OAuth connect — desktop and CLI flows + token refresh (#44335)
* feat(memory): OAuth token storage and refresh for the Honcho provider

* feat(memory): refresh the Honcho OAuth token in the client and session

* feat(memory): zero-CLI loopback OAuth authorization flow

* feat(memory): generic memory-provider OAuth connect endpoints

* feat(desktop): memory-provider OAuth connect link

* feat(memory): CLI OAuth sign-in with source-tagged authorize links

* fix(memory): IP-literal loopback redirect and consent config_path on the authorize link

* fix(memory): profile-scope the memory-provider OAuth endpoints

* refactor(desktop): generic memory-provider OAuth client functions

* docs(memory): trim OAuth module docstrings to the invariants

* docs(memory): document OAuth connect as an optional auth method

* fix(memory): send home-relative display path to consent, not the absolute path

* perf(memory): cache OAuth token expiry in memory to skip the hot-path disk read

* fix(memory): log OAuth refresh failures at warning, not debug

* feat(memory): fall back to an OS-assigned loopback port when 8765 is taken

* test(memory): cover the desktop Connect launcher, status, and provider dispatch

* fix(desktop): keep the memory-provider dropdown one size regardless of connect state

* fix(desktop): move the memory connect link to the description line, leaving the dropdown untouched

* refactor(memory): move OAuth connect routes out of web_server into a memory-layer router

* refactor(desktop): import MemoryConnect directly, drop the single-export barrel

* fix(memory): launch CLI OAuth sign-in right after the auth choice, not after the wizard

* fix(desktop): auto-clear the OAuth error state instead of leaving it sticky

* test(honcho): isolate auth-method prompt from deployment-shape wizard tests

main's wizard suite scripts the cloud prompts without the OAuth auth-method step; auto-answer it in the shared helper so the answer lists stay shape-only.

* d

plugins/memory/honcho/client.py
@@ -679,10 +679,11 @@ class HonchoClientConfig:
         """Resolve Honcho session name.
 
         Resolution order:
-          1. Manual directory override from sessions map
-          2. Hermes session title (from /title command)
-          3. Gateway session key (stable per-chat identifier from gateway platforms)
-          4. per-session strategy — Hermes session_id ({timestamp}_{hex})
+          1. Gateway session key (stable per-chat identifier from gateway platforms)
+          2. per-session strategy — Hermes session_id ({timestamp}_{hex}); authoritative,
+             so a generated title never remaps a live conversation
+          3. Manual directory override from sessions map
+          4. Hermes session title (from /title command; non-per-session)
           5. per-repo strategy — git repo root directory name
           6. per-directory strategy — directory basename
           7. global strategy — workspace name
@@ -692,34 +693,33 @@ class HonchoClientConfig:
         if not cwd:
             cwd = os.getcwd()
 
-        # Manual override always wins
+        # Gateway per-chat key wins everywhere — gateways (telegram/discord/…)
+        # need per-chat isolation no cwd/strategy name can provide.
+        if gateway_session_key:
+            sanitized = re.sub(r'[^a-zA-Z0-9_-]+', '-', gateway_session_key).strip('-')
+            if sanitized:
+                return self._enforce_session_id_limit(sanitized, gateway_session_key)
+
+        # per-session: the run's session_id IS the identity — resolve before the
+        # cwd map / title so an auto-generated title can't remap a live
+        # conversation onto a second Honcho session mid-stream.
+        if self.session_strategy == "per-session" and session_id:
+            if self.session_peer_prefix and self.peer_name:
+                return f"{self.peer_name}-{session_id}"
+            return session_id
+
+        # Manual override (cwd → name), for non-per-session strategies.
         manual = self.sessions.get(cwd)
         if manual:
             return manual
 
-        # /title mid-session remap
+        # /title mid-session remap (non-per-session).
         if session_title:
             sanitized = re.sub(r'[^a-zA-Z0-9_-]+', '-', session_title).strip('-')
             if sanitized:
                 if self.session_peer_prefix and self.peer_name:
                     return f"{self.peer_name}-{sanitized}"
                 return sanitized
-
-        # Gateway session key: stable per-chat identifier passed by the gateway
-        # (e.g. "agent:main:telegram:dm:8439114563"). Sanitize colons to hyphens
-        # for Honcho session ID compatibility. This takes priority over strategy-
-        # based resolution because gateway platforms need per-chat isolation that
-        # cwd-based strategies cannot provide.
-        if gateway_session_key:
-            sanitized = re.sub(r'[^a-zA-Z0-9_-]+', '-', gateway_session_key).strip('-')
-            if sanitized:
-                return self._enforce_session_id_limit(sanitized, gateway_session_key)
-
-        # per-session: inherit Hermes session_id (new Honcho session each run)
-        if self.session_strategy == "per-session" and session_id:
-            if self.session_peer_prefix and self.peer_name:
-                return f"{self.peer_name}-{session_id}"
-            return session_id
 
         # per-repo: one Honcho session per git repository
         if self.session_strategy == "per-repo":
@@ -742,6 +742,39 @@ class HonchoClientConfig:
 _honcho_client_slot: SingletonSlot = SingletonSlot()
 
 
+def _apply_fresh_oauth_token(config: HonchoClientConfig) -> None:
+    """Refresh a near-expiry OAuth grant and point ``config.api_key`` at it.
+
+    No-op for static API keys or when refresh fails (fail-open: the stale token
+    is left in place and the existing 401 handling degrades gracefully).
+    """
+    try:
+        from plugins.memory.honcho import oauth
+
+        token, _ = oauth.ensure_fresh_token(resolve_config_path(), config.host)
+        if token:
+            config.api_key = token
+    except Exception:
+        logger.warning("Honcho OAuth pre-build refresh failed", exc_info=True)
+
+
+def _refresh_cached_oauth(client: "Honcho", config: HonchoClientConfig | None) -> None:
+    """Rotate the cached client's Bearer in place when its OAuth token is stale.
+
+    If the SDK shape changed and the in-place rotation can't apply, the slot is
+    reset so the next acquisition rebuilds with the fresh token.
+    """
+    try:
+        from plugins.memory.honcho import oauth
+
+        host = config.host if config is not None else resolve_active_host()
+        token, refreshed = oauth.ensure_fresh_token(resolve_config_path(), host)
+        if refreshed and token and not oauth.apply_token_to_client(client, token):
+            _honcho_client_slot.reset()
+    except Exception:
+        logger.warning("Honcho OAuth cached refresh failed", exc_info=True)
+
+
 def get_honcho_client(config: HonchoClientConfig | None = None) -> Honcho:
     """Get or create the Honcho client singleton.
 
@@ -754,10 +787,15 @@ def get_honcho_client(config: HonchoClientConfig | None = None) -> Honcho:
     """
     cached = _honcho_client_slot.peek()
     if cached is not None:
+        _refresh_cached_oauth(cached, config)
         return cached
 
     if config is None:
         config = HonchoClientConfig.from_global_config()
+
+    # Refresh a near-expiry OAuth grant before the first build so the client
+    # starts with a live access token rather than 401ing an hour in.
+    _apply_fresh_oauth_token(config)
 
     if not config.api_key and not config.base_url:
         raise ValueError(

hermes_cli/memory_oauth.py
@@ -1,0 +1,83 @@
+"""HTTP routes for memory-provider OAuth connect, mounted by ``web_server``.
+
+Kept out of ``web_server.py`` so the memory feature's surface stays in the
+memory layer. Dispatch is by convention: a provider's flow lives at
+``plugins.memory.<provider>.oauth_flow`` exposing ``start_loopback_flow_background``
+and ``get_flow_status``; a provider without that module simply 404s. No provider
+is named here.
+"""
+
+from __future__ import annotations
+
+import importlib
+from contextlib import contextmanager
+from typing import Optional
+
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter(prefix="/api/memory/providers")
+
+
+def _resolve_flow(provider: str):
+    """Return a provider's OAuth flow module by convention, or raise 404."""
+    if not provider.isidentifier():
+        raise HTTPException(status_code=404, detail=f"unknown memory provider {provider!r}")
+    try:
+        return importlib.import_module(f"plugins.memory.{provider}.oauth_flow")
+    except ImportError:
+        raise HTTPException(status_code=404, detail=f"{provider} does not support OAuth connect")
+
+
+@contextmanager
+def _scope_to_profile(profile: Optional[str]):
+    """Scope config resolution to ``profile`` so the flow's eager path resolve
+    targets that profile's honcho.json. None/""/"current" leaves it untouched."""
+    requested = (profile or "").strip()
+    if not requested or requested.lower() == "current":
+        yield
+        return
+
+    from hermes_cli import profiles as profiles_mod
+    from hermes_constants import reset_hermes_home_override, set_hermes_home_override
+
+    try:
+        profiles_mod.validate_profile_name(requested)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    if not profiles_mod.profile_exists(requested):
+        raise HTTPException(status_code=404, detail=f"Profile '{requested}' does not exist.")
+
+    token = set_hermes_home_override(str(profiles_mod.get_profile_dir(requested)))
+    try:
+        yield
+    finally:
+        reset_hermes_home_override(token)
+
+
+@router.post("/{provider}/oauth/start")
+async def start_memory_oauth(provider: str, profile: Optional[str] = None):
+    """Begin a provider's zero-CLI OAuth flow — opens the browser and captures
+    the grant via the loopback listener. Returns immediately; poll status."""
+    flow = _resolve_flow(provider)
+    try:
+        # The flow resolves its config path eagerly inside this scope; the worker
+        # thread it spawns outlives the request and the override.
+        with _scope_to_profile(profile):
+            return flow.start_loopback_flow_background()
+    except HTTPException:
+        raise
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Failed to start {provider} OAuth: {exc}")
+
+
+@router.get("/{provider}/oauth/status")
+async def memory_oauth_status(provider: str, profile: Optional[str] = None):
+    """Poll a provider's OAuth flow: idle | pending | connected | error."""
+    flow = _resolve_flow(provider)
+    try:
+        with _scope_to_profile(profile):
+            return flow.get_flow_status()
+    except HTTPException:
+        raise
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Failed to read {provider} OAuth status: {exc}")

dabit3/sonic-agent

(cherry picked from commit ba9e3a491bfaa04fbadbb165d3691aca2f80a9e8)

plugins/memory/honcho/client.py
@@ -679,10 +679,11 @@ class HonchoClientConfig:
         """Resolve Honcho session name.
 
         Resolution order:
-          1. Manual directory override from sessions map
-          2. Sonic session title (from /title command)
-          3. Gateway session key (stable per-chat identifier from gateway platforms)
-          4. per-session strategy — Sonic session_id ({timestamp}_{hex})
+          1. Gateway session key (stable per-chat identifier from gateway platforms)
+          2. per-session strategy — Sonic session_id ({timestamp}_{hex}); authoritative,
+             so a generated title never remaps a live conversation
+          3. Manual directory override from sessions map
+          4. Sonic session title (from /title command; non-per-session)
           5. per-repo strategy — git repo root directory name
           6. per-directory strategy — directory basename
           7. global strategy — workspace name
@@ -692,34 +693,33 @@ class HonchoClientConfig:
         if not cwd:
             cwd = os.getcwd()
 
-        # Manual override always wins
+        # Gateway per-chat key wins everywhere — gateways (telegram/discord/…)
+        # need per-chat isolation no cwd/strategy name can provide.
+        if gateway_session_key:
+            sanitized = re.sub(r'[^a-zA-Z0-9_-]+', '-', gateway_session_key).strip('-')
+            if sanitized:
+                return self._enforce_session_id_limit(sanitized, gateway_session_key)
+
+        # per-session: the run's session_id IS the identity — resolve before the
+        # cwd map / title so an auto-generated title can't remap a live
+        # conversation onto a second Honcho session mid-stream.
+        if self.session_strategy == "per-session" and session_id:
+            if self.session_peer_prefix and self.peer_name:
+                return f"{self.peer_name}-{session_id}"
+            return session_id
+
+        # Manual override (cwd → name), for non-per-session strategies.
         manual = self.sessions.get(cwd)
         if manual:
             return manual
 
-        # /title mid-session remap
+        # /title mid-session remap (non-per-session).
         if session_title:
             sanitized = re.sub(r'[^a-zA-Z0-9_-]+', '-', session_title).strip('-')
             if sanitized:
                 if self.session_peer_prefix and self.peer_name:
                     return f"{self.peer_name}-{sanitized}"
                 return sanitized
-
-        # Gateway session key: stable per-chat identifier passed by the gateway
-        # (e.g. "agent:main:telegram:dm:8439114563"). Sanitize colons to hyphens
-        # for Honcho session ID compatibility. This takes priority over strategy-
-        # based resolution because gateway platforms need per-chat isolation that
-        # cwd-based strategies cannot provide.
-        if gateway_session_key:
-            sanitized = re.sub(r'[^a-zA-Z0-9_-]+', '-', gateway_session_key).strip('-')
-            if sanitized:
-                return self._enforce_session_id_limit(sanitized, gateway_session_key)
-
-        # per-session: inherit Sonic session_id (new Honcho session each run)
-        if self.session_strategy == "per-session" and session_id:
-            if self.session_peer_prefix and self.peer_name:
-                return f"{self.peer_name}-{session_id}"
-            return session_id
 
         # per-repo: one Honcho session per git repository
         if self.session_strategy == "per-repo":
@@ -742,6 +742,39 @@ class HonchoClientConfig:
 _honcho_client_slot: SingletonSlot = SingletonSlot()
 
 
+def _apply_fresh_oauth_token(config: HonchoClientConfig) -> None:
+    """Refresh a near-expiry OAuth grant and point ``config.api_key`` at it.
+
+    No-op for static API keys or when refresh fails (fail-open: the stale token
+    is left in place and the existing 401 handling degrades gracefully).
+    """
+    try:
+        from plugins.memory.honcho import oauth
+
+        token, _ = oauth.ensure_fresh_token(resolve_config_path(), config.host)
+        if token:
+            config.api_key = token
+    except Exception:
+        logger.warning("Honcho OAuth pre-build refresh failed", exc_info=True)
+
+
+def _refresh_cached_oauth(client: "Honcho", config: HonchoClientConfig | None) -> None:
+    """Rotate the cached client's Bearer in place when its OAuth token is stale.
+
+    If the SDK shape changed and the in-place rotation can't apply, the slot is
+    reset so the next acquisition rebuilds with the fresh token.
+    """
+    try:
+        from plugins.memory.honcho import oauth
+
+        host = config.host if config is not None else resolve_active_host()
+        token, refreshed = oauth.ensure_fresh_token(resolve_config_path(), host)
+        if refreshed and token and not oauth.apply_token_to_client(client, token):
+            _honcho_client_slot.reset()
+    except Exception:
+        logger.warning("Honcho OAuth cached refresh failed", exc_info=True)
+
+
 def get_honcho_client(config: HonchoClientConfig | None = None) -> Honcho:
     """Get or create the Honcho client singleton.
 
@@ -754,10 +787,15 @@ def get_honcho_client(config: HonchoClientConfig | None = None) -> Honcho:
     """
     cached = _honcho_client_slot.peek()
     if cached is not None:
+        _refresh_cached_oauth(cached, config)
         return cached
 
     if config is None:
         config = HonchoClientConfig.from_global_config()
+
+    # Refresh a near-expiry OAuth grant before the first build so the client
+    # starts with a live access token rather than 401ing an hour in.
+    _apply_fresh_oauth_token(config)
 
     if not config.api_key and not config.base_url:
         raise ValueError(

sonic_cli/memory_oauth.py
@@ -1,0 +1,83 @@
+"""HTTP routes for memory-provider OAuth connect, mounted by ``web_server``.
+
+Kept out of ``web_server.py`` so the memory feature's surface stays in the
+memory layer. Dispatch is by convention: a provider's flow lives at
+``plugins.memory.<provider>.oauth_flow`` exposing ``start_loopback_flow_background``
+and ``get_flow_status``; a provider without that module simply 404s. No provider
+is named here.
+"""
+
+from __future__ import annotations
+
+import importlib
+from contextlib import contextmanager
+from typing import Optional
+
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter(prefix="/api/memory/providers")
+
+
+def _resolve_flow(provider: str):
+    """Return a provider's OAuth flow module by convention, or raise 404."""
+    if not provider.isidentifier():
+        raise HTTPException(status_code=404, detail=f"unknown memory provider {provider!r}")
+    try:
+        return importlib.import_module(f"plugins.memory.{provider}.oauth_flow")
+    except ImportError:
+        raise HTTPException(status_code=404, detail=f"{provider} does not support OAuth connect")
+
+
+@contextmanager
+def _scope_to_profile(profile: Optional[str]):
+    """Scope config resolution to ``profile`` so the flow's eager path resolve
+    targets that profile's honcho.json. None/""/"current" leaves it untouched."""
+    requested = (profile or "").strip()
+    if not requested or requested.lower() == "current":
+        yield
+        return
+
+    from sonic_cli import profiles as profiles_mod
+    from sonic_constants import reset_sonic_home_override, set_sonic_home_override
+
+    try:
+        profiles_mod.validate_profile_name(requested)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    if not profiles_mod.profile_exists(requested):
+        raise HTTPException(status_code=404, detail=f"Profile '{requested}' does not exist.")
+
+    token = set_sonic_home_override(str(profiles_mod.get_profile_dir(requested)))
+    try:
+        yield
+    finally:
+        reset_sonic_home_override(token)
+
+
+@router.post("/{provider}/oauth/start")
+async def start_memory_oauth(provider: str, profile: Optional[str] = None):
+    """Begin a provider's zero-CLI OAuth flow — opens the browser and captures
+    the grant via the loopback listener. Returns immediately; poll status."""
+    flow = _resolve_flow(provider)
+    try:
+        # The flow resolves its config path eagerly inside this scope; the worker
+        # thread it spawns outlives the request and the override.
+        with _scope_to_profile(profile):
+            return flow.start_loopback_flow_background()
+    except HTTPException:
+        raise
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Failed to start {provider} OAuth: {exc}")
+
+
+@router.get("/{provider}/oauth/status")
+async def memory_oauth_status(provider: str, profile: Optional[str] = None):
+    """Poll a provider's OAuth flow: idle | pending | connected | error."""
+    flow = _resolve_flow(provider)
+    try:
+        with _scope_to_profile(profile):
+            return flow.get_flow_status()
+    except HTTPException:
+        raise
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Failed to read {provider} OAuth status: {exc}")