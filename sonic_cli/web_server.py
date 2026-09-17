Carrie111998/Hermes

revert(plugins): restore user dashboard plugin backend API auto-import (#43719) (#51950)
* Revert "refactor(security): centralize non-bundled plugin sources in one constant"

This reverts commit 32112895c6d749f939111f921b7a2333276fdceb.

* Revert "fix(security): restrict dashboard plugin backend import to bundled plugins (#43719)"

This reverts commit 9068089f3946d483510510a28c700241c88708d4.

hermes_cli/web_server.py
@@ -12277,20 +12277,12 @@ def _safe_plugin_api_relpath(api_field: Any, *, dashboard_dir: Path) -> Optional
     return api_field
 
 
-# Plugin sources whose Python backend (dashboard manifest `api` file) must NEVER
-# be auto-imported by the dashboard web server — only bundled plugins may. Shared
-# by the discovery-time scrub and the mount-time refuse guards so a typo in one
-# site cannot silently disable a security gate (GHSA-5qr3-c538-wm9j / #43719).
-_NON_BUNDLED_PLUGIN_SOURCES = frozenset({"user", "project"})
-
-
 def _discover_dashboard_plugins() -> list:
     """Scan plugins/*/dashboard/manifest.json for dashboard extensions.
 
-    Checks three plugin sources. Bundled dashboard plugins win name conflicts
-    so non-bundled plugins cannot shadow trusted backend-capable routes:
-    1. Bundled plugins: <repo>/plugins/<name>/dashboard/manifest.json  (memory/, etc.)
-    2. User plugins:    ~/.hermes/plugins/<name>/dashboard/manifest.json
+    Checks three plugin sources (same as hermes_cli.plugins):
+    1. User plugins:    ~/.hermes/plugins/<name>/dashboard/manifest.json
+    2. Bundled plugins: <repo>/plugins/<name>/dashboard/manifest.json  (memory/, etc.)
     3. Project plugins: ./.hermes/plugins/  (only if HERMES_ENABLE_PROJECT_PLUGINS)
     """
     plugins = []
@@ -12299,9 +12291,9 @@ def _discover_dashboard_plugins() -> list:
     from hermes_cli.plugins import get_bundled_plugins_dir
     bundled_root = get_bundled_plugins_dir()
     search_dirs = [
+        (get_hermes_home() / "plugins", "user"),
         (bundled_root / "memory", "bundled"),
         (bundled_root, "bundled"),
-        (get_hermes_home() / "plugins", "user"),
     ]
     # GHSA-5qr3-c538-wm9j (#29156): the previous ``os.environ.get(...)``
     # check treated *any* non-empty string as truthy, so ``=0``, ``=false``,
@@ -12360,20 +12352,10 @@ def _discover_dashboard_plugins() -> list:
                 raw_api = data.get("api")
                 dashboard_dir = child / "dashboard"
                 safe_api = _safe_plugin_api_relpath(raw_api, dashboard_dir=dashboard_dir)
-                if source in _NON_BUNDLED_PLUGIN_SOURCES and safe_api:
-                    _log.warning(
-                        "Plugin %s: refusing dashboard backend api=%s "
-                        "(only bundled plugins may auto-import Python "
-                        "backend routes; non-bundled plugins may extend "
-                        "the dashboard with static UI assets only)",
-                        name, safe_api,
-                    )
-                    safe_api = None
-                    raw_api = None
                 if raw_api and safe_api is None:
                     _log.warning(
                         "Plugin %s: refusing unsafe api path %r (must be a "
-                        "relative file inside a bundled plugin's dashboard/ "
+                        "relative file inside the plugin's dashboard/ "
                         "directory); backend routes from this plugin will "
                         "not be mounted",
                         name, raw_api,
@@ -12780,36 +12762,23 @@ def _mount_plugin_api_routes():
     a ``router`` (FastAPI APIRouter).  Routes are mounted under
     ``/api/plugins/<name>/``.
 
-    Backend import is restricted to bundled plugins. User and project
-    plugins can extend the dashboard UI via static JS/CSS, but their
-    Python ``api`` files are never auto-imported by the web server.
-    See GHSA-5qr3-c538-wm9j (#29156) and #43719.
+    Backend import is restricted to ``bundled`` and ``user`` sources.
+    Project plugins (``./.hermes/plugins/``) ship with the CWD and are
+    therefore attacker-controlled in any threat model where the user
+    opens a malicious repo; they can extend the dashboard UI via
+    static JS/CSS but their Python ``api`` file is never auto-imported
+    by the web server.  See GHSA-5qr3-c538-wm9j (#29156).
     """
     for plugin in _get_dashboard_plugins():
         api_file_name = plugin.get("_api_file")
         if not api_file_name:
             continue
-        source = plugin.get("source")
-        if source in _NON_BUNDLED_PLUGIN_SOURCES:
-            # Backend Python auto-import is reserved for bundled plugins; user
-            # and project plugins extend the dashboard with static UI assets
-            # only (GHSA-5qr3-c538-wm9j / #43719). Defence-in-depth: discovery
-            # already nulls _api_file for these sources, but re-refusing here —
-            # at the actual importlib call site — keeps the import primitive
-            # contained even if a future caller or a tampered cache entry slips
-            # a non-bundled plugin through with an _api_file set.
-            _reason = {
-                "user": (
-                    "user-installed plugins may not auto-import Python code"
-                ),
-                "project": (
-                    "project plugins may not auto-import Python code; backend "
-                    "auto-import is reserved for bundled plugins"
-                ),
-            }.get(source, "only bundled plugins may auto-import Python code")
+        if plugin.get("source") == "project":
             _log.warning(
-                "Plugin %s: ignoring backend api=%s (%s)",
-                plugin["name"], api_file_name, _reason,
+                "Plugin %s: ignoring backend api=%s (project plugins may "
+                "not auto-import Python code; move the plugin to "
+                "~/.hermes/plugins/ if you trust it)",
+                plugin["name"], api_file_name,
             )
             continue
         dashboard_dir = Path(plugin["_dir"])

tests/hermes_cli/test_web_server.py
@@ -5095,8 +5095,14 @@ class TestPluginAPIAuth:
     """Tests that plugin API routes require the session token (issue #19533)."""
 
     @pytest.fixture(autouse=True)
-    def _setup_test_client(self, monkeypatch, _isolate_hermes_home):
-        """Create TestClients with and without the session token header."""
+    def _setup_test_client(self, monkeypatch, _isolate_hermes_home, _install_example_plugin):
+        """Create a TestClient without the session token header.
+
+        Pulls in ``_install_example_plugin`` so ``test_plugin_route_allows_auth``
+        has the ``/api/plugins/example/hello`` endpoint available — the
+        example plugin is no longer a bundled plugin, so the fixture
+        installs it into the per-test ``HERMES_HOME``.
+        """
         try:
             from starlette.testclient import TestClient
         except ImportError:
@@ -5121,15 +5127,19 @@ class TestPluginAPIAuth:
     def test_plugin_route_allows_auth(self):
         """Plugin API routes should work with a valid session token.
 
-        Uses a bundled plugin route so the test covers authenticated plugin
-        API access without relying on user-installed plugin backend imports.
+        Uses ``/api/plugins/example/hello`` from the example-dashboard
+        test fixture (installed into HERMES_HOME by the class-level
+        ``_install_example_plugin`` fixture) — a stable, side-effect-free
+        GET that's only loaded for tests. With a valid token the handler
+        should run (200); without one the middleware should 401 before
+        the handler is reached.
         """
         # Without auth: middleware blocks before reaching the handler.
-        resp = self.client.get("/api/plugins/kanban/board")
+        resp = self.client.get("/api/plugins/example/hello")
         assert resp.status_code == 401
 
         # With auth: handler runs.
-        resp = self.auth_client.get("/api/plugins/kanban/board")
+        resp = self.auth_client.get("/api/plugins/example/hello")
         assert resp.status_code == 200
 
     def test_plugin_post_requires_auth(self):

dabit3/sonic-agent

This reverts commit e2bea0abe6aae9dd1e9ff275c9240093c0d03245.
This reverts commit 8845f3316c26732cb758d7f7300b9dbf83ef2728.
(cherry picked from commit c42d44cb2fc2e04dcaf04253d6f5bfa8b66a0264)

sonic_cli/web_server.py
@@ -12277,20 +12277,12 @@ def _safe_plugin_api_relpath(api_field: Any, *, dashboard_dir: Path) -> Optional
     return api_field
 
 
-# Plugin sources whose Python backend (dashboard manifest `api` file) must NEVER
-# be auto-imported by the dashboard web server — only bundled plugins may. Shared
-# by the discovery-time scrub and the mount-time refuse guards so a typo in one
-# site cannot silently disable a security gate (GHSA-5qr3-c538-wm9j / #43719).
-_NON_BUNDLED_PLUGIN_SOURCES = frozenset({"user", "project"})
-
-
 def _discover_dashboard_plugins() -> list:
     """Scan plugins/*/dashboard/manifest.json for dashboard extensions.
 
-    Checks three plugin sources. Bundled dashboard plugins win name conflicts
-    so non-bundled plugins cannot shadow trusted backend-capable routes:
-    1. Bundled plugins: <repo>/plugins/<name>/dashboard/manifest.json  (memory/, etc.)
-    2. User plugins:    ~/.sonic/plugins/<name>/dashboard/manifest.json
+    Checks three plugin sources (same as sonic_cli.plugins):
+    1. User plugins:    ~/.sonic/plugins/<name>/dashboard/manifest.json
+    2. Bundled plugins: <repo>/plugins/<name>/dashboard/manifest.json  (memory/, etc.)
     3. Project plugins: ./.sonic/plugins/  (only if SONIC_ENABLE_PROJECT_PLUGINS)
     """
     plugins = []
@@ -12299,9 +12291,9 @@ def _discover_dashboard_plugins() -> list:
     from sonic_cli.plugins import get_bundled_plugins_dir
     bundled_root = get_bundled_plugins_dir()
     search_dirs = [
+        (get_hermes_home() / "plugins", "user"),
         (bundled_root / "memory", "bundled"),
         (bundled_root, "bundled"),
-        (get_sonic_home() / "plugins", "user"),
     ]
     # GHSA-5qr3-c538-wm9j (#29156): the previous ``os.environ.get(...)``
     # check treated *any* non-empty string as truthy, so ``=0``, ``=false``,
@@ -12360,20 +12352,10 @@ def _discover_dashboard_plugins() -> list:
                 raw_api = data.get("api")
                 dashboard_dir = child / "dashboard"
                 safe_api = _safe_plugin_api_relpath(raw_api, dashboard_dir=dashboard_dir)
-                if source in _NON_BUNDLED_PLUGIN_SOURCES and safe_api:
-                    _log.warning(
-                        "Plugin %s: refusing dashboard backend api=%s "
-                        "(only bundled plugins may auto-import Python "
-                        "backend routes; non-bundled plugins may extend "
-                        "the dashboard with static UI assets only)",
-                        name, safe_api,
-                    )
-                    safe_api = None
-                    raw_api = None
                 if raw_api and safe_api is None:
                     _log.warning(
                         "Plugin %s: refusing unsafe api path %r (must be a "
-                        "relative file inside a bundled plugin's dashboard/ "
+                        "relative file inside the plugin's dashboard/ "
                         "directory); backend routes from this plugin will "
                         "not be mounted",
                         name, raw_api,
@@ -12780,36 +12762,23 @@ def _mount_plugin_api_routes():
     a ``router`` (FastAPI APIRouter).  Routes are mounted under
     ``/api/plugins/<name>/``.
 
-    Backend import is restricted to bundled plugins. User and project
-    plugins can extend the dashboard UI via static JS/CSS, but their
-    Python ``api`` files are never auto-imported by the web server.
-    See GHSA-5qr3-c538-wm9j (#29156) and #43719.
+    Backend import is restricted to ``bundled`` and ``user`` sources.
+    Project plugins (``./.hermes/plugins/``) ship with the CWD and are
+    therefore attacker-controlled in any threat model where the user
+    opens a malicious repo; they can extend the dashboard UI via
+    static JS/CSS but their Python ``api`` file is never auto-imported
+    by the web server.  See GHSA-5qr3-c538-wm9j (#29156).
     """
     for plugin in _get_dashboard_plugins():
         api_file_name = plugin.get("_api_file")
         if not api_file_name:
             continue
-        source = plugin.get("source")
-        if source in _NON_BUNDLED_PLUGIN_SOURCES:
-            # Backend Python auto-import is reserved for bundled plugins; user
-            # and project plugins extend the dashboard with static UI assets
-            # only (GHSA-5qr3-c538-wm9j / #43719). Defence-in-depth: discovery
-            # already nulls _api_file for these sources, but re-refusing here —
-            # at the actual importlib call site — keeps the import primitive
-            # contained even if a future caller or a tampered cache entry slips
-            # a non-bundled plugin through with an _api_file set.
-            _reason = {
-                "user": (
-                    "user-installed plugins may not auto-import Python code"
-                ),
-                "project": (
-                    "project plugins may not auto-import Python code; backend "
-                    "auto-import is reserved for bundled plugins"
-                ),
-            }.get(source, "only bundled plugins may auto-import Python code")
+        if plugin.get("source") == "project":
             _log.warning(
-                "Plugin %s: ignoring backend api=%s (%s)",
-                plugin["name"], api_file_name, _reason,
+                "Plugin %s: ignoring backend api=%s (project plugins may "
+                "not auto-import Python code; move the plugin to "
+                "~/.hermes/plugins/ if you trust it)",
+                plugin["name"], api_file_name,
             )
             continue
         dashboard_dir = Path(plugin["_dir"])

tests/sonic_cli/test_web_server.py
@@ -5095,8 +5095,14 @@ class TestPluginAPIAuth:
     """Tests that plugin API routes require the session token (issue #19533)."""
 
     @pytest.fixture(autouse=True)
-    def _setup_test_client(self, monkeypatch, _isolate_sonic_home):
-        """Create TestClients with and without the session token header."""
+    def _setup_test_client(self, monkeypatch, _isolate_sonic_home, _install_example_plugin):
+        """Create a TestClient without the session token header.
+
+        Pulls in ``_install_example_plugin`` so ``test_plugin_route_allows_auth``
+        has the ``/api/plugins/example/hello`` endpoint available — the
+        example plugin is no longer a bundled plugin, so the fixture
+        installs it into the per-test ``SONIC_HOME``.
+        """
         try:
             from starlette.testclient import TestClient
         except ImportError:
@@ -5121,15 +5127,19 @@ class TestPluginAPIAuth:
     def test_plugin_route_allows_auth(self):
         """Plugin API routes should work with a valid session token.
 
-        Uses a bundled plugin route so the test covers authenticated plugin
-        API access without relying on user-installed plugin backend imports.
+        Uses ``/api/plugins/example/hello`` from the example-dashboard
+        test fixture (installed into HERMES_HOME by the class-level
+        ``_install_example_plugin`` fixture) — a stable, side-effect-free
+        GET that's only loaded for tests. With a valid token the handler
+        should run (200); without one the middleware should 401 before
+        the handler is reached.
         """
         # Without auth: middleware blocks before reaching the handler.
-        resp = self.client.get("/api/plugins/kanban/board")
+        resp = self.client.get("/api/plugins/example/hello")
         assert resp.status_code == 401
 
         # With auth: handler runs.
-        resp = self.auth_client.get("/api/plugins/kanban/board")
+        resp = self.auth_client.get("/api/plugins/example/hello")
         assert resp.status_code == 200
 
     def test_plugin_post_requires_auth(self):