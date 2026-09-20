MFcv1/hermes-agent

fix(ci): rip out some xdist legacy stuff... how did these ever work??

tests/agent/test_skill_commands_reload.py
@@ -39,10 +39,9 @@ def _write_skill(skills_dir: Path, name: str, description: str = "") -> Path:
 def hermes_home(monkeypatch):
     """Isolate HERMES_HOME for ``reload_skills`` tests.
 
-    Rather than popping cache-bearing modules from ``sys.modules`` (which
-    races against pytest-xdist's parallel workers), we monkeypatch the
-    module-level ``HERMES_HOME`` / ``SKILLS_DIR`` constants in place so the
-    isolation is local to this fixture's scope.
+    Rather than popping cache-bearing modules from ``sys.modules``,
+    we monkeypatch the module-level ``HERMES_HOME`` / ``SKILLS_DIR``
+    constants in place so the isolation is local to this fixture's scope.
     """
     td = tempfile.mkdtemp(prefix="hermes-reload-skills-")
     monkeypatch.setenv("HERMES_HOME", td)

dabit3/sonic-agent

(cherry picked from commit bcc3eb3419d778a322d2ccb69f2c61885026a6e2)

tests/agent/test_skill_commands_reload.py
@@ -39,10 +39,9 @@ def _write_skill(skills_dir: Path, name: str, description: str = "") -> Path:
 def sonic_home(monkeypatch):
     """Isolate SONIC_HOME for ``reload_skills`` tests.
 
-    Rather than popping cache-bearing modules from ``sys.modules`` (which
-    races against pytest-xdist's parallel workers), we monkeypatch the
-    module-level ``SONIC_HOME`` / ``SKILLS_DIR`` constants in place so the
-    isolation is local to this fixture's scope.
+    Rather than popping cache-bearing modules from ``sys.modules``,
+    we monkeypatch the module-level ``SONIC_HOME`` / ``SKILLS_DIR``
+    constants in place so the isolation is local to this fixture's scope.
     """
     td = tempfile.mkdtemp(prefix="sonic-reload-skills-")
     monkeypatch.setenv("SONIC_HOME", td)