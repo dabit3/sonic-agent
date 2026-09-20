Carrie111998/Hermes

fix(dashboard-auth): exclude non-interactive providers from interactive login surfaces (#53239)
* Return None instead of erroring on drain login failure

* Fix login on drain

* Remove login for drained endpoints flow and clean the code

* chore: drop unrelated credits changes from this PR

* Remove extra comments that were not really necessary

hermes_cli/dashboard_auth/login_page.py
@@ -23,7 +23,7 @@ from __future__ import annotations
 
 import html
 
-from hermes_cli.dashboard_auth import list_providers
+from hermes_cli.dashboard_auth import list_session_providers
 
 # Inline minimal CSS. The dashboard's full skin lives in the React
 # bundle, which we deliberately do NOT load here — the login page must
@@ -465,7 +465,7 @@ def render_login_html(*, next_path: str = "") -> str:
     validating ``next_path`` against the same-origin rules before we
     emit it; we still HTML-escape it as defence in depth.
     """
-    providers = list_providers()
+    providers = list_session_providers()
     if not providers:
         return _EMPTY_HTML
 

hermes_cli/dashboard_auth/middleware.py
@@ -22,7 +22,7 @@ from typing import Awaitable, Callable
 from fastapi import Request
 from fastapi.responses import JSONResponse, RedirectResponse, Response
 
-from hermes_cli.dashboard_auth import list_providers
+from hermes_cli.dashboard_auth import list_session_providers
 from hermes_cli.dashboard_auth.audit import AuditEvent, audit_log
 from hermes_cli.dashboard_auth.base import ProviderError, RefreshExpiredError
 from hermes_cli.dashboard_auth.cookies import read_session_cookies
@@ -230,7 +230,7 @@ async def gated_auth_middleware(
         # 503 — distinguishing "transient IDP outage" (don't force re-login)
         # from "token genuinely invalid" (fall through to refresh/relogin).
         unreachable_provider: str | None = None
-        for provider in list_providers():
+        for provider in list_session_providers():
             try:
                 session = provider.verify_session(access_token=at)
             except ProviderError as e:
@@ -344,7 +344,7 @@ def _attempt_refresh(request: Request, *, refresh_token):
     """
     if not refresh_token:
         return None
-    for provider in list_providers():
+    for provider in list_session_providers():
         try:
             new_session = provider.refresh_session(refresh_token=refresh_token)
         except RefreshExpiredError:

dabit3/sonic-agent

(cherry picked from commit dbe734beff0caf5e8ee2acbe4277db7f6cf84a21)

sonic_cli/dashboard_auth/login_page.py
@@ -23,7 +23,7 @@ from __future__ import annotations
 
 import html
 
-from sonic_cli.dashboard_auth import list_providers
+from sonic_cli.dashboard_auth import list_session_providers
 
 # Inline minimal CSS. The dashboard's full skin lives in the React
 # bundle, which we deliberately do NOT load here — the login page must
@@ -465,7 +465,7 @@ def render_login_html(*, next_path: str = "") -> str:
     validating ``next_path`` against the same-origin rules before we
     emit it; we still HTML-escape it as defence in depth.
     """
-    providers = list_providers()
+    providers = list_session_providers()
     if not providers:
         return _EMPTY_HTML
 

sonic_cli/dashboard_auth/middleware.py
@@ -22,7 +22,7 @@ from typing import Awaitable, Callable
 from fastapi import Request
 from fastapi.responses import JSONResponse, RedirectResponse, Response
 
-from sonic_cli.dashboard_auth import list_providers
+from sonic_cli.dashboard_auth import list_session_providers
 from sonic_cli.dashboard_auth.audit import AuditEvent, audit_log
 from sonic_cli.dashboard_auth.base import ProviderError, RefreshExpiredError
 from sonic_cli.dashboard_auth.cookies import read_session_cookies
@@ -230,7 +230,7 @@ async def gated_auth_middleware(
         # 503 — distinguishing "transient IDP outage" (don't force re-login)
         # from "token genuinely invalid" (fall through to refresh/relogin).
         unreachable_provider: str | None = None
-        for provider in list_providers():
+        for provider in list_session_providers():
             try:
                 session = provider.verify_session(access_token=at)
             except ProviderError as e:
@@ -344,7 +344,7 @@ def _attempt_refresh(request: Request, *, refresh_token):
     """
     if not refresh_token:
         return None
-    for provider in list_providers():
+    for provider in list_session_providers():
         try:
             new_session = provider.refresh_session(refresh_token=refresh_token)
         except RefreshExpiredError: