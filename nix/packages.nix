kojira/hermes-agent

change(nix): ship fat hermes agent by default

nix/packages.nix
@@ -2,54 +2,62 @@
 { inputs, ... }:
 {
   perSystem =
-    { pkgs, lib, inputs', ... }:
+    {
+      pkgs,
+      lib,
+      inputs',
+      ...
+    }:
     let
-      hermesAgent = pkgs.callPackage ./hermes-agent.nix {
+      minimal = pkgs.callPackage ./hermes-agent.nix {
         inherit (inputs) uv2nix pyproject-nix pyproject-build-systems;
         npm-lockfile-fix = inputs'.npm-lockfile-fix.packages.default;
         # Only embed clean revs — dirtyRev doesn't represent any upstream
         # commit, so comparing it would always claim "update available".
         rev = inputs.self.rev or null;
       };
+
+      # All platform-portable optional integrations pre-built.
+      full = minimal.override {
+        extraDependencyGroups = [
+          "anthropic"
+          "azure-identity"
+          "bedrock"
+          "daytona"
+          "dingtalk"
+          "edge-tts"
+          "exa"
+          "fal"
+          "feishu"
+          "firecrawl"
+          "hindsight"
+          "honcho"
+          "messaging"
+          "modal"
+          "parallel-web"
+          "tts-premium"
+          "voice"
+        ]
+        # matrix is Linux-only (oqs/liboqs lacks aarch64-darwin wheels).
+        ++ lib.optionals pkgs.stdenv.isLinux [ "matrix" ];
+      };
     in
     {
       packages = {
-        default = hermesAgent;
+        default = full;
+
+        inherit minimal;
 
         # Ships discord.py + python-telegram-bot + slack-sdk so a plain
         # `nix profile install .#messaging` connects to Discord/Telegram/Slack
         # on first run — lazy-install can't write to the read-only /nix/store.
-        messaging = hermesAgent.override {
+        messaging = minimal.override {
           extraDependencyGroups = [ "messaging" ];
         };
 
-        # All platform-portable optional integrations pre-built.
-        # matrix is Linux-only (oqs/liboqs lacks aarch64-darwin wheels).
-        full = hermesAgent.override {
-          extraDependencyGroups = [
-            "anthropic"
-            "azure-identity"
-            "bedrock"
-            "daytona"
-            "dingtalk"
-            "edge-tts"
-            "exa"
-            "fal"
-            "feishu"
-            "firecrawl"
-            "hindsight"
-            "honcho"
-            "messaging"
-            "modal"
-            "parallel-web"
-            "tts-premium"
-            "voice"
-          ] ++ lib.optionals pkgs.stdenv.isLinux [ "matrix" ];
-        };
-
-        tui = hermesAgent.hermesTui;
-        web = hermesAgent.hermesWeb;
-        desktop = hermesAgent.hermesDesktop;
+        tui = full.hermesTui;
+        web = full.hermesWeb;
+        desktop = full.hermesDesktop;
       };
     };
 }

dabit3/sonic-agent

(cherry picked from commit 8ae793d3deb4b0048bffedb2e9881db7c9e9b65c)

nix/packages.nix
@@ -2,54 +2,62 @@
 { inputs, ... }:
 {
   perSystem =
-    { pkgs, lib, inputs', ... }:
+    {
+      pkgs,
+      lib,
+      inputs',
+      ...
+    }:
     let
-      sonicAgent = pkgs.callPackage ./sonic-agent.nix {
+      minimal = pkgs.callPackage ./sonic-agent.nix {
         inherit (inputs) uv2nix pyproject-nix pyproject-build-systems;
         npm-lockfile-fix = inputs'.npm-lockfile-fix.packages.default;
         # Only embed clean revs — dirtyRev doesn't represent any upstream
         # commit, so comparing it would always claim "update available".
         rev = inputs.self.rev or null;
       };
+
+      # All platform-portable optional integrations pre-built.
+      full = minimal.override {
+        extraDependencyGroups = [
+          "anthropic"
+          "azure-identity"
+          "bedrock"
+          "daytona"
+          "dingtalk"
+          "edge-tts"
+          "exa"
+          "fal"
+          "feishu"
+          "firecrawl"
+          "hindsight"
+          "honcho"
+          "messaging"
+          "modal"
+          "parallel-web"
+          "tts-premium"
+          "voice"
+        ]
+        # matrix is Linux-only (oqs/liboqs lacks aarch64-darwin wheels).
+        ++ lib.optionals pkgs.stdenv.isLinux [ "matrix" ];
+      };
     in
     {
       packages = {
-        default = sonicAgent;
+        default = full;
+
+        inherit minimal;
 
         # Ships discord.py + python-telegram-bot + slack-sdk so a plain
         # `nix profile install .#messaging` connects to Discord/Telegram/Slack
         # on first run — lazy-install can't write to the read-only /nix/store.
-        messaging = sonicAgent.override {
+        messaging = minimal.override {
           extraDependencyGroups = [ "messaging" ];
         };
 
-        # All platform-portable optional integrations pre-built.
-        # matrix is Linux-only (oqs/liboqs lacks aarch64-darwin wheels).
-        full = sonicAgent.override {
-          extraDependencyGroups = [
-            "anthropic"
-            "azure-identity"
-            "bedrock"
-            "daytona"
-            "dingtalk"
-            "edge-tts"
-            "exa"
-            "fal"
-            "feishu"
-            "firecrawl"
-            "hindsight"
-            "honcho"
-            "messaging"
-            "modal"
-            "parallel-web"
-            "tts-premium"
-            "voice"
-          ] ++ lib.optionals pkgs.stdenv.isLinux [ "matrix" ];
-        };
-
-        tui = sonicAgent.sonicTui;
-        web = sonicAgent.sonicWeb;
-        desktop = sonicAgent.sonicDesktop;
+        tui = full.sonicTui;
+        web = full.sonicWeb;
+        desktop = full.sonicDesktop;
       };
     };
 }