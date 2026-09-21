ksergk/hermes-agent-fork

revert(windows): roll back terminal-popup PRs #53791 #53810 #53829 (#53853)
* Revert "fix(windows): capture is not a no-window boundary; route flashing spawns through chokepoint (#53829)"

This reverts commit 2ecca1e7d3e7c387b4d350f99dc699e2f43f73c5.

* Revert "fix(windows): stop terminal-window popups from background spawns (#53810)"

This reverts commit 5db1430af9ecd7ecc1e5eba93e7e1e965af59927.

* Revert "fix(windows): stop subprocess console-window popups + add CI guard (#53791)"

This reverts commit ef17cd204d7583c31fe1ace9255077f8c736b47e.

apps/desktop/electron/main.cjs
@@ -1565,16 +1565,17 @@ function readVenvHome(venvRoot) {
 function getNoConsoleVenvPython(venvRoot) {
   if (!IS_WINDOWS) return getVenvPython(venvRoot)
 
-  // uv venv launchers can re-exec console python.exe, which allocates conhost /
-  // Windows Terminal. Use base pythonw directly and provide imports via env.
+  // Prefer the venv's own pythonw shim — it carries pyvenv.cfg / site-packages
+  // wiring. Falling back to the base uv/python.org pythonw.exe skips the venv
+  // and breaks imports (yaml, hermes_cli, …) even when PYTHONPATH is patched.
+  const venvPythonw = path.join(venvRoot, 'Scripts', 'pythonw.exe')
+  if (fileExists(venvPythonw)) return venvPythonw
+
   const baseHome = readVenvHome(venvRoot)
   if (baseHome) {
     const basePythonw = path.join(baseHome, 'pythonw.exe')
     if (fileExists(basePythonw)) return basePythonw
   }
-
-  const venvPythonw = path.join(venvRoot, 'Scripts', 'pythonw.exe')
-  if (fileExists(venvPythonw)) return venvPythonw
 
   return venvPythonw
 }
@@ -2796,7 +2797,7 @@ function createPythonBackend(root, label, dashboardArgs, options = {}) {
     args: ['-m', 'hermes_cli.main', ...dashboardArgs],
     env: buildDesktopBackendEnv({
       hermesHome: HERMES_HOME,
-      pythonPathEntries: [root, ...getVenvSitePackagesEntries(venvRoot)],
+      pythonPathEntries: [root],
       venvRoot
     }),
     root,
@@ -2820,7 +2821,7 @@ function createActiveBackend(dashboardArgs) {
     args: ['-m', 'hermes_cli.main', ...dashboardArgs],
     env: buildDesktopBackendEnv({
       hermesHome: HERMES_HOME,
-      pythonPathEntries: [ACTIVE_HERMES_ROOT, ...getVenvSitePackagesEntries(VENV_ROOT)],
+      pythonPathEntries: [ACTIVE_HERMES_ROOT],
       venvRoot: VENV_ROOT
     }),
     root: ACTIVE_HERMES_ROOT,

tests/hermes_cli/test_gateway.py
@@ -887,20 +887,23 @@ def test_reap_unsupervised_orphans_returns_false_when_none_found(monkeypatch):
 
 
 def test_scan_gateway_pids_detects_windows_hermes_exe_case_variants(monkeypatch):
-    # Windows scan now goes through psutil first (no console spawn). A
-    # uppercase ``Hermes.EXE gateway run`` must still match case-insensitively.
-    import psutil
-
     monkeypatch.setattr(gateway, "is_windows", lambda: True)
     monkeypatch.setattr(gateway, "_get_ancestor_pids", lambda: set())
+    monkeypatch.setattr(gateway.shutil, "which", lambda name: "wmic.exe" if name == "wmic" else None)
 
-    proc = SimpleNamespace(
-        info={
-            "pid": 2468,
-            "cmdline": ["C:\\Program Files\\Hermes\\Hermes.EXE", "gateway", "run", "--replace"],
-        }
-    )
-    monkeypatch.setattr(psutil, "process_iter", lambda attrs=None: [proc])
+    def fake_run(cmd, **kwargs):
+        if cmd[:4] == ["wmic.exe", "process", "get", "ProcessId,CommandLine"]:
+            return SimpleNamespace(
+                returncode=0,
+                stdout=(
+                    "CommandLine=C:\\Program Files\\Hermes\\Hermes.EXE gateway run --replace\n"
+                    "ProcessId=2468\n\n"
+                ),
+                stderr="",
+            )
+        raise AssertionError(f"Unexpected command: {cmd}")
+
+    monkeypatch.setattr(gateway.subprocess, "run", fake_run)
 
     assert gateway._scan_gateway_pids(set(), all_profiles=True) == [2468]
 

tools/browser_tool.py
@@ -68,7 +68,7 @@ from agent.auxiliary_client import call_llm
 from hermes_constants import agent_browser_runnable, get_hermes_home
 from utils import env_int, is_truthy_value
 from hermes_cli.config import DEFAULT_CONFIG, cfg_get
-from hermes_cli import _subprocess_compat
+from hermes_cli._subprocess_compat import windows_hide_flags
 
 try:
     from tools.website_policy import check_website_access
@@ -905,19 +905,23 @@ def _run_chrome_fallback_command(
             #   fileno=1 (stderr dup'd onto stdout at the OS level).
             # * close_fds=True → block inheritance of every other handle.
             #   (Default on POSIX; must be explicit on Windows for stdio.)
-            # CREATE_NO_WINDOW is applied by _subprocess_compat.popen. We do NOT
-            # add CREATE_NEW_PROCESS_GROUP: on Python 3.11 Windows it interacts
-            # with asyncio's ProactorEventLoop such that the subprocess creation
-            # cancels the running loop task, surfacing as KeyboardInterrupt in
-            # app.run() and tearing down the CLI mid-turn (diag:
-            # "asyncio.CancelledError → KeyboardInterrupt").
             _popen_extra: dict = {}
             if os.name == "nt":
+                # CREATE_NO_WINDOW → don't attach a console (cmd.exe would
+                # otherwise briefly allocate one for the .cmd shim).
+                # Do NOT add CREATE_NEW_PROCESS_GROUP: on Python 3.11 Windows
+                # it interacts with asyncio's ProactorEventLoop such that the
+                # subprocess creation cancels the running loop task, which
+                # surfaces as KeyboardInterrupt in app.run() and tears down
+                # the CLI mid-turn. The agent thread's subprocess spawn
+                # unwound MainThread's prompt_toolkit loop that way — see
+                # diag log: "asyncio.CancelledError → KeyboardInterrupt".
+                _popen_extra["creationflags"] = windows_hide_flags()
                 _popen_extra["close_fds"] = True
                 _si = subprocess.STARTUPINFO()
                 _si.dwFlags |= subprocess.STARTF_USESTDHANDLES
                 _popen_extra["startupinfo"] = _si
-            proc = _subprocess_compat.popen(
+            proc = subprocess.Popen(
                 full, stdout=stdout_fd, stderr=stderr_fd,
                 stdin=subprocess.DEVNULL, env=browser_env,
                 **_popen_extra,
@@ -1938,12 +1942,6 @@ def _find_agent_browser() -> str:
     repo_root = Path(__file__).parent.parent
     local_bin_dir = repo_root / "node_modules" / ".bin"
     if local_bin_dir.is_dir():
-        if sys.platform == "win32":
-            native = repo_root / "node_modules" / "agent-browser" / "bin" / "agent-browser-win32-x64.exe"
-            if native.exists() and agent_browser_runnable(str(native)):
-                _cached_agent_browser = str(native)
-                _agent_browser_resolved = True
-                return _cached_agent_browser
         local_which = shutil.which("agent-browser", path=str(local_bin_dir))
         if local_which and agent_browser_runnable(local_which):
             _cached_agent_browser = local_which
@@ -2193,16 +2191,17 @@ def _run_browser_command(
             # three explicit handles (no leaked parent-console handles to
             # confuse the Rust binary's daemon-spawn), and close_fds=True to
             # block inheritance of everything else.
-            # CREATE_NO_WINDOW via _subprocess_compat.popen; NO
-            # CREATE_NEW_PROCESS_GROUP (cancels asyncio loop task on Python 3.11
-            # Windows → KeyboardInterrupt in CLI MainThread).
             _popen_extra: dict = {}
             if os.name == "nt":
+                # See matching block at the other Popen site — CREATE_NO_WINDOW
+                # only, NO CREATE_NEW_PROCESS_GROUP (cancels asyncio loop task
+                # on Python 3.11 Windows → KeyboardInterrupt in CLI MainThread).
+                _popen_extra["creationflags"] = windows_hide_flags()
                 _popen_extra["close_fds"] = True
                 _si = subprocess.STARTUPINFO()
                 _si.dwFlags |= subprocess.STARTF_USESTDHANDLES
                 _popen_extra["startupinfo"] = _si
-            proc = _subprocess_compat.popen(
+            proc = subprocess.Popen(
                 cmd_parts,
                 stdout=stdout_fd,
                 stderr=stderr_fd,

dabit3/sonic-agent

(cherry picked from commit d3d621f7c38bb801d9d734cb2898bd4f9b134709)

apps/desktop/electron/main.cjs
@@ -1565,16 +1565,17 @@ function readVenvHome(venvRoot) {
 function getNoConsoleVenvPython(venvRoot) {
   if (!IS_WINDOWS) return getVenvPython(venvRoot)
 
-  // uv venv launchers can re-exec console python.exe, which allocates conhost /
-  // Windows Terminal. Use base pythonw directly and provide imports via env.
+  // Prefer the venv's own pythonw shim — it carries pyvenv.cfg / site-packages
+  // wiring. Falling back to the base uv/python.org pythonw.exe skips the venv
+  // and breaks imports (yaml, hermes_cli, …) even when PYTHONPATH is patched.
+  const venvPythonw = path.join(venvRoot, 'Scripts', 'pythonw.exe')
+  if (fileExists(venvPythonw)) return venvPythonw
+
   const baseHome = readVenvHome(venvRoot)
   if (baseHome) {
     const basePythonw = path.join(baseHome, 'pythonw.exe')
     if (fileExists(basePythonw)) return basePythonw
   }
-
-  const venvPythonw = path.join(venvRoot, 'Scripts', 'pythonw.exe')
-  if (fileExists(venvPythonw)) return venvPythonw
 
   return venvPythonw
 }
@@ -2796,7 +2797,7 @@ function createPythonBackend(root, label, dashboardArgs, options = {}) {
     args: ['-m', 'sonic_cli.main', ...dashboardArgs],
     env: buildDesktopBackendEnv({
       sonicHome: SONIC_HOME,
-      pythonPathEntries: [root, ...getVenvSitePackagesEntries(venvRoot)],
+      pythonPathEntries: [root],
       venvRoot
     }),
     root,
@@ -2820,7 +2821,7 @@ function createActiveBackend(dashboardArgs) {
     args: ['-m', 'sonic_cli.main', ...dashboardArgs],
     env: buildDesktopBackendEnv({
       sonicHome: SONIC_HOME,
-      pythonPathEntries: [ACTIVE_SONIC_ROOT, ...getVenvSitePackagesEntries(VENV_ROOT)],
+      pythonPathEntries: [ACTIVE_SONIC_ROOT],
       venvRoot: VENV_ROOT
     }),
     root: ACTIVE_SONIC_ROOT,

tests/sonic_cli/test_gateway.py
@@ -887,20 +887,23 @@ def test_reap_unsupervised_orphans_returns_false_when_none_found(monkeypatch):
 
 
 def test_scan_gateway_pids_detects_windows_sonic_exe_case_variants(monkeypatch):
-    # Windows scan now goes through psutil first (no console spawn). A
-    # uppercase ``Sonic.EXE gateway run`` must still match case-insensitively.
-    import psutil
-
     monkeypatch.setattr(gateway, "is_windows", lambda: True)
     monkeypatch.setattr(gateway, "_get_ancestor_pids", lambda: set())
+    monkeypatch.setattr(gateway.shutil, "which", lambda name: "wmic.exe" if name == "wmic" else None)
 
-    proc = SimpleNamespace(
-        info={
-            "pid": 2468,
-            "cmdline": ["C:\\Program Files\\Sonic\\Sonic.EXE", "gateway", "run", "--replace"],
-        }
-    )
-    monkeypatch.setattr(psutil, "process_iter", lambda attrs=None: [proc])
+    def fake_run(cmd, **kwargs):
+        if cmd[:4] == ["wmic.exe", "process", "get", "ProcessId,CommandLine"]:
+            return SimpleNamespace(
+                returncode=0,
+                stdout=(
+                    "CommandLine=C:\\Program Files\\Sonic\\Sonic.EXE gateway run --replace\n"
+                    "ProcessId=2468\n\n"
+                ),
+                stderr="",
+            )
+        raise AssertionError(f"Unexpected command: {cmd}")
+
+    monkeypatch.setattr(gateway.subprocess, "run", fake_run)
 
     assert gateway._scan_gateway_pids(set(), all_profiles=True) == [2468]
 

tools/browser_tool.py
@@ -68,7 +68,7 @@ from agent.auxiliary_client import call_llm
 from sonic_constants import agent_browser_runnable, get_sonic_home
 from utils import env_int, is_truthy_value
 from sonic_cli.config import DEFAULT_CONFIG, cfg_get
-from sonic_cli import _subprocess_compat
+from sonic_cli._subprocess_compat import windows_hide_flags
 
 try:
     from tools.website_policy import check_website_access
@@ -905,19 +905,23 @@ def _run_chrome_fallback_command(
             #   fileno=1 (stderr dup'd onto stdout at the OS level).
             # * close_fds=True → block inheritance of every other handle.
             #   (Default on POSIX; must be explicit on Windows for stdio.)
-            # CREATE_NO_WINDOW is applied by _subprocess_compat.popen. We do NOT
-            # add CREATE_NEW_PROCESS_GROUP: on Python 3.11 Windows it interacts
-            # with asyncio's ProactorEventLoop such that the subprocess creation
-            # cancels the running loop task, surfacing as KeyboardInterrupt in
-            # app.run() and tearing down the CLI mid-turn (diag:
-            # "asyncio.CancelledError → KeyboardInterrupt").
             _popen_extra: dict = {}
             if os.name == "nt":
+                # CREATE_NO_WINDOW → don't attach a console (cmd.exe would
+                # otherwise briefly allocate one for the .cmd shim).
+                # Do NOT add CREATE_NEW_PROCESS_GROUP: on Python 3.11 Windows
+                # it interacts with asyncio's ProactorEventLoop such that the
+                # subprocess creation cancels the running loop task, which
+                # surfaces as KeyboardInterrupt in app.run() and tears down
+                # the CLI mid-turn. The agent thread's subprocess spawn
+                # unwound MainThread's prompt_toolkit loop that way — see
+                # diag log: "asyncio.CancelledError → KeyboardInterrupt".
+                _popen_extra["creationflags"] = windows_hide_flags()
                 _popen_extra["close_fds"] = True
                 _si = subprocess.STARTUPINFO()
                 _si.dwFlags |= subprocess.STARTF_USESTDHANDLES
                 _popen_extra["startupinfo"] = _si
-            proc = _subprocess_compat.popen(
+            proc = subprocess.Popen(
                 full, stdout=stdout_fd, stderr=stderr_fd,
                 stdin=subprocess.DEVNULL, env=browser_env,
                 **_popen_extra,
@@ -1938,12 +1942,6 @@ def _find_agent_browser() -> str:
     repo_root = Path(__file__).parent.parent
     local_bin_dir = repo_root / "node_modules" / ".bin"
     if local_bin_dir.is_dir():
-        if sys.platform == "win32":
-            native = repo_root / "node_modules" / "agent-browser" / "bin" / "agent-browser-win32-x64.exe"
-            if native.exists() and agent_browser_runnable(str(native)):
-                _cached_agent_browser = str(native)
-                _agent_browser_resolved = True
-                return _cached_agent_browser
         local_which = shutil.which("agent-browser", path=str(local_bin_dir))
         if local_which and agent_browser_runnable(local_which):
             _cached_agent_browser = local_which
@@ -2193,16 +2191,17 @@ def _run_browser_command(
             # three explicit handles (no leaked parent-console handles to
             # confuse the Rust binary's daemon-spawn), and close_fds=True to
             # block inheritance of everything else.
-            # CREATE_NO_WINDOW via _subprocess_compat.popen; NO
-            # CREATE_NEW_PROCESS_GROUP (cancels asyncio loop task on Python 3.11
-            # Windows → KeyboardInterrupt in CLI MainThread).
             _popen_extra: dict = {}
             if os.name == "nt":
+                # See matching block at the other Popen site — CREATE_NO_WINDOW
+                # only, NO CREATE_NEW_PROCESS_GROUP (cancels asyncio loop task
+                # on Python 3.11 Windows → KeyboardInterrupt in CLI MainThread).
+                _popen_extra["creationflags"] = windows_hide_flags()
                 _popen_extra["close_fds"] = True
                 _si = subprocess.STARTUPINFO()
                 _si.dwFlags |= subprocess.STARTF_USESTDHANDLES
                 _popen_extra["startupinfo"] = _si
-            proc = _subprocess_compat.popen(
+            proc = subprocess.Popen(
                 cmd_parts,
                 stdout=stdout_fd,
                 stderr=stderr_fd,