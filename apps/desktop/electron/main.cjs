Carrie111998/Hermes

fix(windows): stop terminal-window popups from background spawns (#53810)
* fix(windows): stop terminal-window popups from background spawns

Native-Windows desktop/gateway users saw cmd/conhost windows flash on
gateway restart, image paste, the dashboard Projects tree, voice notes,
and ~5 min after closing the app (detached cron). Two root causes:

- Console-subsystem exes (taskkill, schtasks, wmic, netstat, tasklist,
  agent-browser, git, ffmpeg, powershell, git-bash) spawned via raw
  subprocess allocate a fresh console when the launching process has
  none (pythonw desktop backend / detached gateway) - even with output
  captured.
- uv venv pythonw shims re-exec console python.exe, so Python children
  get a console regardless of how they're launched.

Fixes:
- Single hidden-spawn primitive (_subprocess_compat.run/.popen) that ORs
  CREATE_NO_WINDOW on Windows, no-op on POSIX. Route every Hermes-owned
  console-exe spawn through it.
- FreeConsole() catch-all in hermes_bootstrap: any Python child that
  exclusively owns an auto-allocated console detaches it at startup
  (GetConsoleProcessList()==1 gate leaves shared interactive consoles
  untouched).
- Replace PowerShell/wmic gateway PID scans with in-process psutil.
- Skip schtasks queries on non-interactive desktop restarts.
- Prefer native agent-browser .exe over .cmd shims.
- Guard test bans raw subprocess spawns of the Windows-only console
  tools repo-wide so the popup class can't regress.

* fix(windows): scope FreeConsole to background entry points; fix merge fallout

Console detach review (per #53810 feedback): GetConsoleProcessList()==1 can't
tell a uv pythonw->python phantom console apart from a user opening the
interactive CLI/TUI in its own fresh console (double-click, shortcut, ConPTY) —
both report a single attached process with a tty. Running FreeConsole() in the
import-time bootstrap therefore risked detaching a legitimately-interactive
terminal.

- Extract FreeConsole into explicit hermes_bootst

apps/desktop/electron/main.cjs
@@ -1565,17 +1565,16 @@ function readVenvHome(venvRoot) {
 function getNoConsoleVenvPython(venvRoot) {
   if (!IS_WINDOWS) return getVenvPython(venvRoot)
 
-  // Prefer the venv's own pythonw shim — it carries pyvenv.cfg / site-packages
-  // wiring. Falling back to the base uv/python.org pythonw.exe skips the venv
-  // and breaks imports (yaml, hermes_cli, …) even when PYTHONPATH is patched.
-  const venvPythonw = path.join(venvRoot, 'Scripts', 'pythonw.exe')
-  if (fileExists(venvPythonw)) return venvPythonw
-
+  // uv venv launchers can re-exec console python.exe, which allocates conhost /
+  // Windows Terminal. Use base pythonw directly and provide imports via env.
   const baseHome = readVenvHome(venvRoot)
   if (baseHome) {
     const basePythonw = path.join(baseHome, 'pythonw.exe')
     if (fileExists(basePythonw)) return basePythonw
   }
+
+  const venvPythonw = path.join(venvRoot, 'Scripts', 'pythonw.exe')
+  if (fileExists(venvPythonw)) return venvPythonw
 
   return venvPythonw
 }
@@ -2797,7 +2796,7 @@ function createPythonBackend(root, label, dashboardArgs, options = {}) {
     args: ['-m', 'hermes_cli.main', ...dashboardArgs],
     env: buildDesktopBackendEnv({
       hermesHome: HERMES_HOME,
-      pythonPathEntries: [root],
+      pythonPathEntries: [root, ...getVenvSitePackagesEntries(venvRoot)],
       venvRoot
     }),
     root,
@@ -2821,7 +2820,7 @@ function createActiveBackend(dashboardArgs) {
     args: ['-m', 'hermes_cli.main', ...dashboardArgs],
     env: buildDesktopBackendEnv({
       hermesHome: HERMES_HOME,
-      pythonPathEntries: [ACTIVE_HERMES_ROOT],
+      pythonPathEntries: [ACTIVE_HERMES_ROOT, ...getVenvSitePackagesEntries(VENV_ROOT)],
       venvRoot: VENV_ROOT
     }),
     root: ACTIVE_HERMES_ROOT,

hermes_cli/gateway_windows.py
@@ -39,10 +39,10 @@ import time
 from pathlib import Path
 from xml.sax.saxutils import escape
 
+from hermes_cli import _subprocess_compat
 from hermes_cli._subprocess_compat import (
     windows_detach_flags,
     windows_detach_flags_without_breakaway,
-    windows_hide_flags,
 )
 
 # Short timeouts: schtasks occasionally wedges and we don't want to hang forever.
@@ -157,7 +157,7 @@ def _exec_schtasks(args: list[str]) -> tuple[int, str, str]:
     if schtasks is None:
         return (1, "", "schtasks.exe not found on PATH")
     try:
-        proc = subprocess.run(
+        proc = _subprocess_compat.run(
             [schtasks, *args],
             capture_output=True,
             text=True,
@@ -168,10 +168,6 @@ def _exec_schtasks(args: list[str]) -> tuple[int, str, str]:
             encoding=_schtasks_encoding(),
             errors="replace",
             timeout=_SCHTASKS_TIMEOUT_S,
-            # CREATE_NO_WINDOW avoids a flashing console window when the CLI
-            # is itself hosted in a TUI. See tools/browser_tool.py for the
-            # same pattern and the windows-subprocess-sigint-storm.md ref.
-            creationflags=windows_hide_flags(),
         )
         return (proc.returncode, proc.stdout or "", proc.stderr or "")
     except subprocess.TimeoutExpired:
@@ -1605,7 +1601,17 @@ def stop() -> None:
         drained = _drain_gateway_pid(pid, _windows_stop_drain_timeout())
 
     stopped_any = drained
-    if is_task_registered():
+    has_service_artifact = (
+        get_task_script_path().exists()
+        or get_task_script_path().with_suffix(".vbs").exists()
+        or get_startup_entry_path().exists()
+        or _legacy_startup_entry_path().exists()
+    )
+    if (
+        has_service_artifact
+        and os.getenv("HERMES_NONINTERACTIVE") != "1"
+        and is_task_registered()
+    ):
         code, _out, err = _exec_schtasks(["/End", "/TN", get_task_name()])
         # schtasks returns nonzero when the task isn't currently running — don't treat that as an error.
         if code == 0:
@@ -1673,7 +1679,8 @@ def restart() -> None:
 
     # Give Windows a moment to release the listening port.
     time.sleep(1.0)
-    start()
+    pid = _spawn_detached()
+    _report_gateway_start(f"direct spawn (PID {pid})")
 
     if not _wait_for_gateway_ready(timeout_s=15.0):
         raise RuntimeError(

hermes_cli/web_server.py
@@ -2481,16 +2481,52 @@ def _record_completed_action(name: str, message: str, exit_code: int = 1) -> Non
     _ACTION_RESULTS[name] = {"exit_code": exit_code, "pid": None}
 
 
-def _dashboard_spawn_executable() -> str:
-    """Prefer pythonw.exe for detached dashboard actions on Windows."""
+def _dashboard_spawn_details() -> Tuple[str, Dict[str, str]]:
+    """Return (executable, env overlay) for detached dashboard actions.
+
+    On Windows this mirrors the gateway's uv-safe detached launcher logic so
+    action spawns do not regress to console python.exe (which creates a visible
+    terminal window). Non-Windows callsites get the current interpreter and no
+    env overlay.
+    """
     if sys.platform != "win32":
-        return sys.executable
+        return sys.executable, {}
+
     exe = sys.executable
-    if exe.lower().endswith("python.exe"):
-        pythonw = os.path.join(os.path.dirname(exe), "pythonw.exe")
-        if os.path.isfile(pythonw):
-            return pythonw
-    return exe
+    try:
+        from hermes_cli.gateway_windows import _resolve_detached_python
+
+        venv_root = os.environ.get("VIRTUAL_ENV", "").strip()
+        if not venv_root:
+            for candidate in (PROJECT_ROOT / "venv", PROJECT_ROOT / ".venv"):
+                if (candidate / "Scripts" / "python.exe").exists():
+                    venv_root = str(candidate)
+                    break
+        probe_exe = (
+            os.path.join(venv_root, "Scripts", "python.exe")
+            if venv_root
+            else exe
+        )
+        windowless_exe, venv_dir, extra_pythonpath = _resolve_detached_python(probe_exe)
+        env_overlay: Dict[str, str] = {}
+        if venv_dir:
+            env_overlay["VIRTUAL_ENV"] = str(venv_dir)
+            site_packages = Path(venv_dir) / "Lib" / "site-packages"
+            if site_packages.exists() and str(site_packages) not in extra_pythonpath:
+                extra_pythonpath = [*extra_pythonpath, str(site_packages)]
+        if extra_pythonpath:
+            existing = os.environ.get("PYTHONPATH", "")
+            env_overlay["PYTHONPATH"] = os.pathsep.join(
+                [*extra_pythonpath, existing] if existing else list(extra_pythonpath)
+            )
+        return windowless_exe, env_overlay
+    except Exception:
+        # Best-effort fallback: sibling pythonw keeps the legacy no-console path.
+        if exe.lower().endswith("python.exe"):
+            pythonw = os.path.join(os.path.dirname(exe), "pythonw.exe")
+            if os.path.isfile(pythonw):
+                return pythonw, {}
+    return exe, {}
 
 
 def _spawn_hermes_action(subcommand: List[str], name: str) -> subprocess.Popen:
@@ -2507,15 +2543,20 @@ def _spawn_hermes_action(subcommand: List[str], name: str) -> subprocess.Popen:
         f"\n=== {name} started {time.strftime('%Y-%m-%d %H:%M:%S')} ===\n".encode()
     )
 
-    cmd = [_dashboard_spawn_executable(), "-m", "hermes_cli.main", *subcommand]
+    spawn_executable, spawn_env_overlay = _dashboard_spawn_details()
+    cmd = [spawn_executable, "-m", "hermes_cli.main", *subcommand]
 
     popen_kwargs: Dict[str, Any] = {
         "cwd": str(PROJECT_ROOT),
         "stdin": subprocess.DEVNULL,
         "stdout": log_file,
         "stderr": subprocess.STDOUT,
-        "env": {**os.environ, "HERMES_NONINTERACTIVE": "1"},
+        "env": {**os.environ, "HERMES_NONINTERACTIVE": "1", **spawn_env_overlay},
     }
+    log_file.write(f"spawn executable: {spawn_executable}\n".encode())
+    if spawn_env_overlay:
+        keys = ",".join(sorted(spawn_env_overlay.keys()))
+        log_file.write(f"spawn env overlay keys: {keys}\n".encode())
     if sys.platform == "win32":
         popen_kwargs["creationflags"] = windows_detach_flags()
     else:
@@ -13547,6 +13588,15 @@ def start_server(
     — used when a profile alias (``<profile> dashboard``) routes to the
     machine dashboard.
     """
+    # Desktop spawns this backend via a no-console venv python; a uv
+    # pythonw→python re-exec can still auto-allocate a console. Drop it.
+    # No-op on POSIX and when launched from an interactive shell.
+    try:
+        import hermes_bootstrap
+        hermes_bootstrap.detach_orphan_console()
+    except Exception:
+        pass
+
     import uvicorn
 
     try:

tools/browser_tool.py
@@ -68,7 +68,7 @@ from agent.auxiliary_client import call_llm
 from hermes_constants import agent_browser_runnable, get_hermes_home
 from utils import env_int, is_truthy_value
 from hermes_cli.config import DEFAULT_CONFIG, cfg_get
-from hermes_cli._subprocess_compat import windows_hide_flags
+from hermes_cli import _subprocess_compat
 
 try:
     from tools.website_policy import check_website_access
@@ -905,23 +905,19 @@ def _run_chrome_fallback_command(
             #   fileno=1 (stderr dup'd onto stdout at the OS level).
             # * close_fds=True → block inheritance of every other handle.
             #   (Default on POSIX; must be explicit on Windows for stdio.)
+            # CREATE_NO_WINDOW is applied by _subprocess_compat.popen. We do NOT
+            # add CREATE_NEW_PROCESS_GROUP: on Python 3.11 Windows it interacts
+            # with asyncio's ProactorEventLoop such that the subprocess creation
+            # cancels the running loop task, surfacing as KeyboardInterrupt in
+            # app.run() and tearing down the CLI mid-turn (diag:
+            # "asyncio.CancelledError → KeyboardInterrupt").
             _popen_extra: dict = {}
             if os.name == "nt":
-                # CREATE_NO_WINDOW → don't attach a console (cmd.exe would
-                # otherwise briefly allocate one for the .cmd shim).
-                # Do NOT add CREATE_NEW_PROCESS_GROUP: on Python 3.11 Windows
-                # it interacts with asyncio's ProactorEventLoop such that the
-                # subprocess creation cancels the running loop task, which
-                # surfaces as KeyboardInterrupt in app.run() and tears down
-                # the CLI mid-turn. The agent thread's subprocess spawn
-                # unwound MainThread's prompt_toolkit loop that way — see
-                # diag log: "asyncio.CancelledError → KeyboardInterrupt".
-                _popen_extra["creationflags"] = windows_hide_flags()
                 _popen_extra["close_fds"] = True
                 _si = subprocess.STARTUPINFO()
                 _si.dwFlags |= subprocess.STARTF_USESTDHANDLES
                 _popen_extra["startupinfo"] = _si
-            proc = subprocess.Popen(
+            proc = _subprocess_compat.popen(
                 full, stdout=stdout_fd, stderr=stderr_fd,
                 stdin=subprocess.DEVNULL, env=browser_env,
                 **_popen_extra,
@@ -1942,6 +1938,12 @@ def _find_agent_browser() -> str:
     repo_root = Path(__file__).parent.parent
     local_bin_dir = repo_root / "node_modules" / ".bin"
     if local_bin_dir.is_dir():
+        if sys.platform == "win32":
+            native = repo_root / "node_modules" / "agent-browser" / "bin" / "agent-browser-win32-x64.exe"
+            if native.exists() and agent_browser_runnable(str(native)):
+                _cached_agent_browser = str(native)
+                _agent_browser_resolved = True
+                return _cached_agent_browser
         local_which = shutil.which("agent-browser", path=str(local_bin_dir))
         if local_which and agent_browser_runnable(local_which):
             _cached_agent_browser = local_which
@@ -2191,17 +2193,16 @@ def _run_browser_command(
             # three explicit handles (no leaked parent-console handles to
             # confuse the Rust binary's daemon-spawn), and close_fds=True to
             # block inheritance of everything else.
+            # CREATE_NO_WINDOW via _subprocess_compat.popen; NO
+            # CREATE_NEW_PROCESS_GROUP (cancels asyncio loop task on Python 3.11
+            # Windows → KeyboardInterrupt in CLI MainThread).
             _popen_extra: dict = {}
             if os.name == "nt":
-                # See matching block at the other Popen site — CREATE_NO_WINDOW
-                # only, NO CREATE_NEW_PROCESS_GROUP (cancels asyncio loop task
-                # on Python 3.11 Windows → KeyboardInterrupt in CLI MainThread).
-                _popen_extra["creationflags"] = windows_hide_flags()
                 _popen_extra["close_fds"] = True
                 _si = subprocess.STARTUPINFO()
                 _si.dwFlags |= subprocess.STARTF_USESTDHANDLES
                 _popen_extra["startupinfo"] = _si
-            proc = subprocess.Popen(
+            proc = _subprocess_compat.popen(
                 cmd_parts,
                 stdout=stdout_fd,
                 stderr=stderr_fd,

tools/process_registry.py
@@ -42,6 +42,7 @@ import uuid
 
 _IS_WINDOWS = platform.system() == "Windows"
 from tools.environments.local import _find_shell, _resolve_safe_cwd, _sanitize_subprocess_env
+from hermes_cli import _subprocess_compat
 from hermes_cli._subprocess_compat import windows_hide_flags
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional
@@ -569,12 +570,11 @@ class ProcessRegistry:
             return
         if _IS_WINDOWS:
             try:
-                subprocess.run(
+                _subprocess_compat.run(
                     ["taskkill", "/PID", str(pid), "/T", "/F"],
                     capture_output=True,
                     text=True,
                     timeout=10,
-                    creationflags=windows_hide_flags(),
                     stdin=subprocess.DEVNULL,
                 )
             except (FileNotFoundError, subprocess.TimeoutExpired, OSError):

dabit3/sonic-agent

(cherry picked from commit 5db1430af9ecd7ecc1e5eba93e7e1e965af59927)

apps/desktop/electron/main.cjs
@@ -1565,17 +1565,16 @@ function readVenvHome(venvRoot) {
 function getNoConsoleVenvPython(venvRoot) {
   if (!IS_WINDOWS) return getVenvPython(venvRoot)
 
-  // Prefer the venv's own pythonw shim — it carries pyvenv.cfg / site-packages
-  // wiring. Falling back to the base uv/python.org pythonw.exe skips the venv
-  // and breaks imports (yaml, sonic_cli, …) even when PYTHONPATH is patched.
-  const venvPythonw = path.join(venvRoot, 'Scripts', 'pythonw.exe')
-  if (fileExists(venvPythonw)) return venvPythonw
-
+  // uv venv launchers can re-exec console python.exe, which allocates conhost /
+  // Windows Terminal. Use base pythonw directly and provide imports via env.
   const baseHome = readVenvHome(venvRoot)
   if (baseHome) {
     const basePythonw = path.join(baseHome, 'pythonw.exe')
     if (fileExists(basePythonw)) return basePythonw
   }
+
+  const venvPythonw = path.join(venvRoot, 'Scripts', 'pythonw.exe')
+  if (fileExists(venvPythonw)) return venvPythonw
 
   return venvPythonw
 }
@@ -2797,7 +2796,7 @@ function createPythonBackend(root, label, dashboardArgs, options = {}) {
     args: ['-m', 'sonic_cli.main', ...dashboardArgs],
     env: buildDesktopBackendEnv({
       sonicHome: SONIC_HOME,
-      pythonPathEntries: [root],
+      pythonPathEntries: [root, ...getVenvSitePackagesEntries(venvRoot)],
       venvRoot
     }),
     root,
@@ -2821,7 +2820,7 @@ function createActiveBackend(dashboardArgs) {
     args: ['-m', 'sonic_cli.main', ...dashboardArgs],
     env: buildDesktopBackendEnv({
       sonicHome: SONIC_HOME,
-      pythonPathEntries: [ACTIVE_SONIC_ROOT],
+      pythonPathEntries: [ACTIVE_SONIC_ROOT, ...getVenvSitePackagesEntries(VENV_ROOT)],
       venvRoot: VENV_ROOT
     }),
     root: ACTIVE_SONIC_ROOT,

sonic_cli/gateway_windows.py
@@ -39,10 +39,10 @@ import time
 from pathlib import Path
 from xml.sax.saxutils import escape
 
+from sonic_cli import _subprocess_compat
 from sonic_cli._subprocess_compat import (
     windows_detach_flags,
     windows_detach_flags_without_breakaway,
-    windows_hide_flags,
 )
 
 # Short timeouts: schtasks occasionally wedges and we don't want to hang forever.
@@ -157,7 +157,7 @@ def _exec_schtasks(args: list[str]) -> tuple[int, str, str]:
     if schtasks is None:
         return (1, "", "schtasks.exe not found on PATH")
     try:
-        proc = subprocess.run(
+        proc = _subprocess_compat.run(
             [schtasks, *args],
             capture_output=True,
             text=True,
@@ -168,10 +168,6 @@ def _exec_schtasks(args: list[str]) -> tuple[int, str, str]:
             encoding=_schtasks_encoding(),
             errors="replace",
             timeout=_SCHTASKS_TIMEOUT_S,
-            # CREATE_NO_WINDOW avoids a flashing console window when the CLI
-            # is itself hosted in a TUI. See tools/browser_tool.py for the
-            # same pattern and the windows-subprocess-sigint-storm.md ref.
-            creationflags=windows_hide_flags(),
         )
         return (proc.returncode, proc.stdout or "", proc.stderr or "")
     except subprocess.TimeoutExpired:
@@ -1605,7 +1601,17 @@ def stop() -> None:
         drained = _drain_gateway_pid(pid, _windows_stop_drain_timeout())
 
     stopped_any = drained
-    if is_task_registered():
+    has_service_artifact = (
+        get_task_script_path().exists()
+        or get_task_script_path().with_suffix(".vbs").exists()
+        or get_startup_entry_path().exists()
+        or _legacy_startup_entry_path().exists()
+    )
+    if (
+        has_service_artifact
+        and os.getenv("HERMES_NONINTERACTIVE") != "1"
+        and is_task_registered()
+    ):
         code, _out, err = _exec_schtasks(["/End", "/TN", get_task_name()])
         # schtasks returns nonzero when the task isn't currently running — don't treat that as an error.
         if code == 0:
@@ -1673,7 +1679,8 @@ def restart() -> None:
 
     # Give Windows a moment to release the listening port.
     time.sleep(1.0)
-    start()
+    pid = _spawn_detached()
+    _report_gateway_start(f"direct spawn (PID {pid})")
 
     if not _wait_for_gateway_ready(timeout_s=15.0):
         raise RuntimeError(

sonic_cli/web_server.py
@@ -2481,16 +2481,52 @@ def _record_completed_action(name: str, message: str, exit_code: int = 1) -> Non
     _ACTION_RESULTS[name] = {"exit_code": exit_code, "pid": None}
 
 
-def _dashboard_spawn_executable() -> str:
-    """Prefer pythonw.exe for detached dashboard actions on Windows."""
+def _dashboard_spawn_details() -> Tuple[str, Dict[str, str]]:
+    """Return (executable, env overlay) for detached dashboard actions.
+
+    On Windows this mirrors the gateway's uv-safe detached launcher logic so
+    action spawns do not regress to console python.exe (which creates a visible
+    terminal window). Non-Windows callsites get the current interpreter and no
+    env overlay.
+    """
     if sys.platform != "win32":
-        return sys.executable
+        return sys.executable, {}
+
     exe = sys.executable
-    if exe.lower().endswith("python.exe"):
-        pythonw = os.path.join(os.path.dirname(exe), "pythonw.exe")
-        if os.path.isfile(pythonw):
-            return pythonw
-    return exe
+    try:
+        from hermes_cli.gateway_windows import _resolve_detached_python
+
+        venv_root = os.environ.get("VIRTUAL_ENV", "").strip()
+        if not venv_root:
+            for candidate in (PROJECT_ROOT / "venv", PROJECT_ROOT / ".venv"):
+                if (candidate / "Scripts" / "python.exe").exists():
+                    venv_root = str(candidate)
+                    break
+        probe_exe = (
+            os.path.join(venv_root, "Scripts", "python.exe")
+            if venv_root
+            else exe
+        )
+        windowless_exe, venv_dir, extra_pythonpath = _resolve_detached_python(probe_exe)
+        env_overlay: Dict[str, str] = {}
+        if venv_dir:
+            env_overlay["VIRTUAL_ENV"] = str(venv_dir)
+            site_packages = Path(venv_dir) / "Lib" / "site-packages"
+            if site_packages.exists() and str(site_packages) not in extra_pythonpath:
+                extra_pythonpath = [*extra_pythonpath, str(site_packages)]
+        if extra_pythonpath:
+            existing = os.environ.get("PYTHONPATH", "")
+            env_overlay["PYTHONPATH"] = os.pathsep.join(
+                [*extra_pythonpath, existing] if existing else list(extra_pythonpath)
+            )
+        return windowless_exe, env_overlay
+    except Exception:
+        # Best-effort fallback: sibling pythonw keeps the legacy no-console path.
+        if exe.lower().endswith("python.exe"):
+            pythonw = os.path.join(os.path.dirname(exe), "pythonw.exe")
+            if os.path.isfile(pythonw):
+                return pythonw, {}
+    return exe, {}
 
 
 def _spawn_sonic_action(subcommand: List[str], name: str) -> subprocess.Popen:
@@ -2507,15 +2543,20 @@ def _spawn_sonic_action(subcommand: List[str], name: str) -> subprocess.Popen:
         f"\n=== {name} started {time.strftime('%Y-%m-%d %H:%M:%S')} ===\n".encode()
     )
 
-    cmd = [_dashboard_spawn_executable(), "-m", "sonic_cli.main", *subcommand]
+    spawn_executable, spawn_env_overlay = _dashboard_spawn_details()
+    cmd = [spawn_executable, "-m", "sonic_cli.main", *subcommand]
 
     popen_kwargs: Dict[str, Any] = {
         "cwd": str(PROJECT_ROOT),
         "stdin": subprocess.DEVNULL,
         "stdout": log_file,
         "stderr": subprocess.STDOUT,
-        "env": {**os.environ, "SONIC_NONINTERACTIVE": "1"},
+        "env": {**os.environ, "SONIC_NONINTERACTIVE": "1", **spawn_env_overlay},
     }
+    log_file.write(f"spawn executable: {spawn_executable}\n".encode())
+    if spawn_env_overlay:
+        keys = ",".join(sorted(spawn_env_overlay.keys()))
+        log_file.write(f"spawn env overlay keys: {keys}\n".encode())
     if sys.platform == "win32":
         popen_kwargs["creationflags"] = windows_detach_flags()
     else:
@@ -13547,6 +13588,15 @@ def start_server(
     — used when a profile alias (``<profile> dashboard``) routes to the
     machine dashboard.
     """
+    # Desktop spawns this backend via a no-console venv python; a uv
+    # pythonw→python re-exec can still auto-allocate a console. Drop it.
+    # No-op on POSIX and when launched from an interactive shell.
+    try:
+        import hermes_bootstrap
+        hermes_bootstrap.detach_orphan_console()
+    except Exception:
+        pass
+
     import uvicorn
 
     try:

tools/browser_tool.py
@@ -68,7 +68,7 @@ from agent.auxiliary_client import call_llm
 from sonic_constants import agent_browser_runnable, get_sonic_home
 from utils import env_int, is_truthy_value
 from sonic_cli.config import DEFAULT_CONFIG, cfg_get
-from sonic_cli._subprocess_compat import windows_hide_flags
+from sonic_cli import _subprocess_compat
 
 try:
     from tools.website_policy import check_website_access
@@ -905,23 +905,19 @@ def _run_chrome_fallback_command(
             #   fileno=1 (stderr dup'd onto stdout at the OS level).
             # * close_fds=True → block inheritance of every other handle.
             #   (Default on POSIX; must be explicit on Windows for stdio.)
+            # CREATE_NO_WINDOW is applied by _subprocess_compat.popen. We do NOT
+            # add CREATE_NEW_PROCESS_GROUP: on Python 3.11 Windows it interacts
+            # with asyncio's ProactorEventLoop such that the subprocess creation
+            # cancels the running loop task, surfacing as KeyboardInterrupt in
+            # app.run() and tearing down the CLI mid-turn (diag:
+            # "asyncio.CancelledError → KeyboardInterrupt").
             _popen_extra: dict = {}
             if os.name == "nt":
-                # CREATE_NO_WINDOW → don't attach a console (cmd.exe would
-                # otherwise briefly allocate one for the .cmd shim).
-                # Do NOT add CREATE_NEW_PROCESS_GROUP: on Python 3.11 Windows
-                # it interacts with asyncio's ProactorEventLoop such that the
-                # subprocess creation cancels the running loop task, which
-                # surfaces as KeyboardInterrupt in app.run() and tears down
-                # the CLI mid-turn. The agent thread's subprocess spawn
-                # unwound MainThread's prompt_toolkit loop that way — see
-                # diag log: "asyncio.CancelledError → KeyboardInterrupt".
-                _popen_extra["creationflags"] = windows_hide_flags()
                 _popen_extra["close_fds"] = True
                 _si = subprocess.STARTUPINFO()
                 _si.dwFlags |= subprocess.STARTF_USESTDHANDLES
                 _popen_extra["startupinfo"] = _si
-            proc = subprocess.Popen(
+            proc = _subprocess_compat.popen(
                 full, stdout=stdout_fd, stderr=stderr_fd,
                 stdin=subprocess.DEVNULL, env=browser_env,
                 **_popen_extra,
@@ -1942,6 +1938,12 @@ def _find_agent_browser() -> str:
     repo_root = Path(__file__).parent.parent
     local_bin_dir = repo_root / "node_modules" / ".bin"
     if local_bin_dir.is_dir():
+        if sys.platform == "win32":
+            native = repo_root / "node_modules" / "agent-browser" / "bin" / "agent-browser-win32-x64.exe"
+            if native.exists() and agent_browser_runnable(str(native)):
+                _cached_agent_browser = str(native)
+                _agent_browser_resolved = True
+                return _cached_agent_browser
         local_which = shutil.which("agent-browser", path=str(local_bin_dir))
         if local_which and agent_browser_runnable(local_which):
             _cached_agent_browser = local_which
@@ -2191,17 +2193,16 @@ def _run_browser_command(
             # three explicit handles (no leaked parent-console handles to
             # confuse the Rust binary's daemon-spawn), and close_fds=True to
             # block inheritance of everything else.
+            # CREATE_NO_WINDOW via _subprocess_compat.popen; NO
+            # CREATE_NEW_PROCESS_GROUP (cancels asyncio loop task on Python 3.11
+            # Windows → KeyboardInterrupt in CLI MainThread).
             _popen_extra: dict = {}
             if os.name == "nt":
-                # See matching block at the other Popen site — CREATE_NO_WINDOW
-                # only, NO CREATE_NEW_PROCESS_GROUP (cancels asyncio loop task
-                # on Python 3.11 Windows → KeyboardInterrupt in CLI MainThread).
-                _popen_extra["creationflags"] = windows_hide_flags()
                 _popen_extra["close_fds"] = True
                 _si = subprocess.STARTUPINFO()
                 _si.dwFlags |= subprocess.STARTF_USESTDHANDLES
                 _popen_extra["startupinfo"] = _si
-            proc = subprocess.Popen(
+            proc = _subprocess_compat.popen(
                 cmd_parts,
                 stdout=stdout_fd,
                 stderr=stderr_fd,

tools/process_registry.py
@@ -42,6 +42,7 @@ import uuid
 
 _IS_WINDOWS = platform.system() == "Windows"
 from tools.environments.local import _find_shell, _resolve_safe_cwd, _sanitize_subprocess_env
+from sonic_cli import _subprocess_compat
 from sonic_cli._subprocess_compat import windows_hide_flags
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional
@@ -569,12 +570,11 @@ class ProcessRegistry:
             return
         if _IS_WINDOWS:
             try:
-                subprocess.run(
+                _subprocess_compat.run(
                     ["taskkill", "/PID", str(pid), "/T", "/F"],
                     capture_output=True,
                     text=True,
                     timeout=10,
-                    creationflags=windows_hide_flags(),
                     stdin=subprocess.DEVNULL,
                 )
             except (FileNotFoundError, subprocess.TimeoutExpired, OSError):