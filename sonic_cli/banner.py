Carrie111998/Hermes

fix(windows): capture is not a no-window boundary; route flashing spawns through chokepoint (#53829)
Follow-up to #53791 addressing review feedback: the footgun checker treated
capture_output=/stdout=/stderr=/check_output as proof a subprocess can't pop a
Windows console. That invariant is false — stream redirection controls where a
child's output goes, not whether a console is allocated. From a console-less
parent (Desktop/Electron, pythonw.exe, detached gateway/cron) a console-subsystem
child still flashes a window even when fully captured.

- check-windows-footguns.py: capture/redirect/check_output is no longer a blanket
  safe-pass. Added _WINDOWS_FLASHING_PROGRAMS (git/gh/npm/node/python/uv/ffmpeg/
  docker/powershell/…); calls to those are flagged even when captured. Non-flashing
  programs keep the capture exemption (no 271-site noise). _subprocess_compat.run/
  popen calls are inherently safe (wrapper injects CREATE_NO_WINDOW).
- Routed the 35 genuine flashing git/gh/npm/uv/ffmpeg/docker spawns through the
  _subprocess_compat.run/popen chokepoint (Brooklyn's wrapper from #53810) — the
  durable fix, not per-site annotations. cmd.exe /c start stays # ok (intentional).
- Updated tests + CONTRIBUTING.md rule #17 to the corrected invariant.

hermes_cli/banner.py
@@ -60,6 +60,7 @@ def _skin_color(key: str, fallback: str) -> str:
 # =========================================================================
 
 from hermes_cli import __version__ as VERSION, __release_date__ as RELEASE_DATE
+from hermes_cli import _subprocess_compat
 
 HERMES_AGENT_LOGO = """[bold #FFD700]██╗  ██╗███████╗██████╗ ███╗   ███╗███████╗███████╗       █████╗  ██████╗ ███████╗███╗   ██╗████████╗[/]
 [bold #FFD700]██║  ██║██╔════╝██╔══██╗████╗ ████║██╔════╝██╔════╝      ██╔══██╗██╔════╝ ██╔════╝████╗  ██║╚══██╔══╝[/]
@@ -157,7 +158,7 @@ def _is_official_ssh_remote(url: str | None) -> bool:
 
 def _git_stdout(args: list[str], *, cwd: Path, timeout: int = 5) -> Optional[str]:
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", *args],
             capture_output=True,
             text=True,
@@ -178,7 +179,7 @@ def _check_via_rev(local_rev: str) -> Optional[int]:
     or ``None`` on failure.
     """
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "ls-remote", _UPSTREAM_REPO_URL, "refs/heads/main"],
             capture_output=True, text=True, timeout=10,
         )
@@ -240,7 +241,7 @@ def _check_via_local_git(repo_dir: Path) -> Optional[int]:
         return 0 if head_rev == target_rev else UPDATE_AVAILABLE_NO_COUNT
 
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "rev-list", "--count", "HEAD..origin/main"],
             capture_output=True, text=True, timeout=5,
             cwd=str(repo_dir),
@@ -387,7 +388,7 @@ def _resolve_repo_dir() -> Optional[Path]:
 def _git_short_hash(repo_dir: Path, rev: str) -> Optional[str]:
     """Resolve a git revision to an 8-character short hash."""
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "rev-parse", "--short=8", rev],
             capture_output=True,
             text=True,
@@ -443,7 +444,7 @@ def get_git_banner_state(repo_dir: Optional[Path] = None) -> Optional[dict]:
 
     ahead = 0
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "rev-list", "--count", "origin/main..HEAD"],
             capture_output=True,
             text=True,
@@ -479,7 +480,7 @@ def get_latest_release_tag(repo_dir: Optional[Path] = None) -> Optional[tuple]:
         return None
 
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "describe", "--tags", "--abbrev=0"],
             capture_output=True,
             text=True,

hermes_cli/managed_uv.py
@@ -20,6 +20,7 @@ from pathlib import Path
 from typing import Optional
 
 from hermes_constants import get_hermes_home
+from hermes_cli import _subprocess_compat
 
 logger = logging.getLogger(__name__)
 
@@ -243,7 +244,7 @@ def _install_uv_windows(env: dict[str, str]) -> None:
     cmd = (
         'irm https://astral.sh/uv/install.ps1 | iex'
     )
-    subprocess.run(
+    _subprocess_compat.run(
         ["powershell", "-ExecutionPolicy", "Bypass", "-c", cmd],
         env=env,
         check=True,

hermes_cli/web_server.py
@@ -2814,7 +2814,7 @@ def _recent_upstream_commits(n: int = 20) -> List[Dict[str, Any]]:
     or git is unavailable. Never raises into the request path.
     """
     try:
-        out = subprocess.run(
+        out = _subprocess_compat.run(
             [
                 "git",
                 "-C",
@@ -13474,6 +13474,7 @@ _mount_plugin_api_routes()
 # always mounted — the gate middleware decides whether to enforce auth,
 # not whether the routes exist.
 from hermes_cli.dashboard_auth.routes import router as _dashboard_auth_router  # noqa: E402
+from hermes_cli import _subprocess_compat
 app.include_router(_dashboard_auth_router)
 
 mount_spa(app)

dabit3/sonic-agent

(cherry picked from commit 2ecca1e7d3e7c387b4d350f99dc699e2f43f73c5)

sonic_cli/banner.py
@@ -60,6 +60,7 @@ def _skin_color(key: str, fallback: str) -> str:
 # =========================================================================
 
 from sonic_cli import __version__ as VERSION, __release_date__ as RELEASE_DATE
+from sonic_cli import _subprocess_compat
 
 SONIC_AGENT_LOGO = """[bold #00BFFF]███████╗ ██████╗ ███╗   ██╗██╗ ██████╗[/]
 [bold #00BFFF]██╔════╝██╔═══██╗████╗  ██║██║██╔════╝[/]
@@ -155,7 +156,7 @@ def _is_official_ssh_remote(url: str | None) -> bool:
 
 def _git_stdout(args: list[str], *, cwd: Path, timeout: int = 5) -> Optional[str]:
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", *args],
             capture_output=True,
             text=True,
@@ -176,7 +177,7 @@ def _check_via_rev(local_rev: str) -> Optional[int]:
     or ``None`` on failure.
     """
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "ls-remote", _UPSTREAM_REPO_URL, "refs/heads/main"],
             capture_output=True, text=True, timeout=5,
         )
@@ -238,7 +239,7 @@ def _check_via_local_git(repo_dir: Path) -> Optional[int]:
         return 0 if head_rev == target_rev else UPDATE_AVAILABLE_NO_COUNT
 
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "rev-list", "--count", "HEAD..origin/main"],
             capture_output=True, text=True, timeout=5,
             cwd=str(repo_dir),
@@ -385,7 +386,7 @@ def _resolve_repo_dir() -> Optional[Path]:
 def _git_short_hash(repo_dir: Path, rev: str) -> Optional[str]:
     """Resolve a git revision to an 8-character short hash."""
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "rev-parse", "--short=8", rev],
             capture_output=True,
             text=True,
@@ -441,7 +442,7 @@ def get_git_banner_state(repo_dir: Optional[Path] = None) -> Optional[dict]:
 
     ahead = 0
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "rev-list", "--count", "origin/main..HEAD"],
             capture_output=True,
             text=True,
@@ -477,7 +478,7 @@ def get_latest_release_tag(repo_dir: Optional[Path] = None) -> Optional[tuple]:
         return None
 
     try:
-        result = subprocess.run(
+        result = _subprocess_compat.run(
             ["git", "describe", "--tags", "--abbrev=0"],
             capture_output=True,
             text=True,

sonic_cli/managed_uv.py
@@ -20,6 +20,7 @@ from pathlib import Path
 from typing import Optional
 
 from sonic_constants import get_sonic_home
+from sonic_cli import _subprocess_compat
 
 logger = logging.getLogger(__name__)
 
@@ -243,7 +244,7 @@ def _install_uv_windows(env: dict[str, str]) -> None:
     cmd = (
         'irm https://astral.sh/uv/install.ps1 | iex'
     )
-    subprocess.run(
+    _subprocess_compat.run(
         ["powershell", "-ExecutionPolicy", "Bypass", "-c", cmd],
         env=env,
         check=True,

sonic_cli/web_server.py
@@ -2814,7 +2814,7 @@ def _recent_upstream_commits(n: int = 20) -> List[Dict[str, Any]]:
     or git is unavailable. Never raises into the request path.
     """
     try:
-        out = subprocess.run(
+        out = _subprocess_compat.run(
             [
                 "git",
                 "-C",
@@ -13474,6 +13474,7 @@ _mount_plugin_api_routes()
 # always mounted — the gate middleware decides whether to enforce auth,
 # not whether the routes exist.
 from sonic_cli.dashboard_auth.routes import router as _dashboard_auth_router  # noqa: E402
+from sonic_cli import _subprocess_compat
 app.include_router(_dashboard_auth_router)
 
 mount_spa(app)