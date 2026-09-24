NousResearch/hermes-agent

Add dashboard backup upload and download

docker/stage2-hook.sh
@@ -338,6 +338,7 @@ fi
 # shell isn't a second interpreter — defends against $HERMES_HOME values
 # containing shell metacharacters. PR #30136 review item O2.
 as_hermes mkdir -p \
+    "$HERMES_HOME/backups" \
     "$HERMES_HOME/cron" \
     "$HERMES_HOME/sessions" \
     "$HERMES_HOME/logs" \

dabit3/sonic-agent

(cherry picked from commit 476875acb9f00b00f659dd6f71c843fb2f08aac2)

docker/stage2-hook.sh
@@ -338,6 +338,7 @@ fi
 # shell isn't a second interpreter — defends against $SONIC_HOME values
 # containing shell metacharacters. PR #30136 review item O2.
 as_sonic mkdir -p \
+    "$SONIC_HOME/backups" \
     "$SONIC_HOME/cron" \
     "$SONIC_HOME/sessions" \
     "$SONIC_HOME/logs" \