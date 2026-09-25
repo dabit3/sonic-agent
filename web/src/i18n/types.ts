ZHJay/hermes-agent

fix(web): confirm sidebar Update Hermes before running
Match the Restart Gateway flow with a confirm dialog that fetches cached
update metadata so users see commit-behind context before applying.

Co-authored-by: Cursor <cursoragent@cursor.com>

web/src/i18n/types.ts
@@ -149,6 +149,9 @@ export interface Translations {
     startedInBackground: string;
     stopped: string;
     updateHermes: string;
+    updateHermesConfirmMessage?: string;
+    updateHermesConfirmNow?: string;
+    updateHermesConfirmTitle?: string;
     updatingHermes: string;
     waitingForOutput: string;
   };

dabit3/sonic-agent

(cherry picked from commit 75d4aa93251329c7bd7f2921b449faa2d0fc636b)

web/src/i18n/types.ts
@@ -149,6 +149,9 @@ export interface Translations {
     startedInBackground: string;
     stopped: string;
     updateSonic: string;
+    updateSonicConfirmMessage?: string;
+    updateSonicConfirmNow?: string;
+    updateSonicConfirmTitle?: string;
     updatingSonic: string;
     waitingForOutput: string;
   };